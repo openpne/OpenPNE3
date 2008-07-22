18d3790857d264e3d700a6e95a9a6a0828b60042
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-22 08:49:00 +0000

    #2816:revert r7709, r7710
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@7798 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/admin/config/adminConfiguration.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class adminConfiguration extends sfApplicationConfiguration
-{
-  public function configure()
-  {
-  }
-}
--- apps/pc/config/pcConfiguration.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class pcConfiguration extends sfApplicationConfiguration
-{
-  public function configure()
-  {
-  }
-}
