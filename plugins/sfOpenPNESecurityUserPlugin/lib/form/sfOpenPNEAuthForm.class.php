52db2465e6bc1ce218e4c6fe5c023ca198ca5bd7
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-24 13:01:38 +0000

    #2857:sfOpenPNEAuthForm を追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@7834 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ plugins/sfOpenPNESecurityUserPlugin/lib/form/sfOpenPNEAuthForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * sfOpenPNEAuthForm represents a form to login.
+ *
+ * @package    symfony
+ * @subpackage user
+ * @author     Kousuke Ebihara <ebihara@tejimaya.net>
+ */
+abstract class sfOpenPNEAuthForm extends sfForm
+{
+  public function setForRegisterWidgets()
+  {
+  }
+}
