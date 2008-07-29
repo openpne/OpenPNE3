82021f8553dacc0a77be5ebe2f0acdf176877815
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-29 10:33:03 +0000

    #2861:オプションを考慮してプロフィール項目を取得するようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@7870 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/model/Member.php
+++ lib/model/Member.php
@@ -9,8 +9,24 @@
  */ 
 class Member extends BaseMember
 {
-  public function getProfile($profileName)
+  public function getProfile($profileName, $isGetProfileOptionId = false)
   {
-    return MemberProfilePeer::retrieveByMemberIdAndProfileName($this->getId(), $profileName);
+    $c = new Criteria();
+    $c->add(ProfilePeer::NAME, $profileName);
+    $c->add(MemberProfilePeer::MEMBER_ID, $this->getId());
+    $c->addJoin(MemberProfilePeer::PROFILE_ID, ProfilePeer::ID);
+    $profile = MemberProfilePeer::doSelectOne($c);
+
+    if ($isGetProfileOptionId) {
+      return $profile->getValue();
+    }
+
+    $formType = $profile->getProfile()->getFormType();
+    if ($formType == 'radio' || $formType == 'checkbox' || $formType == 'select') {
+      $option = ProfileOptionPeer::retrieveByPk($profile->getProfileOptionId());
+      return $option->getValue();
+    }
+
+    return $profile->getValue();
   }
 }
