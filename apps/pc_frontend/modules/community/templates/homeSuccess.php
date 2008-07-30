6db9e66ac7ac15655a91d7f8f325531a01c44e94
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-30 17:03:46 +0000

    テンプレート中でクレデンシャルを見ていたところを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@7882 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/community/templates/homeSuccess.php
+++ apps/pc_frontend/modules/community/templates/homeSuccess.php
@@ -3,12 +3,12 @@
 <ul>
 <li><?php echo link_to(sprintf('コミュニティメンバー一覧(%d)', $community->countCommunityMembers()), 'community/memberList?id=' . $community->getId()) ?></li>
 
-<?php if ($sf_user->hasCredential('editCommunity')) : ?>
+<?php if ($isEditCommunity) : ?>
 <li><?php echo link_to('このコミュニティを編集する', 'community/edit?id=' . $community->getId()) ?></li>
 <?php endif; ?>
 
-<?php if (!$sf_user->hasCredential('communityAdmin')) : ?>
-<?php if ($sf_user->hasCredential('communityMember')) : ?>
+<?php if (!$isCommunityAdmin) : ?>
+<?php if ($isCommunityMember) : ?>
 <li><?php echo link_to('このコミュニティを退会する', 'community/quit?id=' . $community->getId()) ?></li>
 <?php else : ?>
 <li><?php echo link_to('このコミュニティに参加する', 'community/join?id=' . $community->getId()) ?></li>
