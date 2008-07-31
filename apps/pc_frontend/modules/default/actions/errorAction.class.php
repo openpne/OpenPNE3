a9f9401faa81319815fd32b3dcd46d70e6eca08a
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-31 12:03:36 +0000

    製品環境において、 symfony の default モジュールのエラーメッセージを出さないように変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@7890 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/pc_frontend/modules/default/actions/errorAction.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    OpenPNE
+ * @subpackage default
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class errorAction extends sfAction
+{
+ /**
+  * Executes error action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function execute($request)
+  {
+  }
+}
