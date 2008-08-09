57f6d423a0678f5b8f453d53a20fcd32c691bf46
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-09 15:46:09 +0000

    #2911:モデルの単体テスト用にDBデータの初期化処理とfixtureを追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@7944 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ test/bootstrap/database.php
@@ -0,0 +1,12 @@
+<?php
+
+$app_configuration = ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true);
+$databaseManager = new sfDatabaseManager($app_configuration);
+$databaseManager->loadConfiguration();
+
+// execute propel:insert-sql task
+$task = new sfPropelInsertSqlTask(new sfEventDispatcher(), new sfAnsiColorFormatter());
+$task->execute();
+
+$data = new sfPropelData();
+$data->loadData(dirname(__FILE__) . '/../fixtures');
