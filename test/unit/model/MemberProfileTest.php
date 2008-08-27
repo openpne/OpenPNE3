521a9a569acf97925571eb62217c2f1264f4d4f4
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-27 11:45:59 +0000

    MemberProfileTest を記述
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8137 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/unit/model/MemberProfileTest.php
+++ test/unit/model/MemberProfileTest.php
@@ -7,11 +7,20 @@ $t = new lime_test(null, new lime_output_color());
 
 //------------------------------------------------------------
 
-$t->diag('Member::__toString()');
+$t->diag('MemberProfile::__toString()');
+$memberProfile = MemberProfilePeer::retrieveByPK(1);
+$option = ProfileOptionPeer::retrieveByPk($memberProfile->getProfileOptionId());
+$t->cmp_ok((string)$memberProfile, '===', $option->getValue(), '__toString() returns an option');
+$memberProfile = MemberProfilePeer::retrieveByPK(2);
+$t->cmp_ok((string)$memberProfile, '===', $memberProfile->getValue(), '__toString() returns a value');
 
 //------------------------------------------------------------
 
-$t->diag('Member::getValue()');
+$t->diag('MemberProfile::getValue()');
+$memberProfile = MemberProfilePeer::retrieveByPK(1);
+$t->cmp_ok($memberProfile->getValue(), '===', 1, 'getValue() returns an option');
+$memberProfile = MemberProfilePeer::retrieveByPK(2);
+$t->cmp_ok($memberProfile->getValue(), '===', 'よろしくお願いします。', 'getValue() returns a value');
 
 //------------------------------------------------------------
 
