d0687685960e7a06e0cba7117072792e0516f730
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-27 09:52:45 +0000

    #2911:NaviPeerTestを追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8132 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ test/unit/model/NaviPeerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once dirname(__FILE__) . '/../../bootstrap/unit.php';
+include_once dirname(__FILE__) . '/../../bootstrap/database.php';
+
+$t = new lime_test(null, new lime_output_color());
+
+//------------------------------------------------------------
+
+$t->diag('NaviPeer::retrieveByType()');
+
+//------------------------------------------------------------
+
+$t->diag('NaviPeer::retrieveTypes()');
