3f0f4fd42947793fb34457e33915874d9b914281
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-27 10:05:42 +0000

    #2911:NaviPeer に対するテストを記述
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8133 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/unit/model/NaviPeerTest.php
+++ test/unit/model/NaviPeerTest.php
@@ -8,7 +8,17 @@ $t = new lime_test(null, new lime_output_color());
 //------------------------------------------------------------
 
 $t->diag('NaviPeer::retrieveByType()');
+$navis = NaviPeer::retrieveByType('test');
+$t->isa_ok($navis, 'array', 'retrieveByType() returns an array.');
+foreach ($navis as $navi)
+{
+  $t->isa_ok($navi, 'Navi', 'each navi is a Navi');
+  $t->cmp_ok($navi->getType(), '===', 'test', 'Navi::getType() returns "test".');
+}
 
 //------------------------------------------------------------
 
 $t->diag('NaviPeer::retrieveTypes()');
+$types = NaviPeer::retrieveTypes();
+$t->isa_ok($navis, 'array', 'retrieveTypes() returns an array.');
+$t->cmp_ok(in_array('test', $types), '===', true, '$types contains "test".');
