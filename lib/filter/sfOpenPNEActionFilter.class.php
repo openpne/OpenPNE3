ac3abf661a8722dc0b0e3769269a82974789e783
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-04 13:42:54 +0000

    #2940:added sfOpenPNEActionFilter
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8566 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ lib/filter/sfOpenPNEActionFilter.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * sfOpenPNEActionFilter
+ *
+ * @package    OpenPNE
+ * @subpackage filter
+ * @author     Kousuke Ebihara <ebihara@tejimaya.net>
+ */
+class sfOpenPNEActionFilter extends sfFilter
+{
+  /**
+   * @see sfFilter
+   */
+  final public function execute($filterChain)
+  {
+    $this->preFilter();
+    $filterChain->execute();
+    $this->postFilter();
+  }
+
+ /**
+  * Executes this filter before an action.
+  *
+  * If you want to do something before an action, you have to override the preFilter() method.
+  */
+  protected function preFilter()
+  {
+  }
+
+ /**
+  * Executes this filter after an action.
+  *
+  * If you want to do something after an action, you have to override the postFilter() method.
+  */
+  protected function postFilter()
+  {
+  }
+}
