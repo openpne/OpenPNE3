f807b6faba3f39fbd455495a922266688550a12b
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-07 09:30:56 +0000

    #2885:enabled authentication for mobile_frontend
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8597 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/modules/member/actions/actions.class.php
+++ apps/mobile_frontend/modules/member/actions/actions.class.php
@@ -5,18 +5,51 @@
  *
  * @package    OpenPNE
  * @subpackage member
- * @author     Your name here
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
  * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
 class memberActions extends sfActions
 {
  /**
-  * Executes index action
+  * Executes login action
   *
   * @param sfRequest $request A request object
   */
-  public function executeIndex($request)
+  public function executeLogin($request)
   {
-    $this->forward('default', 'module');
+    $this->getUser()->logout();
+
+    $this->form = $this->getUser()->getAuthForm();
+
+    if ($request->isMethod('post')) {
+      $this->form->bind($request->getParameter('auth'));
+      if ($this->form->isValid()) {
+        $this->redirectIf($this->getUser()->login($this->form), 'member/home');
+      }
+      return sfView::ERROR;
+    }
+
+    return sfView::SUCCESS;
+  }
+
+ /**
+  * Executes logout action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeLogout($request)
+  {
+    $this->getUser()->logout();
+    $this->redirect('member/login');
+  }
+
+ /**
+  * Executes home action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeHome($request)
+  {
+    return sfView::SUCCESS;
   }
 }
