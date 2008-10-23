83043944d633820a42fc371f4b25ad7c9fdd88d8
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-23 16:12:43 +0000

    #2886:adjusted the font size for mobile phone
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8788 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/templates/layout.php
+++ apps/mobile_frontend/templates/layout.php
@@ -2,6 +2,15 @@
 <head>
 <?php include_http_metas() ?>
 <title><?php echo OpenPNEConfig::get('sns_name') ?></title>
+<?php if ($sf_request->getMobile()->isSoftBank()) : ?>
+<style type="text/css">
+*{font-size:small;}
+</style>
+<?php elseif ($sf_request->getMobile()->isEZWeb()) : ?>
+<style type="text/css">
+*{font-size:xx-small;}
+</style>
+<?php endif; ?>
 </head>
 <body>
 <?php echo $sf_content ?>
