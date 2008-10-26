7a7d881bb2b4dc2951e9ba5969ab34b02fe1000d
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-26 09:20:36 +0000

    #2885:added friend module for mobile phone
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8817 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/mobile_frontend/modules/friend/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * friend actions.
+ *
+ * @package    OpenPNE
+ * @subpackage friend
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class friendActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex($request)
+  {
+    $this->forward('default', 'module');
+  }
+}
