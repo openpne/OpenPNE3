cc9b77315ae38d3ed7e34c0114036c87cf6dba08
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-26 13:34:26 +0000

    #2885:added community module for mobile phone
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8820 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/mobile_frontend/modules/community/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * community actions.
+ *
+ * @package    OpenPNE
+ * @subpackage community
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
+ */
+class communityActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex($request)
+  {
+    $this->forward('default', 'module');
+  }
+}
