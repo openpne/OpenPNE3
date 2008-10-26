012a9a61c14184e00e0f5b1bcc8a16a77d0b654d
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-26 05:47:03 +0000

    #2886:added a footer menu for mobile phone
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8811 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/templates/layout.php
+++ apps/mobile_frontend/templates/layout.php
@@ -13,6 +13,21 @@
 <?php endif; ?>
 </head>
 <body>
+
+<a name="#top">
+
 <?php echo $sf_content ?>
+
+<a name="#bottom">
+
+<hr color="#0d6ddf">
+
+■<?php echo link_to('ﾛｸﾞｱｳﾄ', 'member/logout') ?><br>
+
+<table width="100%">
+<tbody><tr><td align="center" bgcolor="#0d6ddf">
+<font color="#eeeeee"><font color="#eeeeee"><?php echo link_to('ﾎｰﾑ', 'member/home') ?></font> / <a href="#top"><font color="#eeeeee">↑上へ</font></a> / <a href="#bottom"><font color="#eeeeee">下へ</font></a></font><br>
+</td></tr></tbody></table>
+
 </body>
 </html>
