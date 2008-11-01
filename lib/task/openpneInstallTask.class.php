70a14aaa87de33ebc6921d8c2cdc332d18f3a2b8
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-01 12:25:20 +0000

    #3114:openpneInstallTask is now initializing database, loading datas and clearing caches
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8921 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/task/openpneInstallTask.class.php
+++ lib/task/openpneInstallTask.class.php
@@ -1,26 +1,83 @@
 <?php
 
-class openpneInstallTask extends sfBaseTask
+class openpneInstallTask extends sfPropelBaseTask
 {
   protected function configure()
   {
     $this->namespace        = 'openpne';
     $this->name             = 'install';
-    $this->briefDescription = '';
+
+    $this->addArguments(array(
+      new sfCommandArgument('dsn', sfCommandArgument::REQUIRED, 'The database dsn'),
+    ));
+
+    $this->briefDescription = 'Install OpenPNE';
     $this->detailedDescription = <<<EOF
-The [openpne:install|INFO] task does things.
+The [openpne:install|INFO] task installs and configures OpenPNE.
 Call it with:
 
-  [php symfony openpne:install|INFO]
+  [./symfony openpne:install|INFO]
 EOF;
-    // add arguments here, like the following:
-    //$this->addArgument('application', sfCommandArgument::REQUIRED, 'The application name');
-    // add options here, like the following:
-    //$this->addOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev');
   }
 
   protected function execute($arguments = array(), $options = array())
   {
-    // add code here
+    $this->clearCache();
+    $this->configureDatabase($arguments['dsn']);
+    $this->buildDb();
+    $this->clearCache();
+  }
+
+  protected function configureDatabase($dsn)
+  {
+    $file = sfConfig::get('sf_config_dir').'/databases.yml';
+    $config = array();
+
+    if (file_exists($file))
+    {
+      $config = sfYaml::load($file);
+    }
+
+    $config['all']['propel'] = array(
+      'class' => 'sfPropelDatabase',
+      'param' => array('dsn' => $dsn, 'encoding' => 'utf8'),
+    );
+
+    file_put_contents($file, sfYaml::dump($config, 4));
+
+    // update propel.ini
+    $propelini = sfConfig::get('sf_config_dir').'/propel.ini';
+    if (file_exists($propelini))
+    {
+      $content = file_get_contents($propelini);
+      if (preg_match('/^(.+?):\/\//', $dsn, $match))
+      {
+        $content = preg_replace('/^propel\.database(\s*)=(\s*)(.+?)$/m', 'propel.database$1=$2'.$match[1], $content);
+        $content = preg_replace('/^propel\.database\.createUrl(\s*)=(\s*)(.+?)$/m', 'propel.database.createUrl$1=$2'.$dsn, $content);
+        $content = preg_replace('/^propel\.database\.url(\s*)=(\s*)(.+?)$/m', 'propel.database.url$1=$2'.$dsn, $content);
+
+        file_put_contents($propelini, $content);
+      }
+    }
+  }
+
+  protected function clearCache()
+  {
+    $cc = new sfCacheClearTask($this->dispatcher, $this->formatter);
+    $cc->run();
+  }
+
+  protected function buildDb()
+  {
+    $this->schemaToXML(self::DO_NOT_CHECK_SCHEMA, 'generated-');
+    $this->copyXmlSchemaFromPlugins('generated-');
+
+/*
+    $buildDb = new sfPropelBuildDbTask($this->dispatcher, $this->formatter);
+    $buildDb->run();
+    */
+
+    $buildAllLoad = new sfPropelBuildAllLoadTask($this->dispatcher, $this->formatter);
+    $buildAllLoad->run(array('application' => 'pc_frontend'));
   }
 }
