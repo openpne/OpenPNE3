acf4f92e083ed45c103b68fc3f45e6c2612620ee
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-02 08:17:59 +0000

    #3115:applied temporal styles to header
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@8927 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/layout.php
+++ apps/pc_frontend/templates/layout.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head>
-
 <?php include_http_metas() ?>
 <?php include_metas() ?>
-
 <title><?php echo OpenPNEConfig::get('sns_name') ?></title>
-
-<link rel="shortcut icon" href="/favicon.ico" />
-
 </head>
-<body>
+<body><div id="Body">
+<div id="Container">
 
+<div id="Header">
 <h1><?php echo link_to(OpenPNEConfig::get('sns_name'), '@homepage') ?></h1>
 
+<div id="globalNav">
 <?php include_component('default', 'globalNavi') ?>
+</div><!-- globalNav -->
 
+<div id="localNav">
 <?php if (isset($naviType)) : ?>
 <?php include_component('default', 'localNavi', array('type' => $naviType)) ?>
 <?php else: ?>
 <?php include_component('default', 'localNavi') ?>
 <?php endif; ?>
+</div><!-- localNav -->
+</div><!-- Header -->
+
 
 <?php echo $sf_content ?>
 
+</div><!-- Container -->
+</div><!-- Body -->
 </body>
 </html>
