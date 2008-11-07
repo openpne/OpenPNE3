368be02c919b1ec332908f7782141eb4a6bf7425
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-07 09:09:39 +0000

    #3123:consolidated duplicated action methods to sfOpenPNEFriendAction
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9033 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/friend/actions/actions.class.php
+++ apps/pc_frontend/modules/friend/actions/actions.class.php
@@ -6,135 +6,16 @@
  * @package    OpenPNE
  * @subpackage friend
  * @author     Kousuke Ebihara <ebihara@tejimaya.com>
- * @version    SVN: $Id: actions.class.php 9301 2008-05-27 01:08:46Z dwhittle $
  */
-class friendActions extends sfActions
+class friendActions extends sfOpenPNEFriendAction
 {
   public function preExecute()
   {
-    $this->id = $this->getRequestParameter('id', $this->getUser()->getMemberId());
-    $this->isFriend = FriendPeer::isFriend($this->getUser()->getMemberId(), $this->id);
-    $this->isFriendPre = FriendPrePeer::isFriendPre($this->getUser()->getMemberId(), $this->id);
-    if ($this->id == $this->getUser()->getMemberId()) {
+    parent::preExecute();
+
+    if ($this->id == $this->getUser()->getMemberId())
+    {
       sfConfig::set('sf_navi_type', 'default');
     }
-  }
-
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex($request)
-  {
-    $this->forward('default', 'module');
-  }
-
- /**
-  * Executes list action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeList($request)
-  {
-    $this->pager = FriendPeer::getFriendListPager($this->id, $request->getParameter('page', 1));
-
-    if (!$this->pager->getNbResults()) {
-      return sfView::ERROR;
-    }
-
-    return sfView::SUCCESS;
-  }
-
- /**
-  * Executes link action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeLink($request)
-  {
-    if ($this->isFriend) {
-      return sfView::ERROR;
-    }
-
-    if ($this->isFriendPre) {
-      return sfView::ERROR;
-    }
-
-    $this->redirectToHomeIfIdIsNotValid();
-
-    $friendPre = new FriendPre();
-    $friendPre->setMemberIdFrom($this->getUser()->getMemberId());
-    $friendPre->setMemberIdTo($this->id);
-    $friendPre->save();
-
-    $this->redirect('member/profile?id=' . $this->id);
-  }
-
- /**
-  * Executes linkAccept action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeLinkAccept($request)
-  {
-    if (!$this->isFriendPre) {
-      return sfView::ERROR;
-    }
-
-    $this->redirectToHomeIfIdIsNotValid();
-
-    $friendPre = FriendPrePeer::retrieveByMemberIdToAndMemberIdFrom($this->getUser()->getMemberId(), $this->id);
-    $this->forward404Unless($friendPre);
-
-    FriendPeer::link($friendPre->getMemberIdTo(), $friendPre->getMemberIdFrom());
-    $friendPre->delete();
-
-    $this->redirect('member/profile?id=' . $this->id);
-  }
-
- /**
-  * Executes linkReject action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeLinkReject($request)
-  {
-    if (!$this->isFriendPre) {
-      return sfView::ERROR;
-    }
-
-    $this->redirectToHomeIfIdIsNotValid();
-
-    $friendPre = FriendPrePeer::retrieveByMemberIdToAndMemberIdFrom($this->getUser()->getMemberId(), $this->id);
-    $this->forward404Unless($friendPre);
-
-    $friendPre->delete();
-
-    $this->redirect('@homepage');
-  }
-
- /**
-  * Executes unlink action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeUnlink($request)
-  {
-    if (!$this->isFriend) {
-      return sfView::ERROR;
-    }
-    $this->redirectToHomeIfIdIsNotValid();
-    FriendPeer::unlink($this->getUser()->getMemberId(), $this->id);
-    $this->redirect('member/profile?id=' . $this->id);
-  }
-
- /**
-  * Redirects to your home if ID is yours or it is empty.
-  */
-  private function redirectToHomeIfIdIsNotValid()
-  {
-    $this->redirectUnless($this->id, 'member/home');
-    $this->redirectIf(($this->id == $this->getUser()->getMemberId()), 'member/home');
   }
 }
