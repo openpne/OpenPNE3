9504d392d09ff320c733b2097055f0066bf2b520
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-30 20:09:02 +0000

    #3030:added opPearRest class
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9436 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ lib/plugin/opPearRest.class.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * opPearRest interacts with a PEAR channel for OpenPNE plugin
+ *
+ * @package    OpenPNE
+ * @subpackage plugin
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ */
+class opPearRest extends sfPearRest
+{
+  public function retrieveData($url, $accept = false, $forcestring = false, $channel = false)
+  {
+    $content = parent::retrieveData($url, $accept, true, $channel);
+
+    $result = $this->parseXML($content);
+    return $result;
+  }
+
+  public function saveCache($url, $contents, $lastmodified, $nochange = false, $cacheid = null)
+  {
+    if (!is_array($contents))
+    {
+      $contents = $this->parseXML($contents);
+    }
+    return parent::saveCache($url, $contents, $lastmodified, $nochange, $cacheid);
+  }
+
+  public function parseXML($content)
+  {
+    if (is_array($content))
+    {
+      return $content;
+    }
+
+    $parser = new PEAR_XMLParser();
+    $parser->parse($content);
+    return $parser->getData();
+  }
+
+}
