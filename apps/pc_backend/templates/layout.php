daf8994715ade62c38d38d32de90eee3ff92d080
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-06 14:55:35 +0000

    #3190:added styles for pc_backend
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9488 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_backend/templates/layout.php
+++ apps/pc_backend/templates/layout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head>
 
 <?php include_http_metas() ?>
@@ -7,27 +7,32 @@
 
 <title><?php echo OpenPNEConfig::get('sns_name') ?>管理画面</title>
 
-<link rel="shortcut icon" href="/favicon.ico" />
-
 </head>
 <body>
 
+<div id="header">
 <h1><?php echo OpenPNEConfig::get('sns_name') ?>管理画面</h1>
+</div>
 
 <?php if ($sf_user->isAuthenticated()) : ?>
+<div id="menu">
 <ul>
-<li><?php echo link_to('SNS設定', 'sns/config'); ?>
+<li><?php echo link_to(__('管理画面トップ'), 'security/top'); ?>
+<li><?php echo link_to(__('SNS設定'), 'sns/config'); ?>
   <ul>
-    <li><?php echo link_to('お知らせ設定', 'sns/informationConfig'); ?></li>
+    <li><?php echo link_to(__('お知らせ設定'), 'sns/informationConfig'); ?></li>
   </ul>
 </li>
-<li><?php echo link_to('ナビ設定', 'navi/index'); ?></li>
-<li><?php echo link_to('プロフィール項目設定', 'profile/list'); ?></li>
-<li><?php echo link_to('プラグイン設定', 'plugin/list'); ?></li>
+<li><?php echo link_to(__('ナビ設定'), 'navi/index'); ?></li>
+<li><?php echo link_to(__('プロフィール項目設定'), 'profile/list'); ?></li>
+<li><?php echo link_to(__('プラグイン設定'), 'plugin/list'); ?></li>
 </ul>
+</div>
 <?php endif; ?>
 
+<div id="body">
 <?php echo $sf_content ?>
+</div>
 
 </body>
 </html>
