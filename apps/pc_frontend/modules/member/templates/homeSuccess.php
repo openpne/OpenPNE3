25777cd32f94e56dd85151ad862d60dc860ee41a
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-07 06:11:34 +0000

    #3192:added styles for LayoutA
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9496 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/templates/homeSuccess.php
+++ apps/pc_frontend/modules/member/templates/homeSuccess.php
@@ -1,3 +1,4 @@
+<?php slot('op_top') ?>
 <?php
 $body = '';
 if ($information) {
@@ -5,10 +6,13 @@ if ($information) {
 }
 include_information_box('information', $body)
 ?>
+<?php end_slot() ?>
 
-<?php include_box('homeTopSampleBox', 'ホーム画面', sprintf('あなたのホームです。(メンバーID:%d, ニックネーム:%s)', $sf_user->getMemberId(), $sf_user->getMember()->getName())); ?>
-
+<?php slot('op_sidemenu') ?>
 <ul>
 <li><?php echo link_to(sprintf('フレンド一覧(%d)', $sf_user->getMember()->countFriends()), 'friend/list') ?></li>
 <li><?php echo link_to(sprintf('参加コミュニティ一覧(%d)', $sf_user->getMember()->countCommunityMembers()), 'community/joinlist') ?></li>
 </ul>
+<?php end_slot() ?>
+
+<?php include_box('homeTopSampleBox', 'ホーム画面', sprintf('あなたのホームです。(メンバーID:%d, ニックネーム:%s)', $sf_user->getMemberId(), $sf_user->getMember()->getName())); ?>
