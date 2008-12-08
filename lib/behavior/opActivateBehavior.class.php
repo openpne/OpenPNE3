94fc70846bf78ce554003353a1e1ea6c35e78291
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-08 14:39:53 +0000

    #3131:added a $enabled property for temporally disabling opActivateBehavior to it
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9514 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/behavior/opActivateBehavior.class.php
+++ lib/behavior/opActivateBehavior.class.php
@@ -1,13 +1,39 @@
 <?php
 class opActivateBehavior
 {
+  static protected $enabled = true;
+
   public function doSelectStmt($class, Criteria $criteria, $con = null)
   {
-    $criteria->add(call_user_func(array($class, 'translateFieldName'), 'is_active', BasePeer::TYPE_FIELDNAME, BasePeer::TYPE_COLNAME), 0, Criteria::NOT_EQUAL);
+    if (self::$enabled)
+    {
+      $criteria->add(call_user_func(array($class, 'translateFieldName'), 'is_active', BasePeer::TYPE_FIELDNAME, BasePeer::TYPE_COLNAME), 0, Criteria::NOT_EQUAL);
+    }
+    else
+    {
+      self::enable();
+    }
   }
 
   public function doCount($class, Criteria $criteria, $con = null)
   {
-    $criteria->add(call_user_func(array($class, 'translateFieldName'), 'is_active', BasePeer::TYPE_FIELDNAME, BasePeer::TYPE_COLNAME), 0, Criteria::NOT_EQUAL);
+    if (self::$enabled)
+    {
+      $criteria->add(call_user_func(array($class, 'translateFieldName'), 'is_active', BasePeer::TYPE_FIELDNAME, BasePeer::TYPE_COLNAME), 0, Criteria::NOT_EQUAL);
+    }
+    else
+    {
+      self::enable();
+    }
+  }
+
+  public static function enable()
+  {
+    self::$enabled = true;
+  }
+
+  public static function disable()
+  {
+    self::$enabled = false;
   }
 }
