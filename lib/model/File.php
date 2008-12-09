977827d91561dfec0d523bbf34a1fa51430e9cf3
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-09 14:53:20 +0000

    #3208:fixed File for images
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9531 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/model/File.php
+++ lib/model/File.php
@@ -6,4 +6,35 @@ class File extends BaseFile
   {
     return (string)$this->getName();
   }
+
+  public function getImageFormat()
+  {
+    if (!$this->isImage())
+    {
+      return false;
+    }
+
+    $type = explode('/', $this->getType());
+    $result = $type[1];
+
+    if ($result === 'jpeg')
+    {
+      $result = 'jpg';
+    }
+
+    return $result;
+  }
+
+  public function isImage()
+  {
+    $type = $this->getType();
+    if ($type === 'image/jpeg'
+      || $type === 'image/gif'
+      || $type === 'image/png')
+    {
+      return true;
+    }
+
+    return false;
+  }
 }
--- lib/model/FilePeer.php
+++ lib/model/FilePeer.php
@@ -2,4 +2,17 @@
 
 class FilePeer extends BaseFilePeer
 {
+  public static function retrieveByFilename($filename, PropelPDO $con = null)
+  {
+    if (is_null($con))
+    {
+      $con = Propel::getConnection(FilePeer::DATABASE_NAME, Propel::CONNECTION_READ);
+    }
+
+    $c = new Criteria(FilePeer::DATABASE_NAME);
+    $c->add(FilePeer::NAME, $filename);
+
+    $result = FilePeer::doSelectOne($c, $con);
+    return $result;
+  }
 }
