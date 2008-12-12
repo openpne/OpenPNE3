f06a7e89eaa1ef877bf325d4aed08ff922a01699
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-12 04:56:20 +0000

    #3211:fixed notice
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9546 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_partsMemberImageBox.php
+++ apps/pc_frontend/templates/_partsMemberImageBox.php
@@ -3,7 +3,7 @@
 <p class="photo"><?php echo image_tag_sf_image($option['image'], array('size' => '180x180')) ?></p>
 </div>
 <p class="text"><?php echo $option['name'] ?></p>
-<?php if ($option['moreInfo']) : ?>
+<?php if (!empty($option['moreInfo'])) : ?>
 <ul>
 <?php foreach ($option['moreInfo'] as $key => $value) : ?>
 <li><?php echo $option['moreInfo']->getRaw($key) ?></li>
