4a3a777b2145980b45d283c186204dc6eae49ec3
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-21 12:40:54 +0000

    #3217,#3218:added the searchResultList parts
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9614 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/templates/searchSuccess.php
+++ apps/pc_frontend/modules/member/templates/searchSuccess.php
@@ -8,28 +8,28 @@ $options = array(
 include_box('searchMember', __('メンバー検索'), '', $options);
 ?>
 
-<?php use_helper('Pagination', 'Date'); ?>
+<?php use_helper('Date'); ?>
 
 <?php if ($pager->getNbResults()): ?>
-<div class="dparts searchResultList"><div class="parts">
-<div class="partsHeading"><h3><?php echo __('検索結果') ?></h3></div>
+<?php
+$list = array();
+foreach ($pager->getResults() as $key => $member)
+{
+  $list[$key] = array();
+  $list[$key][__('ニックネーム')] = $member->getName();
+  $list[$key][$member->getProfile('self_intro')->getCaption()] = $member->getProfile('self_intro');
+  $list[$key][__('最終ログイン')] = distance_of_time_in_words($member->getLastLoginTime());
+}
 
-<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, 'member/search?page=%d'); ?></p></div>
+$options = array(
+  'pager'          => $pager,
+  'link_to_page'   => 'member/search?page=%d',
+  'link_to_detail' => 'member/profile?id=%d',
+  'list'           => $list,
+);
 
-<div class="block">
-<?php foreach ($pager->getResults() as $member): ?>
-<div class="ditem"><div class="item"><table><tbody><tr>
-<td rowspan="4" class="photo"><?php echo link_to(image_tag_sf_image($member->getImageFilename(), array('size' => '76x76')), 'member/profile?id='.$member->getId()) ?></td>
-<th><?php echo __('ニックネーム') ?></th><td><?php echo $member->getName() ?></td>
-</tr><tr>
-<th><?php echo $member->getProfile('self_intro')->getCaption() ?></th><td><?php echo $member->getProfile('self_intro') ?></td>
-</tr><tr class="operation">
-<th><?php echo __('最終ログイン') ?></th><td><span class="text"><?php echo distance_of_time_in_words($member->getLastLoginTime()) ?></span><span class="moreInfo"><?php echo link_to(__('詳細を見る'), 'member/profile?id='.$member->getId()) ?></span></td>
-</tr></tbody></table></div></div>
-<?php endforeach; ?>
-</div>
-<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, 'member/search?page=%d'); ?></p></div>
-</div></div>
+include_parts('searchResultList', 'searchCommunityResult', $options);
+?>
 <?php else: ?>
 <?php include_box('searchMemberResult', __('検索結果'), __('該当するメンバーはいませんでした。')) ?>
 <?php endif; ?>
