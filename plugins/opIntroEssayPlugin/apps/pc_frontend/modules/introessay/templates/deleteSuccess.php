5175d2def42ba99bc10711fb65fa6f28a91c4f05
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-22 04:50:08 +0000

    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9620 8bbc783d-d51a-0410-adab-8fa69d43f207

--- plugins/opIntroEssayPlugin/apps/pc_frontend/modules/introessay/templates/deleteSuccess.php
+++
@@ -1,1 +0,0 @@
-<?php include_box( 'infobox', '紹介文を作成する', '削除しました') ?>
--- plugins/opIntroEssayPlugin/apps/pc_frontend/modules/introessay/templates/indexInput.php
+++
@@ -1,6 +0,0 @@
-<?php include_box( 'writeform', '紹介文を作成する', '', array(
-  'form' => array($form),
-  'url' => 'introessay/index?id=' . $id,
-  'button' => '作成'
-)) ?>
-
