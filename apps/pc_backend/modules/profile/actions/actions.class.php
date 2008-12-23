739831be6ef9e37212b74ddb76e6317a0a97dae8
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-23 07:16:49 +0000

    added ability to change sort order of the profile items by Ajax
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9640 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_backend/modules/profile/actions/actions.class.php
+++ apps/pc_backend/modules/profile/actions/actions.class.php
@@ -17,11 +17,16 @@ class profileActions extends sfActions
   */
   public function executeList($request)
   {
-    $this->profiles = ProfilePeer::doSelect(new Criteria());
+    $criteria = new Criteria();
+    $criteria->addAscendingOrderByColumn(ProfilePeer::SORT_ORDER);
+    $this->profiles = ProfilePeer::doSelect($criteria);
     $this->option_form = array();
+
+    $criteria = new Criteria();
+    $criteria->addAscendingOrderByColumn(ProfileOptionPeer::SORT_ORDER);
     foreach ($this->profiles as $value) {
       $this->option_form[$value->getId()] = array();
-      foreach ($value->getProfileOptions() as $option) {
+      foreach ($value->getProfileOptions($criteria) as $option) {
         $this->option_form[$value->getId()][$option->getId()] = new ProfileOptionForm(ProfileOptionPeer::retrieveByPk($option->getId()));
       }
       $newProfileOption = new ProfileOption();
@@ -98,5 +103,60 @@ class profileActions extends sfActions
       $this->profileOption->delete();
     }
     $this->redirect('profile/list');
+  }
+
+  /**
+   * Executes sortProfile action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeSortProfile($request)
+  {
+    if ($request->isXmlHttpRequest())
+    {
+      $order = $request->getParameter('profiles');
+      for ($i = 0; $i < count($order); $i++)
+      {
+        $profile = ProfilePeer::retrieveByPk($order[$i]);
+        if ($profile)
+        {
+          $profile->setSortOrder($i * 10);
+          $profile->save();
+        }
+      }
+    }
+    return sfView::NONE;
+  }
+
+  /**
+   * Executes sortProfileOption action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeSortProfileOption($request)
+  {
+    if ($request->isXmlHttpRequest())
+    {
+      $parameters = $request->getParameterHolder();
+      $keys       = $parameters->getNames();
+      foreach ($keys as $key)
+      {
+        if (preg_match('/^profile_options_\d+$/', $key, $match))
+        {
+          $order = $parameters->get($match[0]);
+          for ($i = 0; $i < count($order); $i++)
+          {
+            $profileOption = ProfileOptionPeer::retrieveByPk($order[$i]);
+            if ($profileOption)
+            {
+              $profileOption->setSortOrder($i * 10);
+              $profileOption->save();
+            }
+          }
+          break;
+        }
+      }
+    }
+    return sfView::NONE;
   }
 }
--- apps/pc_backend/modules/profile/templates/listSuccess.php
+++ apps/pc_backend/modules/profile/templates/listSuccess.php
@@ -1,8 +1,9 @@
+<?php use_helper('Javascript'); ?>
 <h2>プロフィール項目設定</h2>
 
 <h3>プロフィール項目一覧</h3>
 <p><?php echo link_to('プロフィール項目登録', 'profile/edit') ?></p>
-<table>
+<table id="profiles">
 <thead><tr>
 <th colspan="2">操作</th>
 <th>ID</th>
@@ -18,6 +19,7 @@
 <th>検索</th>
 </tr></thead>
 <?php foreach ($profiles as $value): ?>
+<tbody id="profile_<?php echo $value->getId() ?>" class="sortable">
 <tr>
 <td><?php echo link_to('変更', 'profile/edit?id=' . $value->getId()) ?></td>
 <td><?php echo link_to('削除', 'profile/delete?id=' . $value->getId()) ?></td>
@@ -33,15 +35,20 @@
 <td><?php echo ($value->getIsDispConfig() ? '○' : '×') ?></td>
 <td><?php echo ($value->getIsDispSearch() ? '○' : '×') ?></td>
 </tr>
+</tbody>
 <?php endforeach; ?>
 </table>
+<?php echo sortable_element('profiles',array(
+  'tag' => 'tbody',
+  'url' => 'profile/sortProfile'
+)) ?>
 
 <h3>プロフィール選択肢一覧</h3>
 <?php foreach ($profiles as $value): ?>
 <?php if ($value->getFormType() == 'radio' || $value->getFormType() == 'checkbox' || $value->getFormType() == 'select') : ?>
 
 <h4><?php echo $value->getCaption() ?></h4>
-<table>
+<table id="profile_options_<?php echo $value->getId() ?>">
 <thead><tr>
 <th>ID</th>
 <th>項目名(ja_JP)</th>
@@ -50,6 +57,11 @@
 </tr></thead>
 <?php foreach ($option_form[$value->getId()] as $form) : ?>
 <form action="<?php echo url_for('profile/editOption?id=' . $form->getObject()->getId()) ?>" method="post">
+<?php if (!$form->getObject()->isNew()) : ?>
+<tbody id="profile_option_<?php echo $form->getObject()->getId() ?>" class="sortable">
+<?php else: ?>
+<tbody>
+<?php endif; ?>
 <tr>
 <td><?php echo ($form->getObject()->isNew() ? '-' : $form->getObject()->getId()) ?></td>
 <td>
@@ -80,8 +92,13 @@
 </td>
 <?php endif; ?>
 </tr>
+</tbody>
 <?php endforeach; ?>
 </table>
-
+<?php echo sortable_element('profile_options_'.$value->getId(),array(
+  'tag'  => 'tbody',
+  'only' => 'sortable',
+  'url'  => 'profile/sortProfileOption'
+)) ?>
 <?php endif; ?>
 <?php endforeach; ?>
