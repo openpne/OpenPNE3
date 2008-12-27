9cdf9d74179c3098b65d6eef3ffcc2aa92b763c6
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-12-27 20:18:10 +0000

    #2879:changed configuration of the password to confirm current password
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9743 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/form/MemberConfigForm/MemberConfigPasswordForm.class.php
+++ lib/form/MemberConfigForm/MemberConfigPasswordForm.class.php
@@ -10,4 +10,22 @@
 class MemberConfigPasswordForm extends MemberConfigForm
 {
   protected $category = 'password';
+
+  public function configure()
+  {
+    $this->setWidget('now_password', new sfWidgetFormInputPassword());
+    $this->setValidator('now_password', new sfValidatorCallback(array('callback' => array($this, 'isValidPassword'))));
+    $this->widgetSchema->setLabel('now_password', '現在のパスワード');
+  }
+
+  public function isValidPassword($validator, $value)
+  {
+    $member = sfContext::getInstance()->getUser()->getMember();
+    if (md5($value) !== MemberConfigPeer::retrieveByNameAndMemberId('password', $member->getId())->getValue())
+    {
+      throw new sfValidatorError(new sfValidatorPass(), 'invalid', array('value' => $value));
+    }
+
+    return $value;
+  }
 }
