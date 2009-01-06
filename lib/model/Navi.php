2107a2bf443a0aec59ee862ec6b47022682df3c9
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-06 06:29:45 +0000

    #3341:fixed NaviForm allows an empty navi item
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9947 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/model/Navi.php
+++ lib/model/Navi.php
@@ -9,4 +9,22 @@
  */ 
 class Navi extends BaseNavi
 {
+  public function save(PropelPDO $con = null)
+  {
+    if (!$this->getSortOrder())
+    {
+      $maxSortOrder = 0;
+
+      $navis = NaviPeer::retrieveByType($this->getType());
+      $finalNavi = array_pop($navis);
+      if ($finalNavi)
+      {
+        $maxSortOrder = $finalNavi->getSortOrder();
+      }
+
+      $this->setSortOrder($maxSortOrder + 10);
+    }
+
+    return parent::save($con);
+  }
 }
