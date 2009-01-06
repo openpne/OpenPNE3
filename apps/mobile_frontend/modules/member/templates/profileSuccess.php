a27447eb9fbdbfbeb4bdd145cde31be70f83d3ca
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-06 05:45:17 +0000

    #3334:fixed member/profile of the mobile_frontend application doesn't display a member image
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@9945 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/modules/member/templates/profileSuccess.php
+++ apps/mobile_frontend/modules/member/templates/profileSuccess.php
@@ -13,6 +13,7 @@
 </td></tr>
 
 <tr><td align="center" width="50%" valign="top">
+<?php echo image_tag_sf_image($member->getImageFileName(), array('size' => '120x120', 'format' => 'jpg')) ?>
 </td>
 <td valign="top">
 <?php foreach ($member->getProfiles() as $profile) : ?>
