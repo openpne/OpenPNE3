247dab273aa6b6deb8af871807eeeec67addef2b
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-07 12:19:39 +0000

    Include_customizes is added for the change and the introduction sentence insertion of the layout.
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10026 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/friend/templates/manageSuccess.php
+++ apps/pc_frontend/modules/friend/templates/manageSuccess.php
@@ -1,11 +1,28 @@
-<?php echo pager_navigation($pager, 'friend/list?page=%d&id=' . $sf_params->get('id')); ?>
-<ul>
-<?php foreach ($pager->getResults() as $member) : ?>
-<li>
-<?php echo link_to($member->getName(), 'member/profile?id=' . $member->getId()); ?>
-&nbsp;
-<?php echo link_to(__('フレンドから外す'), 'friend/unlink?id='.$member->getId()); ?>
-</li>
+<div class="dparts searchResultList"><div class="parts">
+<div class="partsHeading"><h3><?php echo __('マイフレンド管理') ?></h3></div>
+
+<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, 'friend/list?page=%d&id=' . $sf_params->get('id')); ?></p></div>
+
+<div class="item"><table><tbody>
+<?php foreach ($pager->getResults() as $member): ?>
+<?php $sf_user->setAttribute('id', $member->getId()) ?>
+
+<tr>
+
+<?php include_customizes('id_photo', 'before') ?>
+<td class="photo">
+<?php echo link_to(image_tag_sf_image($member->getImageFilename(), array('size' => '76x76')), 'member/profile?id=' . $member->getId()); ?><br />
+<?php echo link_to( $member->getName(), 'member/profile?id=' . $member->getId()) ?>
+</td>
+<?php include_customizes('id_photo', 'after') ?>
+
+<?php include_customizes('id_friend', 'before') ?>
+<td><?php echo link_to(__('フレンドから外す'), 'friend/unlink?id='.$member->getId()) ?></td>
+<?php include_customizes('id_friend', 'after') ?>
+</tr>
 <?php endforeach; ?>
-</ul>
-<?php echo pager_navigation($pager, 'friend/list?page=%d&id=' . $sf_params->get('id')); ?>
+</tbody></table></div>
+
+<div class="pagerRelative"><p class="number"><?php echo pager_navigation($pager, 'friend/list?page=%d&id=' . $sf_params->get('id')); ?></p></div>
+
+</div></div>
