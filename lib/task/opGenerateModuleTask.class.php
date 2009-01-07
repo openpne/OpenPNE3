f54c8d31446c9ffcb46cde1dd4de3184c3c439b1
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-07 03:34:10 +0000

    #3385:changed reference to the Apache License 2.0 and copyright for the OpenPNE package are now compact.
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10003 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/task/opGenerateModuleTask.class.php
+++ lib/task/opGenerateModuleTask.class.php
@@ -1,19 +1,11 @@
 <?php
 
 /**
- * Copyright (C) 2005-2009 OpenPNE Project
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
  */
 
 class opGenerateModuleTask extends sfBaseTask
