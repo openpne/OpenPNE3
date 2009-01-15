07201d1c726bdd6f46d2c5a171ea92654b8e0bea
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-15 10:47:12 +0000

    #3111:changed the apiConfiguration class to inherit the sfOpenPNEApplicationConfiguration class
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10182 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/api/config/apiConfiguration.class.php
+++ apps/api/config/apiConfiguration.class.php
@@ -1,6 +1,16 @@
 <?php
 
-class apiConfiguration extends sfApplicationConfiguration
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+require_once dirname(__FILE__).'/../../../lib/config/sfOpenPNEApplicationConfiguration.class.php';
+
+class apiConfiguration extends sfOpenPNEApplicationConfiguration
 {
   public function configure()
   {
