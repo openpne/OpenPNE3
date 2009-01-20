593afe048853602467353e05befdd8ad772c3e4e
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-20 02:33:50 +0000

    #3124:fixed notice when a "cross-application" task is executed
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10237 8bbc783d-d51a-0410-adab-8fa69d43f207

--- data/version.php
+++ data/version.php
@@ -1,2 +1,14 @@
 <?php
-define('OPENPNE_VERSION', '3.0beta4-dev');
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+if (!defined('OPENPNE_VERSION'))
+{
+  define('OPENPNE_VERSION', '3.0beta4-dev');
+}
