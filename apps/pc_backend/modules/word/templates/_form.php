7c6874760c7fe3e8c699d105e513911c0df87952
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-23 11:44:10 +0000

    #3316 added word edit link in the pc_backend but yet do not replace
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10345 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/pc_backend/modules/word/templates/_form.php
@@ -0,0 +1,39 @@
+<?php include_stylesheets_for_form($form) ?>
+<?php include_javascripts_for_form($form) ?>
+
+<form action="<?php echo url_for('word/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+<?php if (!$form->getObject()->isNew()): ?>
+<input type="hidden" name="sf_method" value="put" />
+<?php endif; ?>
+  <table>
+    <tfoot>
+      <tr>
+        <td colspan="2">
+          <?php echo $form->renderHiddenFields() ?>
+          &nbsp;<a href="<?php echo url_for('word/index') ?>">Cancel</a>
+          <?php if (!$form->getObject()->isNew()): ?>
+            &nbsp;<?php echo link_to('Delete', 'word/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+          <?php endif; ?>
+          <input type="submit" value="Save" />
+        </td>
+      </tr>
+    </tfoot>
+    <tbody>
+      <?php echo $form->renderGlobalErrors() ?>
+      <tr>
+        <th><?php echo $form['word_type_id']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['word_type_id']->renderError() ?>
+          <?php echo $form['word_type_id'] ?>
+        </td>
+      </tr>
+      <tr>
+        <th><?php echo $form['name']->renderLabel() ?></th>
+        <td>
+          <?php echo $form['name']->renderError() ?>
+          <?php echo $form['name'] ?>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</form>
