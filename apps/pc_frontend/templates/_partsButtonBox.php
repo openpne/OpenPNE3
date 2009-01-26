2b8b77c0e876a9c1b6eed5291a6ee1a0e2d6acbc
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-26 13:45:03 +0000

    #3372:added some parts
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10437 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/pc_frontend/templates/_partsButtonBox.php
@@ -0,0 +1,21 @@
+<?php
+$url = sfContext::getInstance()->getRouting()->getCurrentInternalUri();
+$options->setDefault('button', __('Yes'));
+$options->setDefault('url', $url);
+$options->setDefault('method', 'post');
+?>
+<?php if(isset($options['body'])): ?>
+<div class="block">
+<?php echo $options['body'] ?>
+</div>
+<?php endif ?>
+
+<div class="operation">
+<ul class="moreInfo button">
+<li>
+<form action="<?php echo url_for($options['url']) ?>" method="<?php echo $options['method'] ?>">
+<input type="submit" class="input_submit" value="<?php echo $options['button'] ?>" />
+</form>
+</li>
+</ul>
+</div>
