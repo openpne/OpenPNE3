4313072bbe3950f5af258ffc9f41bd1d74538f48
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-26 03:02:39 +0000

    #3316:reverted to r10344
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10408 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_backend/templates/layout.php
+++ apps/pc_backend/templates/layout.php
@@ -19,7 +19,6 @@
 <ul>
 <li><?php echo link_to(__('管理画面トップ'), 'default/top'); ?></li>
 <li><?php echo link_to(__('メンバー管理'), 'member/index'); ?></li>
-<li><?php echo link_to(__('SNS内名称設定'), 'word/index'); ?></li>
 <li><?php echo link_to(__('SNS設定'), 'sns/config'); ?></li>
 <li><?php echo link_to(__('デザイン設定'), 'design/index'); ?></li>
 <li><?php echo link_to(__('プロフィール項目設定'), 'profile/list'); ?></li>
