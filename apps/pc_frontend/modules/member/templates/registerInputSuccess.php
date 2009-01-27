71321150acd8b30eca18d148bd74d3ca08846f39
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-27 05:24:58 +0000

    #3448:fixed Member Registration from
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10491 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/templates/registerInputSuccess.php
+++ apps/pc_frontend/modules/member/templates/registerInputSuccess.php
@@ -1,8 +1,11 @@
+<div class="dparts form"><div class="parts">
+<div class="partsHeading"><h3><?php echo __('Member Registration') ?></h3></div>
 <form action="<?php echo url_for('member/registerInput') ?>" method="post">
 <table>
 <?php echo $form ?>
 <tr>
-<td colspan="2"><input type="submit" class="input_submit" value="登録" /></td>
+<td colspan="2"><input type="submit" class="input_submit" value="<?php echo __('Register') ?>" /></td>
 </tr>
 </table>
 </form>
+</div></div>
