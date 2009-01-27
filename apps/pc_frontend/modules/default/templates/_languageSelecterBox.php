e45bf31f7cfcbc6bf0d9002cdd2d3c934bf605f3
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-01-27 04:20:19 +0000

    #3448:fixed languageSelectBox style
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10484 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/default/templates/_languageSelecterBox.php
+++ apps/pc_frontend/modules/default/templates/_languageSelecterBox.php
@@ -1,5 +1,5 @@
 <form action="<?php echo url_for('member/changeLanguage') ?>" method="post">
-<?php echo $form['culture']->renderLabel() ?>
+<?php echo $form['culture']->renderLabel() ?>:
 <?php echo $form['culture']->render(array('onchange' => 'submit(this.form)')) ?>
 <?php echo $form->renderHiddenFields() ?>
 </form>
