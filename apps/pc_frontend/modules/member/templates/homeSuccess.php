acca0263ec65c49c4556fd704d66cf758c38bcea
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-01 08:18:01 +0000

    #3555:added ability to sort the home gadgets
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10591 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/templates/homeSuccess.php
+++ apps/pc_frontend/modules/member/templates/homeSuccess.php
@@ -1,22 +1,26 @@
-<?php slot('op_top') ?>
+<?php use_helper('Javascript') ?>
+
 <?php if ($topGadgets): ?>
+<?php slot('op_top') ?>
 <?php foreach ($topGadgets as $gadget): ?>
 <?php if ($gadget->isEnabled()): ?>
 <?php include_component($gadget->getComponentModule(), $gadget->getComponentAction(), array('gadget' => $gadget)); ?>
 <?php endif; ?>
 <?php endforeach; ?>
-<?php endif; ?>
 <?php end_slot() ?>
+<?php echo sortable_element('Top', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
+<?php endif; ?>
 
-<?php slot('op_sidemenu') ?>
 <?php if ($sideMenuGadgets): ?>
+<?php slot('op_sidemenu') ?>
 <?php foreach ($sideMenuGadgets as $gadget): ?>
 <?php if ($gadget->isEnabled()): ?>
 <?php include_component($gadget->getComponentModule(), $gadget->getComponentAction(), array('gadget' => $gadget)); ?>
 <?php endif; ?>
 <?php endforeach; ?>
-<?php endif; ?>
 <?php end_slot() ?>
+<?php echo sortable_element('Left', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
+<?php endif; ?>
 
 <?php if ($contentsGadgets): ?>
 <?php foreach ($contentsGadgets as $gadget): ?>
@@ -24,17 +28,19 @@
 <?php include_component($gadget->getComponentModule(), $gadget->getComponentAction(), array('gadget' => $gadget)); ?>
 <?php endif; ?>
 <?php endforeach; ?>
+<?php echo sortable_element('Center', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
 <?php endif; ?>
 
-<?php slot('op_bottom') ?>
 <?php if ($bottomGadgets): ?>
+<?php slot('op_bottom') ?>
 <?php foreach ($bottomGadgets as $gadget): ?>
 <?php if ($gadget->isEnabled()): ?>
 <?php include_component($gadget->getComponentModule(), $gadget->getComponentAction(), array('gadget' => $gadget)); ?>
 <?php endif; ?>
 <?php endforeach; ?>
-<?php endif; ?>
 <?php end_slot() ?>
+<?php echo sortable_element('Bottom', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
+<?php endif; ?>
 
 <?php echo javascript_tag('
 $$(".partsHeading").each(function(obj){
