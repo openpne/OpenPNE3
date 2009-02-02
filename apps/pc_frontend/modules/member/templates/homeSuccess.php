90c598e8906b8d3b87712f4d398d9496f81c5178
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-02 16:50:45 +0000

    #3555:added ability to store the current gadget's sorting status
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10600 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/templates/homeSuccess.php
+++ apps/pc_frontend/modules/member/templates/homeSuccess.php
@@ -8,7 +8,7 @@
 <?php endif; ?>
 <?php endforeach; ?>
 <?php end_slot() ?>
-<?php echo sortable_element('Top', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
+<?php echo sortable_element('Top', array('tag' => 'div', 'handle' => 'partsHeading', 'onChange' => 'function(obj){storeSort(obj)}')) ?>
 <?php endif; ?>
 
 <?php if ($sideMenuGadgets): ?>
@@ -19,7 +19,7 @@
 <?php endif; ?>
 <?php endforeach; ?>
 <?php end_slot() ?>
-<?php echo sortable_element('Left', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
+<?php echo sortable_element('Left', array('tag' => 'div', 'handle' => 'partsHeading', 'onChange' => 'function(obj){storeSort(obj)}')) ?>
 <?php endif; ?>
 
 <?php if ($contentsGadgets): ?>
@@ -28,7 +28,7 @@
 <?php include_component($gadget->getComponentModule(), $gadget->getComponentAction(), array('gadget' => $gadget)); ?>
 <?php endif; ?>
 <?php endforeach; ?>
-<?php echo sortable_element('Center', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
+<?php echo sortable_element('Center', array('tag' => 'div', 'handle' => 'partsHeading', 'onChange' => 'function(obj){storeSort(obj)}')) ?>
 <?php endif; ?>
 
 <?php if ($bottomGadgets): ?>
@@ -43,6 +43,25 @@
 <?php endif; ?>
 
 <?php echo javascript_tag('
+function storeSort(obj)
+{
+  var result = "";
+  Element.childElements(obj.parentNode).each(function(child, index){
+    if (child.id && child.id.match(/_[0-9]+$/)) {
+      if (result) {
+        result = result + ",";
+      }
+      result = result + child.id;
+    }
+  });
+  var path = "'.($sf_request->getRelativeUrlRoot() ? $sf_request->getRelativeUrlRoot() : '/').'";
+  var expires = new Date();
+  expires.setTime((new Date()).getTime() + (10 * 12 * 30 * 24 * 60 * 60 * 1000));
+  var pos = obj.parentNode.id;
+
+  opCookie.set("HomeGadget_" + pos + "_sort", result, expires, path);
+}
+
 function foldObj(obj, display)
 {
   Element.childElements(obj.parentNode).each(function(child, index){
@@ -63,7 +82,7 @@ function foldObj(obj, display)
         var id = child.parentNode.parentNode.id;
         var expires = new Date();
         expires.setTime((new Date()).getTime() + (10 * 12 * 30 * 24 * 60 * 60 * 1000));
-        opCookie.set("HomeGadget_"+id+"_toggle", child.visible(), expires, path);
+        opCookie.set("HomeGadget_" + id + "_toggle", child.visible(), expires, path);
       }
     }
   });
@@ -79,6 +98,61 @@ $$(".partsHeading").each(function(obj){
   if (display != null) {
     foldObj(obj, display);
   }
+
+});
+
+var TopSort = opCookie.get("HomeGadget_Top_sort");
+console.debug(TopSort);
+var Top = document.getElementById("Top");
+var preGadget = null;
+TopSort.split(",").each(function(value){
+  var gadget = document.getElementById(value);
+  if (!preGadget) {
+    console.debug("first gadget");
+    Element.remove(gadget);
+    Insertion.Top(Top, gadget);
+  } else {
+    console.debug("normal gadget");
+    Element.remove(gadget);
+    Insertion.After(preGadget, gadget);
+  }
+  preGadget = gadget;
+});
+
+var LeftSort = opCookie.get("HomeGadget_Left_sort");
+console.debug(LeftSort);
+var Left = document.getElementById("Left");
+var preGadget = null;
+LeftSort.split(",").each(function(value){
+  var gadget = document.getElementById(value);
+  if (!preGadget) {
+    console.debug("first gadget");
+    Element.remove(gadget);
+    Insertion.Top(Left, gadget);
+  } else {
+    console.debug("normal gadget");
+    Element.remove(gadget);
+    Insertion.After(preGadget, gadget);
+  }
+  preGadget = gadget;
+});
+
+var CenterSort = opCookie.get("HomeGadget_Center_sort");
+console.debug(CenterSort);
+var Center = document.getElementById("Center");
+var preGadget = null;
+CenterSort.split(",").each(function(value){
+  var gadget = document.getElementById(value);
+  if (!preGadget) {
+    console.debug("first gadget");
+    Element.remove(gadget);
+    Insertion.Top(Center, gadget);
+  } else {
+    console.debug("normal gadget");
+    Element.remove(gadget);
+    Insertion.After(preGadget, gadget);
+  }
+  preGadget = gadget;
 });
 
 ') ?>
