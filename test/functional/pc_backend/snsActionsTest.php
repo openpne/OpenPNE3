9f4a034af78a4cf07babe8cc947eec2383064d67
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-03 03:56:09 +0000

    #3488:added functional tests for sns/config
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10603 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/functional/pc_backend/snsActionsTest.php
+++ test/functional/pc_backend/snsActionsTest.php
@@ -2,12 +2,38 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-// create a new test browser
-$browser = new sfTestBrowser();
+$browser = new sfTestFunctional(new sfBrowser(), new lime_test(null, new lime_output_color()));
+$params = array('admin_user' => array(
+));
+$browser
+  ->info('0. Login')
+  ->get('/default/login')
+  ->click('ログイン', array('admin_user' => array(
+    'username' => 'admin',
+    'password' => 'password',
+  )))
+  ->isStatusCode(302)
 
-$browser->
-  get('/sns/index')->
-  isStatusCode(200)->
-  isRequestParameter('module', 'sns')->
-  isRequestParameter('action', 'index')->
-  checkResponseElement('body', '!/This is a temporary page/');
+  ->info('1. When an admin user tries to change the SNS configuration. (ref. #3488)')
+  ->info('A category is not selected, admin user can change the general configuration.')
+  ->get('/sns/config')
+  ->click('設定変更', array('sns_config' => array(
+    'sns_name'           => 'Super MySNS',
+    'sns_title'          => 'Super MySNS - Come on join us! -',
+    'admin_mail_address' => 'admin@example.com',
+    'enable_pc'          => 1,
+    'enable_mobile'      => 1,
+  )))
+  ->isStatusCode(302)
+
+  ->info('A general category is selected, admin user can change the general configuration.')
+  ->get('/sns/config/category/general')
+  ->click('設定変更', array('sns_config' => array(
+    'sns_name'           => 'Super MySNS',
+    'sns_title'          => 'Super MySNS - Come on join us! -',
+    'admin_mail_address' => 'admin@example.com',
+    'enable_pc'          => 1,
+    'enable_mobile'      => 1,
+  )))
+  ->isStatusCode(302)
+;
