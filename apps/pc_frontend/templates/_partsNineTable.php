0d05b377e03ce8aaaa304c38bd1086f1cce29d4c
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-03 08:28:05 +0000

    #3546:fixed links of the join community list gadget is not a valid
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10609 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_partsNineTable.php
+++ apps/pc_frontend/templates/_partsNineTable.php
@@ -1,4 +1,4 @@
-<?php if (count($options['list'])): ?>
+<?php if (count($options->list)): ?>
 
 <?php
 $options->setDefault('row', 3);
@@ -7,22 +7,22 @@ $options->setDefault('type', 'full');
 ?>
 
 <table>
-<?php $row = ceil(count($options['list']) / $options['row']) ?>
+<?php $row = ceil(count($options->list) / $options->row) ?>
 <?php for ($i = $j = 1; $row >= $i; $i++): ?>
-<?php if ($options['type'] === 'full' || $options['type'] === 'only_image'): ?>
+<?php if ($options->type === 'full' || $options->type === 'only_image'): ?>
 <tr class="photo">
-<?php for ($j = ($i * $options['col']) - $options['col']; ($i * $options['col']) > $j; $j++): ?>
-<td><?php if (!empty($options['list'][$j])): ?>
-<?php echo link_to(image_tag_sf_image($options['list'][$j]->getImageFileName(), array('size' => '76x76')), $options['link_to'].$options['list'][$j]->getId()) ?>
+<?php for ($j = ($i * $options->col) - $options->col; ($i * $options->col) > $j; $j++): ?>
+<td><?php if (!empty($options->list[$j])): ?>
+<?php echo link_to(image_tag_sf_image($options->list[$j]->getImageFileName(), array('size' => '76x76')), $options->link_to.$options->list[$j]->getId()) ?>
 <?php endif; ?></td>
 <?php endfor; ?>
 </tr>
 <?php endif; ?>
-<?php if ($options['type'] === 'full' || $options['type'] === 'only_name'): ?>
+<?php if ($options->type === 'full' || $options->type === 'only_name'): ?>
 <tr class="text">
-<?php for ($j = ($i * $options['col']) - $options['col']; ($i * $options['col']) > $j; $j++): ?>
-<td><?php if (!empty($options['list'][$j])): ?>
-<?php echo link_to($options['list'][$j]->getName(), 'member/profile?id='.$options['list'][$j]->getId()) ?>
+<?php for ($j = ($i * $options->col) - $options->col; ($i * $options->col) > $j; $j++): ?>
+<td><?php if (!empty($options->list[$j])): ?>
+<?php echo link_to($options->list[$j]->getName(), $options->link_to.$options->list[$j]->getId()) ?>
 <?php endif; ?></td>
 <?php endfor; ?>
 </tr>
