e5743761c2426eaa956ba9790dd51c8deeaff566
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-04 10:36:54 +0000

    #3579:fixed added 'information' id
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10631 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_partsInformationBox.php
+++ apps/pc_frontend/templates/_partsInformationBox.php
@@ -2,6 +2,8 @@
 
 <div class="body">
 <?php include_customizes($id, 'bodyTop') ?>
+<?php include_customizes('information', 'bodyTop') ?>
 <?php echo $options->getRaw('body') ?>
+<?php include_customizes('information', 'bodyBottom') ?>
 <?php include_customizes($id, 'bodyBottom') ?>
 </div>
