9eba06bdbe150d1509ad24ea4c1f4969516a9be2
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-10 09:37:46 +0000

    #3502:added the openpne:migrate task
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10737 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/migration/opMigration.class.php
+++ lib/migration/opMigration.class.php
@@ -15,7 +15,7 @@
  * @subpackage migration
  * @author     Kousuke Ebihara <ebihara@tejimaya.com>
  */
-abstract class opMigration extends Doctrine_Migration
+class opMigration extends Doctrine_Migration
 {
   protected $targetName = '';
 
@@ -29,7 +29,7 @@ abstract class opMigration extends Doctrine_Migration
     if (!$connectionName)
     {
       $names = $this->dbManager->getNames();
-      $connectionName = array_shift($connectionName);
+      $connectionName = array_shift($names);
     }
     $this->database = $this->dbManager->getDatabase($connectionName);
 
@@ -88,5 +88,83 @@ abstract class opMigration extends Doctrine_Migration
 
     $method = 'post'.$direction;
     $this->$method();
+  }
+
+ /**
+  * @see Doctrine_Migration
+  */
+  protected function createMigrationTable()
+  {
+    // do nothing
+  }
+
+ /**
+  * @see Doctrine_Migration
+  */
+  protected function setCurrentVersion($number)
+  {
+    SnsConfigPeer::set($this->targetName.'_revision', $number);
+  }
+
+ /**
+  * @see Doctrine_Migration
+  */
+  public function getCurrentVersion()
+  {
+    return SnsConfigPeer::get($this->targetName.'_revision', 0);
+  }
+
+ /**
+  * @see Doctrine_Migration
+  */
+  public function hasMigrated()
+  {
+    return is_null(SnsConfigPeer::get($this->targetName.'_revision'));
+  }
+
+ /**
+  * @see Doctrine_Migration
+  */
+  public function loadMigrationClassesFromDirectory()
+  {
+    $classes = get_declared_classes();
+
+    foreach ((array)$this->_migrationClassesDirectory as $dir)
+    {
+      $files = sfFinder::type('file')->name('*.php')->in($dir);
+      $iterator = new CompareMigrateDirectoryVersionFilterIterator($files, str_replace('-dev', '', OPENPNE_VERSION));
+      foreach ($iterator as $file)
+      {
+        if (!in_array(basename($file), $this->_loadedMigrations))
+        {
+          require_once $file;
+
+          $requiredClass = array_diff(get_declared_classes(), $classes);
+          $requiredClass = end($requiredClass);
+          if ($requiredClass)
+          {
+            $this->_loadedMigrations[$requiredClass] = basename($file);
+          }
+        }
+      }
+    }
+  }
+}
+
+class CompareMigrateDirectoryVersionFilterIterator extends FilterIterator
+{
+  protected $limitVersion;
+
+  public function __construct(array $fileList, $limitVersion)
+  {
+    $this->limitVersion = $limitVersion;
+
+    return parent::__construct(new ArrayIterator($fileList));
+  }
+
+  public function accept()
+  {
+    $version = basename(dirname($this->current()));
+    return version_compare($version, $this->limitVersion, '<=');
   }
 }
---
+++ lib/task/openpneMigrateTask.class.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+class openpneMigrateTask extends sfPropelBaseTask
+{
+  protected function configure()
+  {
+    $this->namespace        = 'openpne';
+    $this->name             = 'migrate';
+
+    require sfConfig::get('sf_data_dir').'/version.php';
+
+    $this->addArguments(array(
+      new sfCommandArgument('name', sfCommandArgument::OPTIONAL, 'The plugin name or "OpenPNE"', 'OpenPNE'),
+    ));
+
+    $this->addOptions(array(
+      new sfCommandOption('openpne-version', 'v', sfCommandOption::PARAMETER_OPTIONAL, 'To version', OPENPNE_VERSION),
+      new sfCommandOption('revision', 'r', sfCommandOption::PARAMETER_OPTIONAL, 'To revision'),
+      new sfCommandOption('no-build-model', null, sfCommandOption::PARAMETER_NONE, 'Do not build model classes'),
+    ));
+
+    $this->briefDescription = 'update OpenPNE';
+    $this->detailedDescription = <<<EOF
+The [openpne:migrade|INFO] task upgrades or downgrades OpenPNE and/or plugin.
+Call it with:
+
+  [./symfony openpne:migrade|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $migration = new opMigration($this->dispatcher, $databaseManager);
+    $migration->migrate();
+
+    if (!$options['no-build-model'])
+    {
+      $this->buildModel();
+    }
+  }
+
+  protected function buildModel()
+  {
+    $task = new sfPropelBuildModelTask($this->dispatcher, $this->formatter);
+    $task->run();
+    $task = new sfPropelBuildFormsTask($this->dispatcher, $this->formatter);
+    $task->run();
+    $task = new sfPropelBuildFiltersTask($this->dispatcher, $this->formatter);
+    $task->run();
+  }
+}
