c9e0eaa6471ab96c7edd248ef1d6b13e75d02b93
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-23 11:04:33 +0000

    #3502:changed the openpne:migrate task to migrate OpenPNE and all plugins when the target option isn't specified. This task warns when the when the user specifies the revision / the version option without the target option now
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10833 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/task/openpneMigrateTask.class.php
+++ lib/task/openpneMigrateTask.class.php
@@ -17,11 +17,8 @@ class openpneMigrateTask extends sfPropelBaseTask
 
     require sfConfig::get('sf_data_dir').'/version.php';
 
-    $this->addArguments(array(
-      new sfCommandArgument('name', sfCommandArgument::OPTIONAL, 'The plugin name or "OpenPNE"', 'OpenPNE'),
-    ));
-
     $this->addOptions(array(
+      new sfCommandOption('target', null, sfCommandOption::PARAMETER_OPTIONAL, 'The target of migration'),
       new sfCommandOption('to-version', 'v', sfCommandOption::PARAMETER_OPTIONAL, 'To version'),
       new sfCommandOption('to-revision', 'r', sfCommandOption::PARAMETER_OPTIONAL, 'To revision'),
       new sfCommandOption('no-build-model', null, sfCommandOption::PARAMETER_NONE, 'Do not build model classes'),
@@ -43,14 +40,45 @@ EOF;
       throw new sfCommandException("This task requires sfDoctrinePlugin.\nPlease enable the plugin by your config/ProjectConfiguration.class.php");
     }
 
+    if (!$options['target'] && ($options['to-version'] || $options['to-revision']))
+    {
+      throw new sfCommandException("You can't specify the to-version option or the to-revision option without the target option");
+    }
+
     if (!$options['no-build-model'])
     {
       $this->buildModel();
     }
 
     $databaseManager = new sfDatabaseManager($this->configuration);
-    $migration = new opMigration($this->dispatcher, $databaseManager, $arguments['name'], null, $options['to-version']);
-    $migration->migrate();
+
+    if (!empty($options['target']))
+    {
+      $targets = array($options['target']);
+    }
+    else
+    {
+      $targets = array_merge(array('OpenPNE'), $this->getEnabledOpenPNEPlugin());
+    }
+
+    foreach ($targets as $target)
+    {
+      $migration = new opMigration($this->dispatcher, $databaseManager, $target, null, $options['to-version']);
+
+      try
+      {
+        $migration->migrate();
+      }
+      catch (Doctrine_Migration_Exception $e)
+      {
+        if (0 !== strpos($e->getMessage(), 'Already at version #'))
+        {
+          throw $e;
+        }
+      }
+
+      $this->logSection('migrate', sprintf('%s is now at revision %d.', $target, $migration->getCurrentVersion()));
+    }
   }
 
   protected function buildModel()
@@ -63,5 +91,21 @@ EOF;
     $task->run();
     $task = new sfCacheClearTask($this->dispatcher, $this->formatter);
     $task->run();
+  }
+
+  protected function getEnabledOpenPNEPlugin()
+  {
+    $list = $this->configuration->getPlugins();
+    $result = array();
+
+    foreach ($list as $value)
+    {
+      if (!strncmp($value, 'op', 2))
+      {
+        $result[] = $value;
+      }
+    }
+
+    return $result;
   }
 }
