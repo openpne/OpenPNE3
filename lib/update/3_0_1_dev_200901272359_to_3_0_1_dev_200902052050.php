60fb8bfcb063fb77c8fd651dd7810767fa65e93a
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-24 12:09:11 +0000

    #3502:reverted lib/update to r10852
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10853 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ lib/update/3_0_1_dev_200901272359_to_3_0_1_dev_200902052050.php
@@ -0,0 +1,19 @@
+<?php
+
+class opUpdate_3_0_1_dev_200901272359_to_3_0_1_dev_200902052050 extends opUpdate
+{
+  public function update()
+  {
+    $this->createTable(
+      'invitelist',
+      array(
+        'id' => array('type' => 'integer', 'primary' => true, 'autoincrement' => true),
+        'created_at' => array('type' => 'timestamp', 'notnull' => true),
+        'member_id_from' => array('type' => 'integer', 'size' => 4, 'foreign' => 'id', 'foreignAlias' => 'member', 'onDelete' => 'CASCADE'),
+        'member_id_to' => array('type' => 'integer', 'size' => 4, 'foreign' => 'id', 'foreignAlias' => 'member'),
+        'mail_address' => array('type' => 'string', 'size' => 128)
+      )
+    );
+  }
+}
+
---
+++ lib/update/opUpdate.class.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * opUpdate provides updating
+ *
+ * @package    OpenPNE
+ * @subpackage update
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ */
+abstract class opUpdate extends Doctrine_Migration
+{
+  protected
+    $dispatcher = null,
+    $formatter = null,
+    $dbManager = null,
+    $database = null,
+    $connection = null,
+    $doctrineProcess = null;
+
+  public function __construct($dispatcher, $dbManager, $name = '')
+  {
+    $this->dispatcher = $dispatcher;
+
+    $this->dbManager = $dbManager;
+    if (!$name)
+    {
+      $names = $this->dbManager->getNames();
+      $name = array_shift($names);
+    }
+    $this->database = $this->dbManager->getDatabase($name);
+
+    $params = $this->database->getParameterHolder()->getAll();
+    unset($params['classname']);
+    $doctrine = new sfDoctrineDatabase($params);
+
+    $this->connection = $doctrine->getDoctrineConnection();
+    $this->doctrineProcess = new opUpdateDoctrineMigrationProcess($this->connection);
+
+
+    $this->formatter = new sfFormatter();
+    if ('cli' === PHP_SAPI)
+    {
+      $this->formatter = new sfAnsiColorFormatter();
+    }
+
+  }
+
+  abstract public function update();
+
+  public function preUpdate()
+  {
+  }
+
+  public function postUpdate()
+  {
+  }
+
+  public function doUpdate()
+  {
+    $this->preUpdate();
+    $this->update();
+    $this->postUpdate();
+
+    foreach ($this->_changes as $type => $changes)
+    {
+      if ($changes)
+      {
+        $method = 'process'.Doctrine_Inflector::classify($type);
+        $this->doctrineProcess->$method($changes);
+      }
+    }
+  }
+}
