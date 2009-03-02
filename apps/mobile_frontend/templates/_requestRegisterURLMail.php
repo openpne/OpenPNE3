26c7b71e486d800cf2d0dd482a810ea768c0655c
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-02 03:04:23 +0000

    #3625:fixed changed the message to raw data
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10918 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/templates/_requestRegisterURLMail.php
+++ apps/mobile_frontend/templates/_requestRegisterURLMail.php
@@ -1,19 +1,19 @@
-<?php $sns_name = $op_config['sns_name'] ?>
-<?php echo __('Hello! This is information from %1%.', array('%1%' => $sns_name)) ?>
+<?php $rawMessage = $sf_data->getRaw('message') ?>
+<?php $snsName = $op_config['sns_name'] ?>
+<?php echo __('Hello! This is information from %1%.', array('%1%' => $snsName)) ?>
 
 <?php if (isset($name)): ?>
 
-<?php echo __('%1% is inviting in %2% you.', array('%1%' => $name, '%2%' => $sns_name)) ?>
+<?php echo __('%1% is inviting in %2% you.', array('%1%' => $name, '%2%' => $snsName)) ?>
 
-<?php if (strlen($message)): ?>
+<?php if (strlen($rawMessage)): ?>
 
 <?php echo __('---------- The message to you from < %1% > ----------', array('%1%' => $name)) ?>
 
 
 <?php echo __('---------------------------------------------------') ?>
 
-
-<?php echo $message ?>
+<?php echo $rawMessage ?>
 
 
 <?php echo __('---------------------------------------------------') ?>
@@ -21,11 +21,11 @@
 <?php endif ?>
 <?php endif ?>
 
-<?php echo __('If you register(free) of member by the following URL,%br%you can participate in %1%.', array('%1%' => $sns_name, '%br%' => "\n")) ?>
+<?php echo __('If you register(free) of member by the following URL,%br%you can participate in %1%.', array('%1%' => $snsName, '%br%' => "\n")) ?>
 
 
 
-<?php echo __('* Participate in %1%', array('%1%' => $sns_name)) ?>
+<?php echo __('* Participate in %1%', array('%1%' => $snsName)) ?>
 
 <?php if ($isMobile) : ?>
 <?php echo app_url_for('mobile_frontend', sprintf('opAuthMailAddress/register?token=%s&authMode=%s', $token, $authMode), true) ?>
