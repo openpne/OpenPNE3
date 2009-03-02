53c5170478683d801c8272ff3e39c5dff3f91eda
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-02 06:48:04 +0000

    #3596:fixed the memberImageBox parts always display "no image" images
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@10921 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_partsMemberImageBox.php
+++ apps/pc_frontend/templates/_partsMemberImageBox.php
@@ -2,8 +2,8 @@
 
 <p class="photo">
 <?php $imgParam = array('size' => '180x180', 'alt' => $options->object->getName()) ?>
-<?php if ($options->image): ?>
-<?php echo image_tag_sf_image($options->image, $imgParam) ?>
+<?php if ($options->object): ?>
+<?php echo image_tag_sf_image($options->object->getImageFileName(), $imgParam) ?>
 <?php else: ?>
 <?php echo image_tag('no_image.gif', $imgParam) ?>
 <?php endif; ?>
