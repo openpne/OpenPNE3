26ff2a48a9f051dff61ca0abddaeaa662bab0fc9
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-16 05:39:38 +0000

    #3706:fixed MemberImagesBox parts for max image filesize
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@11022 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_partsMemberImagesBox.php
+++ apps/pc_frontend/templates/_partsMemberImagesBox.php
@@ -33,7 +33,7 @@
 </form>
 <ul>
 <li><?php echo __('You can upload 3 photos.') ?></li>
-<li><?php echo __('Please make it to GIF･JPEG･PNG within 300KB.') ?></li>
+<li><?php echo __('Please make it to GIF･JPEG･PNG within %max_size% bytes.', array('%max_size%' =>  $op_config['image_max_filesize'])) ?></li>
 <li><?php echo __('Photograph that hits violation of copyright and portrait right and violence and obscene photograph and other members are seen and revolted a press ban. Please publish by the self-responsibility.') ?></li>
 </ul>
 </div>
