418d94fc00b0adeb4501fdff2121f42e19dcd116
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-17 11:38:18 +0000

    added op_within_page_link() to the opUtil helper
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@11034 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/helper/opUtilHelper.php
+++ lib/helper/opUtilHelper.php
@@ -464,4 +464,23 @@ function op_truncate_callback($string, $width, $etc = '')
   return $string;
 }
 
+function op_within_page_link($marker = '▼')
+{
+  static $n = 0;
+
+  $options = array();
+  if ($n)
+  {
+    $options['name'] = sprintf('a%d', $n);
+  }
+  if ($marker)
+  {
+    $options['href'] = sprintf('#a%d', $n+1);
+  }
+
+  $n++;
+
+  return content_tag('a', $marker, $options);
+}
+
 ?>
