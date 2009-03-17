7c9c4cb177dea5aa9936d239f18c12affc3796b2
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-17 13:16:39 +0000

    added ButtonBox parts to the mobile_frontend
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@11041 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/mobile_frontend/templates/_partsButtonBox.php
@@ -0,0 +1,15 @@
+<?php
+$url = sfContext::getInstance()->getRouting()->getCurrentInternalUri();
+$options->setDefault('button', __('Yes'));
+$options->setDefault('url', url_for($url));
+$options->setDefault('method', 'post');
+?>
+
+<?php if(isset($options['body'])): ?>
+<?php echo $options['body'] ?>
+<?php endif; ?>
+<center>
+<form action="<?php echo $options['url'] ?>" method="<?php echo $options['method'] ?>">
+<input type="submit" value="<?php echo $options['button'] ?>">
+</form>
+</center>
