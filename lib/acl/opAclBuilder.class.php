ec975bb2372587fbe2498fadd01fff6acbd8b653
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-05-02 19:07:19 +0000

    #3908:added the opAclBuilder class
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@11572 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ lib/acl/opAclBuilder.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * opAclBuilder builds instances of the Zend_Acl by the specified conditions
+ *
+ * @package    OpenPNE
+ * @subpackage acl
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ */
+abstract class opAclBuilder
+{
+}
