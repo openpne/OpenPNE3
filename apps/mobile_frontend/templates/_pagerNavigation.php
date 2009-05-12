1e73fb04869ad34f3280a6732cf0ca0cfd7603de
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-05-12 08:55:53 +0000

    fixed pager template for the mobile (3.0.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/branches/STABLE/3.0.x@11617 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/templates/_pagerNavigation.php
+++ apps/mobile_frontend/templates/_pagerNavigation.php
@@ -9,13 +9,14 @@ $options->setDefault('next_text', __('Next', array(), 'pager'));
 <?php if ($options['is_total'] || $pager->haveToPaginate()): ?>
 <center>
 <?php if ($pager->getPreviousPage() != $pager->getPage()): ?>
-<?php echo link_to($options['prev_text'], sprintf($sf_data->getRaw('link_to'), $pager->getPreviousPage()), array('query_string' => $options['query_string'])) ?>&nbsp;
+<?php echo link_to("[i:128]".$options['prev_text'], sprintf($sf_data->getRaw('link_to'), $pager->getPreviousPage()), array('query_string' => $options['query_string'], 'accesskey' => 4)) ?>
 <?php endif; ?>
 <?php if ($options['is_total']): ?>
 <?php op_include_pager_total($pager) ?>
 <?php endif; ?>
 <?php if ($pager->getNextPage() != $pager->getPage()): ?>
-&nbsp;<?php echo link_to($options['next_text'], sprintf($sf_data->getRaw('link_to'), $pager->getNextPage()), array('query_string' => $options['query_string'])) ?>
+<?php if ($pager->getPreviousPage() != $pager->getPage()): ?>&nbsp;<?php endif; ?>
+<?php echo link_to("[i:130]".$options['next_text'], sprintf($sf_data->getRaw('link_to'), $pager->getNextPage()), array('query_string' => $options['query_string'], 'accesskey' => 6)) ?>
 <?php endif; ?>
 </center>
 <?php endif; ?>
--- apps/mobile_frontend/templates/_pagerReversibleNavigation.php
+++ apps/mobile_frontend/templates/_pagerReversibleNavigation.php
@@ -9,13 +9,14 @@ $options->setDefault('next_text', __('Next', array(), 'pager'));
 <?php if ($options['is_total'] || $pager->haveToPaginate()): ?>
 <center>
 <?php if ($pager->hasOlderPage()): ?>
-<?php echo link_to($options['prev_text'], sprintf($sf_data->getRaw('link_to'), $pager->getOlderPage()), array('query_string' => $options['query_string'])) ?>&nbsp;
+<?php echo link_to("[i:128]".$options['prev_text'], sprintf($sf_data->getRaw('link_to'), $pager->getOlderPage()), array('query_string' => $options['query_string'], 'accesskey' => 4)) ?>
 <?php endif; ?>
 <?php if ($options['is_total']): ?>
 <?php op_include_pager_total($pager) ?>
 <?php endif; ?>
 <?php if ($pager->hasNewerPage()): ?>
-&nbsp;<?php echo link_to($options['next_text'], sprintf($sf_data->getRaw('link_to'), $pager->getNewerPage()), array('query_string' => $options['query_string'])) ?>
+<?php if ($pager->hasOlderPage()): ?>&nbsp;<?php endif; ?>
+<?php echo link_to("[i:130]".$options['next_text'], sprintf($sf_data->getRaw('link_to'), $pager->getNewerPage()), array('query_string' => $options['query_string'], 'accesskey' => 6)) ?>
 <?php endif; ?>
 </center>
 <?php endif; ?>
