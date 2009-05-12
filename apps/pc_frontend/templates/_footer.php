f580fbf9e2edf680402bc15e2037be7b317ee142
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-05-12 05:58:41 +0000

    #3915:fixed footer template of pc_frontend
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@11616 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_footer.php
+++ apps/pc_frontend/templates/_footer.php
@@ -1,8 +1,8 @@
 <p>
 Powered by <a href="http://www.openpne.jp/" target="_blank">OpenPNE</a>
 <?php if (opToolkit::isSecurePage()) : ?>
-<?php echo SnsConfigPeer::get('footer_after'); ?>
+<?php echo Doctrine::getTable('SnsConfig')->get('footer_after'); ?>
 <?php else: ?>
-<?php echo SnsConfigPeer::get('footer_before'); ?>
+<?php echo Doctrine::getTable('SnsConfig')->get('footer_before'); ?>
 <?php endif; ?>
 </p>
