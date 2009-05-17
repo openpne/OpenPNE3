aa7fa597b02db841da0b51fc80eeadc61e2536a2
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-05-17 17:40:55 +0000

    #3271:removed changes from r11599 because it doesn't work correctly
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@11653 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/templates/_footer.php
+++ apps/pc_frontend/templates/_footer.php
@@ -1,8 +1,1 @@
-<p>
-Powered by <a href="http://www.openpne.jp/" target="_blank">OpenPNE</a>
-<?php if (opToolkit::isSecurePage()) : ?>
-<?php echo Doctrine::getTable('SnsConfig')->get('footer_after'); ?>
-<?php else: ?>
-<?php echo Doctrine::getTable('SnsConfig')->get('footer_before'); ?>
-<?php endif; ?>
-</p>
+<p>Powered by <a href="http://www.openpne.jp/" target="_blank">OpenPNE</a></p>
