5b00a3a921f60993429300772bf452fda62f0a19
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-06-03 10:01:24 +0000

    #3975:fixed _sideMenu template on the pc_backend
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@11836 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_backend/modules/default/templates/_sideMenu.php
+++ apps/pc_backend/modules/default/templates/_sideMenu.php
@@ -1,3 +1,12 @@
 <?php foreach ($navs as $nav): ?>
-<li><?php echo link_to($nav->getCaption(), $nav->getUri()) ?></li>
+<li><?php 
+try
+{
+  echo link_to($nav->getCaption(), $nav->getUri());
+}
+catch(Exception $e)
+{
+  echo $nav->getCaption();
+}
+?></li>
 <?php endforeach; ?>
