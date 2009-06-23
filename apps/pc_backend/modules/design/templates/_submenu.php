893eefad964e1de5c3f032f0a599c1becc8af9dd
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-06-23 12:31:21 +0000

    #3271:reverted to r11597 (3.0.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/branches/STABLE/3.0.x@11993 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_backend/modules/design/templates/_submenu.php
+++ apps/pc_backend/modules/design/templates/_submenu.php
@@ -1,6 +1,5 @@
 <li><?php echo link_to(__('ホーム画面レイアウト設定'), 'design/layout') ?></li>
 <li><?php echo link_to(__('ガジェット設定'), 'design/gadget') ?></li>
 <li><?php echo link_to(__('ナビ設定'), 'design/navigation') ?></li>
-<li><?php echo link_to(__('フッターデザイン設定'), 'design/footer?type=before') ?></li>
 <li><?php echo link_to(__('携帯版ナビ設定'), 'design/navigation?app=mobile') ?></li>
 <li><?php echo link_to(__('バナー設定'), 'design/banner') ?></li>
