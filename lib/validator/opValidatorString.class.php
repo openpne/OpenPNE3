849d66c33b0f39bfde7cb332573a09cfc7441056
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-06 13:34:56 +0000

    #4053:added opValidatorString to trim double byte spaces
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12142 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ lib/validator/opValidatorString.class.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * opValidatorString validates a string.
+ * It support to trim double byte spaces.
+ * 
+ * @package    OpenPNE
+ * @subpackage validator
+ * @author     Shogo Kawahara <kawahara@tejimaya.net>
+ */
+class opValidatorString extends sfValidatorString
+{
+  public function configure($options = array(), $messages = array())
+  {
+    parent::configure($options, $messages);
+
+    $this->addOption('ltrim', false);
+    $this->addOption('rtrim', false);
+  }
+
+  public function clean($value)
+  {
+    $clean = $value;
+
+    if (is_string($clean))
+    {
+      if ($this->options['trim'])
+      {
+        $clean = preg_replace('/^[\s　]+/u', '', $clean);
+        $clean = preg_replace('/[\s　]+$/u', '', $clean);
+      }
+      if ($this->options['ltrim'])
+      {
+        $clean = preg_replace('/^[\s　]+/u', '', $clean);
+      }
+      if ($this->options['rtrim'])
+      {
+        $clean = preg_replace('/[\s　]+$/u', '', $clean);
+      }
+    }
+
+    if ($this->isEmpty($clean))
+    {
+      if ($this->options['required'])
+      {
+        throw new sfValidatorError($this, 'required');
+      }
+
+      return $this->getEmptyValue();
+    }
+
+    return $this->doClean($clean);
+  }
+}
