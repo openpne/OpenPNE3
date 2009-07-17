e462c5eb5024d40684f527af3a4a591f901df823
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-17 11:55:19 +0000

    #4130:fixed friend/showImage that calls undefined method
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12298 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/modules/friend/templates/showImageSuccess.php
+++ apps/mobile_frontend/modules/friend/templates/showImageSuccess.php
@@ -1,13 +1,12 @@
 <?php op_mobile_page_title($member->getName(), __('Photo')) ?>
 <center>
-<?php $images = $member->getMemberImages() ?>
+<?php $images = $member->getMemberImage() ?>
 <?php for ($i = 0; $i < 3 && $i < count($images); $i++) : ?>
 <?php $image = $images[$i] ?>
 <?php echo image_tag_sf_image($image->getFile(), array('size' => '120x120', 'format' => 'jpg')) ?><br>
 <?php echo '['.link_to(__('Expansion'), sf_image_path($image->getFile(), array('size' => '320x320', 'format' => 'jpg'))).']' ?><br><br>
 <?php endfor; ?>
 </center>
-<hr color="<?php echo $op_color["core_color_11"] ?>">
 <?php slot('op_mobile_footer_menu') ?>
 <?php echo link_to(__("%1%'s Profile", array('%1%' => $member->getName())), 'member/profile?id='.$member->getId()); ?>
 <?php end_slot(); ?>
