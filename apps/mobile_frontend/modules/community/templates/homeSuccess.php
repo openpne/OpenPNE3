941c8e30541bd496ebdff2f70536db86ae704f3a
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-21 01:27:32 +0000

    #4087 reset a change because this version wont be added the ability of posting from mobile by mail
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/branches/STABLE/3.0.x@12329 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/modules/community/templates/homeSuccess.php
+++ apps/mobile_frontend/modules/community/templates/homeSuccess.php
@@ -8,9 +8,6 @@
 
 <tr><td align="center" width="50%" valign="top">
 <?php echo image_tag_sf_image($community->getImageFileName(), array('size' => '120x120', 'format' => 'jpg')) ?>
-<?php if ($isAdmin) : ?>
-<br><?php echo link_to(__('Edit Photo'), 'community/edit?id='.$community->getId()) ?>
-<?php endif; ?>
 </td>
 
 <td valign="top">
