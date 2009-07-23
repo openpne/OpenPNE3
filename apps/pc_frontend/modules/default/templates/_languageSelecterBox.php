e4fb6819cd1771c4df10b0cf401123cf3d6a7629
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-23 14:22:23 +0000

    #3489:fixed opWidgetFormInputHiddenNextUri doesn't set value correctly (3.0.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/branches/STABLE/3.0.x@12376 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/default/templates/_languageSelecterBox.php
+++ apps/pc_frontend/modules/default/templates/_languageSelecterBox.php
@@ -1,4 +1,4 @@
-<form action="<?php echo url_for('member/changeLanguage') ?>" method="post">
+<?php echo $form->renderFormTag(url_for('member/changeLanguage')) ?>
 <?php echo $form['culture']->renderLabel() ?>:
 <?php echo $form['culture']->render(array('onchange' => 'submit(this.form)')) ?>
 <?php echo $form->renderHiddenFields() ?>
