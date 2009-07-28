cf39dbec7c6a122cb580bfb8610d43b1c843f892
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-28 13:26:41 +0000

    fixed CommunityMemberTest.php for adjusting Doctrine
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12442 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/unit/model/CommunityMemberTest.php
+++ test/unit/model/CommunityMemberTest.php
@@ -5,63 +5,65 @@ include_once dirname(__FILE__) . '/../../bootstrap/database.php';
 
 $t = new lime_test(null, new lime_output_color());
 
+$communityMemberTable = Doctrine::getTable('CommunityMember');
+
 //------------------------------------------------------------
 
-$t->diag('CommunityMemberPeer::retrieveByMemberIdAndCommunityId()');
-$t->isa_ok(CommunityMemberPeer::retrieveByMemberIdAndCommunityId(1, 1), 'CommunityMember',
+$t->diag('CommunityMemberTable::retrieveByMemberIdAndCommunityId()');
+$t->isa_ok($communityMemberTable->retrieveByMemberIdAndCommunityId(1, 1), 'CommunityMember',
   'retrieveByMemberIdAndCommunityId() returns a CommunityMember if member joins community');
-$t->cmp_ok(CommunityMemberPeer::retrieveByMemberIdAndCommunityId(1, 2), '===', NULL,
+$t->cmp_ok($communityMemberTable->retrieveByMemberIdAndCommunityId(1, 2), '===', false,
   'retrieveByMemberIdAndCommunityId() returns NULL if member does not join community');
-$t->cmp_ok(CommunityMemberPeer::retrieveByMemberIdAndCommunityId(999, 1), '===', NULL,
+$t->cmp_ok($communityMemberTable->retrieveByMemberIdAndCommunityId(999, 1), '===', false,
   'retrieveByMemberIdAndCommunityId() returns NULL if member does not exist');
-$t->cmp_ok(CommunityMemberPeer::retrieveByMemberIdAndCommunityId(1, 999), '===', NULL,
+$t->cmp_ok($communityMemberTable->retrieveByMemberIdAndCommunityId(1, 999), '===', false,
   'retrieveByMemberIdAndCommunityId() returns NULL if community does not exist');
-$t->cmp_ok(CommunityMemberPeer::retrieveByMemberIdAndCommunityId(999, 999), '===', NULL,
+$t->cmp_ok($communityMemberTable->retrieveByMemberIdAndCommunityId(999, 999), '===', false,
   'retrieveByMemberIdAndCommunityId() returns NULL if member and community do not exist');
 
 //------------------------------------------------------------
 
-$t->diag('CommunityMemberPeer::isMember()');
-$t->cmp_ok(CommunityMemberPeer::isMember(1, 1), '===', true,
+$t->diag('CommunityMemberTable::isMember()');
+$t->cmp_ok($communityMemberTable->isMember(1, 1), '===', true,
   'isMember() returns true if member joins community');
-$t->cmp_ok(CommunityMemberPeer::isMember(1, 2), '===', false,
+$t->cmp_ok($communityMemberTable->isMember(1, 2), '===', false,
   'isMember() returns false if member does not join community');
-$t->cmp_ok(CommunityMemberPeer::isMember(999, 1), '===', false,
+$t->cmp_ok($communityMemberTable->isMember(999, 1), '===', false,
   'isMember() returns false if member does not exist');
-$t->cmp_ok(CommunityMemberPeer::isMember(1, 999), '===', false,
+$t->cmp_ok($communityMemberTable->isMember(1, 999), '===', false,
   'isMember() returns false if community does not exist');
-$t->cmp_ok(CommunityMemberPeer::isMember(999, 999), '===', false,
+$t->cmp_ok($communityMemberTable->isMember(999, 999), '===', false,
   'isMember() returns false if member and community do not exist');
 
 //------------------------------------------------------------
 
-$t->diag('CommunityMemberPeer::isAdmin()');
-$t->cmp_ok(CommunityMemberPeer::isAdmin(1, 1), '===', true,
+$t->diag('CommunityMemberTable::isAdmin()');
+$t->cmp_ok($communityMemberTable->isAdmin(1, 1), '===', true,
   'isAdmin() returns true if member joins community and position is admin');
-$t->cmp_ok(CommunityMemberPeer::isAdmin(2, 1), '===', false,
+$t->cmp_ok($communityMemberTable->isAdmin(2, 1), '===', false,
   'isAdmin() returns false if member joins community and position is not admin');
-$t->cmp_ok(CommunityMemberPeer::isAdmin(1, 2), '===', false,
+$t->cmp_ok($communityMemberTable->isAdmin(1, 2), '===', false,
   'isAdmin() returns false if member does not join community');
-$t->cmp_ok(CommunityMemberPeer::isAdmin(999, 1), '===', false,
+$t->cmp_ok($communityMemberTable->isAdmin(999, 1), '===', false,
   'isAdmin() returns false if member does not exist');
-$t->cmp_ok(CommunityMemberPeer::isAdmin(1, 999), '===', false,
+$t->cmp_ok($communityMemberTable->isAdmin(1, 999), '===', false,
   'isAdmin() returns false if community does not exist');
-$t->cmp_ok(CommunityMemberPeer::isAdmin(999, 999), '===', false,
+$t->cmp_ok($communityMemberTable->isAdmin(999, 999), '===', false,
   'isAdmin() returns false if member and community do not exist');
 
 //------------------------------------------------------------
 
-$t->diag('CommunityMemberPeer::join()');
+$t->diag('CommunityMemberTable::join()');
 
-$t->cmp_ok(CommunityMemberPeer::isMember(1, 2), '===', false, 'isMember() returns false');
-CommunityMemberPeer::join(1, 2);
-$t->cmp_ok(CommunityMemberPeer::isMember(1, 2), '===', true, 'isMember() returns true');
-$t->cmp_ok(CommunityMemberPeer::isAdmin(1, 2), '===', false, 'isAdmin() returns false');
+$t->cmp_ok($communityMemberTable->isMember(1, 2), '===', false, 'isMember() returns false');
+$communityMemberTable->join(1, 2);
+$t->cmp_ok($communityMemberTable->isMember(1, 2), '===', true, 'isMember() returns true');
+$t->cmp_ok($communityMemberTable->isAdmin(1, 2), '===', false, 'isAdmin() returns false');
 
 $message = 'join() throws exception if member already joins community';
-$t->cmp_ok(CommunityMemberPeer::isMember(1, 1), '===', true, 'isMember() returns true');
+$t->cmp_ok($communityMemberTable->isMember(1, 1), '===', true, 'isMember() returns true');
 try {
-  CommunityMemberPeer::join(1, 1);
+  $communityMemberTable->join(1, 1);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -69,7 +71,7 @@ try {
 
 $message = 'join() throws exception if member does not exist';
 try {
-  CommunityMemberPeer::join(999, 1);
+  $communityMemberTable->join(999, 1);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -77,7 +79,7 @@ try {
 
 $message = 'join() throws exception if community does not exist';
 try {
-  CommunityMemberPeer::join(1, 999);
+  $communityMemberTable->join(1, 999);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -85,7 +87,7 @@ try {
 
 $message = 'join() throws exception if member and community do not exist';
 try {
-  CommunityMemberPeer::join(999, 999);
+  $communityMemberTable->join(999, 999);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -93,16 +95,16 @@ try {
 
 //------------------------------------------------------------
 
-$t->diag('CommunityMemberPeer::quit()');
+$t->diag('CommunityMemberTable::quit()');
 
-$t->cmp_ok(CommunityMemberPeer::isMember(2, 1), '===', true, 'isMember() returns true');
-CommunityMemberPeer::quit(2, 1);
-$t->cmp_ok(CommunityMemberPeer::isMember(2, 1), '===', false, 'isMember() returns false');
+$t->cmp_ok($communityMemberTable->isMember(2, 1), '===', true, 'isMember() returns true');
+$communityMemberTable->quit(2, 1);
+$t->cmp_ok($communityMemberTable->isMember(2, 1), '===', false, 'isMember() returns false');
 
 $message = 'quit() throws exception if member is community admin';
-$t->cmp_ok(CommunityMemberPeer::isAdmin(1, 1), '===', true, 'isAdmin() returns true');
+$t->cmp_ok($communityMemberTable->isAdmin(1, 1), '===', true, 'isAdmin() returns true');
 try {
-  CommunityMemberPeer::quit(1, 1);
+  $communityMemberTable->quit(1, 1);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -110,7 +112,7 @@ try {
 
 $message = 'quit() throws exception if member does not join community';
 try {
-  CommunityMemberPeer::quit(3, 1);
+  $communityMemberTable->quit(3, 1);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -118,7 +120,7 @@ try {
 
 $message = 'quit() throws exception if member does not exist';
 try {
-  CommunityMemberPeer::quit(999, 1);
+  $communityMemberTable->quit(999, 1);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -126,7 +128,7 @@ try {
 
 $message = 'quit() throws exception if community does not exist';
 try {
-  CommunityMemberPeer::quit(1, 999);
+  $communityMemberTable->quit(1, 999);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
@@ -134,7 +136,7 @@ try {
 
 $message = 'quit() throws exception if member and community do not exist';
 try {
-  CommunityMemberPeer::quit(999, 999);
+  $communityMemberTable->quit(999, 999);
   $t->fail($message);
 } catch (Exception $e) {
   $t->pass($message);
