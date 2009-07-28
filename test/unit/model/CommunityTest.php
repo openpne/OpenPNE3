0d1125d9e02709c3c0e1474bdc1d98c6e916ccc4
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-28 12:32:53 +0000

    fixed the unit tests and the fixture of community for adjusting Doctrine. and add some tests
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12439 8bbc783d-d51a-0410-adab-8fa69d43f207

--- test/unit/model/CommunityTest.php
+++ test/unit/model/CommunityTest.php
@@ -3,26 +3,45 @@
 include_once dirname(__FILE__) . '/../../bootstrap/unit.php';
 include dirname(__FILE__) . '/../../bootstrap/database.php';
 
-$t = new lime_test(null, new lime_output_color());
+$t = new lime_test(12, new lime_output_color());
 
 //------------------------------------------------------------
 
-$t->diag('CommunityPeer::getJoinCommunityListPager()');
-$pager = CommunityPeer::getJoinCommunityListPager(1);
-$t->isa_ok($pager, 'sfPropelPager', 'getJoinCommunityListPager() returns a sfPropelPager');
-$t->is($pager->getNbResults(), 1, 'getNbResults() returns 1');
+$t->diag('CommunityTable::retrievesByMemberId()');
+$communities = Doctrine::getTable('Community')->retrievesByMemberId(1);
+$t->is(count($communities), 3, 'retrievesByMemberId returns 3 communities');
 
-$pager = CommunityPeer::getJoinCommunityListPager(999);
-$t->isa_ok($pager, 'sfPropelPager', 'getJoinCommunityListPager() returns a sfPropelPager');
+//------------------------------------------------------------
+
+$t->diag('CommunityTable::getJoinCommunityListPager()');
+$pager = Doctrine::getTable('Community')->getJoinCommunityListPager(1);
+$t->isa_ok($pager, 'sfDoctrinePager', 'getJoinCommunityListPager() returns a sfDoctrinePager');
+$t->is($pager->getNbResults(), 3, 'getNbResults() returns 3');
+
+$pager = Doctrine::getTable('Community')->getJoinCommunityListPager(999);
+$t->isa_ok($pager, 'sfDoctrinePager', 'getJoinCommunityListPager() returns a sfDoctrinePager');
 $t->is($pager->getNbResults(), 0, 'getNbResults() returns 0');
 
 //------------------------------------------------------------
 
-$t->diag('CommunityPeer::getCommunityMemberListPager()');
-$pager = CommunityPeer::getCommunityMemberListPager(1);
-$t->isa_ok($pager, 'sfPropelPager', 'getCommunityMemberListPager() returns a sfPropelPager');
+$t->diag('CommunityTable::getCommunityMemberListPager()');
+$pager = Doctrine::getTable('Community')->getCommunityMemberListPager(1);
+$t->isa_ok($pager, 'sfDoctrinePager', 'getCommunityMemberListPager() returns a sfDoctrinePager');
 $t->is($pager->getNbResults(), 2, 'getNbResults() returns 2');
 
-$pager = CommunityPeer::getCommunityMemberListPager(999);
-$t->isa_ok($pager, 'sfPropelPager', 'getCommunityMemberListPager() returns a sfPropelPager');
+$pager = Doctrine::getTable('Community')->getCommunityMemberListPager(999);
+$t->isa_ok($pager, 'sfDoctrinePager', 'getCommunityMemberListPager() returns a sfDoctrinePager');
 $t->is($pager->getNbResults(), 0, 'getNbResults() returns 0');
+
+//------------------------------------------------------------
+
+$t->diag('CommunityTable::getIdsByMemberId()');
+$communityIds = Doctrine::getTable('Community')->getIdsByMemberId(1);
+$t->is(count($communityIds), 3, 'getIdsByMemberId() returns 3 ids');
+$t->is($communityIds, array(1, 3, 4), 'getIdsByMemberId() returns array(1, 3, 4)');
+
+//------------------------------------------------------------
+
+$t->diag('CommunityTable::getDefaultCommunities()');
+$communities = Doctrine::getTable('Community')->getDefaultCommunities();
+$t->is(count($communities), 2, 'getDefaultCommunities() returns 2 communities');
