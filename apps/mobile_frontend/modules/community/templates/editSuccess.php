bb734539f2bb4007c9f24b36739c5b8b89dae06e
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-31 09:45:35 +0000

    #4200: deleted the community top link from community create form
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12493 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/mobile_frontend/modules/community/templates/editSuccess.php
+++ apps/mobile_frontend/modules/community/templates/editSuccess.php
@@ -21,9 +21,8 @@ else
   'align' => 'center',
 )) ?>
 
+<?php if (!$communityForm->isNew()): ?>
 <?php
-if (!$communityForm->isNew())
-{
   op_include_parts('buttonBox', 'deleteForm', array(
     'title' => __('Delete this community'),
     'body' => __('delete this community.if you delete this community please to report in advance for all this community members.'),
@@ -31,9 +30,7 @@ if (!$communityForm->isNew())
     'method' => 'get',
     'url' => url_for('community/delete?id=' . $community->getId()),
   ));
-}
 ?>
-
 <hr color="<?php echo $op_color['core_color_11'] ?>">
-
 <?php echo link_to(__('Community Top'), 'community/home?id='.$community->getId()) ?>
+<?php endif; ?>
