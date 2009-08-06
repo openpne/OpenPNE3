44468a766b071fbeab392af930f9b40a5045a897
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-06 01:57:23 +0000

    #4169:fixed action of member/configImage, because the image registered just before is not displayed
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12545 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/actions/actions.class.php
+++ apps/pc_frontend/modules/member/actions/actions.class.php
@@ -134,6 +134,7 @@ class memberActions extends sfOpenPNEMemberAction
       {
         $this->getUser()->setFlash('error', $e->getMessage());
       }
+      $this->redirect('member/configImage');
     }
   }
 
