00c6a61d607e1d46bc54f41c6dbb3b390b7c8c88
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-11 07:19:06 +0000

    #4164:fixed template of member/profile on the pc_frontend, because top Gadgets doesn't render.
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12593 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/member/templates/profileSuccess.php
+++ apps/pc_frontend/modules/member/templates/profileSuccess.php
@@ -60,6 +60,13 @@
 <?php $content = ob_get_clean() ?>
 <?php op_include_parts('descriptionBox', 'informationAboutThisIsYourProfilePage', array('body' => $content)) ?>
 <?php endif; ?>
+<?php if (isset($topGadgets)): ?>
+<?php foreach ($topGadgets as $gadget): ?>
+<?php if ($gadget->isEnabled()): ?>
+<?php include_component($gadget->getComponentModule(), $gadget->getComponentAction(), array('gadget' => $gadget)); ?>
+<?php endif; ?>
+<?php endforeach; ?>
+<?php endif; ?>
 <?php end_slot(); ?>
 
 <?php echo javascript_tag('
