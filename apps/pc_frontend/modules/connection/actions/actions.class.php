b9a6a142f0237d87bd5d020d28c2522efcdb6254
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-13 11:26:48 +0000

    #3943:generated the connection module of the pc_frontend application
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12635 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/pc_frontend/modules/connection/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * connection actions.
+ *
+ * @package    OpenPNE
+ * @subpackage connection
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class connectionActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
