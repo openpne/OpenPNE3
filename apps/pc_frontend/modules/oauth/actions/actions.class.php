96b7c355a4298b0d8a5d14be69c4a56d0eed75fe
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-13 15:47:14 +0000

    #3943:generated the oauth module of the pc_frontend application
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12638 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/pc_frontend/modules/oauth/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * oauth actions.
+ *
+ * @package    OpenPNE
+ * @subpackage oauth
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class oauthActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
