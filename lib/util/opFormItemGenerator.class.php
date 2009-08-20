7de084650e03a766a413da477512b550decdf03c
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-20 12:35:40 +0000

    #4267:fixed opFormItemGenerator doesn't generate correctly validator at the date type field (3.0.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/branches/STABLE/3.0.x@12715 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/util/opFormItemGenerator.class.php
+++ lib/util/opFormItemGenerator.class.php
@@ -133,11 +133,11 @@ class opFormItemGenerator
 
     if ($field['ValueType'] === 'integer' || $field['FormType'] === 'date')
     {
-        if (isset($field['ValueMin']))
+        if (!empty($field['ValueMin']))
         {
           $option['min'] = $field['ValueMin'];
         }
-        if (isset($field['ValueMax']))
+        if (!empty($field['ValueMax']))
         {
           $option['max'] = $field['ValueMax'];
         }
