29329b417b4aa2a1cbc3ae83c6eac8b918836cf5
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-25 07:54:09 +0000

    #4282:added templete of community/dropMember
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12749 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ apps/mobile_frontend/modules/community/templates/dropMemberInput.php
@@ -0,0 +1,8 @@
+<?php op_mobile_page_title($community->getName(), __('Manage member')) ?>
+
+<?php op_include_parts('yesNo', 'dropMemberConfirmForm', array(
+  'body'      => __('Do you drop %0% from this community?', array('%0%' => $member->getName())),
+  'no_url'    => url_for('community/memberManage?id='.$community->getId()),
+  'no_method' => 'get',
+  'align'     => 'center',
+)) ?>
