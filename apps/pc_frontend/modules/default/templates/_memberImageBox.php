de0d6a3b3e6eca6fe22b9903d76675531f9f9bca
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-31 07:48:33 +0000

    #4157:fixed the memberImageBox gadget that displays "Show Profile" link even if a viewer isn't himself
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12787 8bbc783d-d51a-0410-adab-8fa69d43f207

--- apps/pc_frontend/modules/default/templates/_memberImageBox.php
+++ apps/pc_frontend/modules/default/templates/_memberImageBox.php
@@ -1,18 +1,17 @@
 <?php
-$photoLink = '';
+$moreInfo = array();
 if ($member->getId() === $id)
 {
-  $photoLink = link_to(__('Edit Photo'), 'member/configImage');
+  $moreInfo[] = link_to(__('Edit Photo'), 'member/configImage');
+  $moreInfo[] = link_to(__('Show Profile'), 'member/profile');
 }
 elseif ($member->getImageFileName())
 {
-  $photoLink = link_to(__('Show more Photos'), 'friend/showImage?id='.$member->getId());
+  $moreInfo[] = link_to(__('Show more Photos'), 'friend/showImage?id='.$member->getId());
 }
+
 $options = array(
   'object'   => $member,
-  'moreInfo' => array(
-    $photoLink,
-    link_to(__('Show Profile'), 'member/profile'),
-  ),
+  'moreInfo' => $moreInfo,
 );
 op_include_parts('memberImageBox', 'memberImageBox_'.$gadget->getId(), $options);
