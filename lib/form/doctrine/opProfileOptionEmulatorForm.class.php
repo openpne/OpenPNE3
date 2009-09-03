a4f776f692e817baaa7176f2f72f5de810f657f1
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-09-03 06:20:12 +0000

    #4295:added forms for dummy record class (isn't this avoidable?)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE3/trunk@12798 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ lib/form/doctrine/opProfileOptionEmulatorForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * opProfileOptionEmulator form.
+ *
+ * @package    form
+ * @subpackage opProfileOptionEmulator
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class opProfileOptionEmulatorForm extends BaseopProfileOptionEmulatorForm
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/opProfileOptionEmulatorTranslationForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * opProfileOptionEmulatorTranslation form.
+ *
+ * @package    form
+ * @subpackage opProfileOptionEmulatorTranslation
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 6174 2007-11-27 06:22:40Z fabien $
+ */
+class opProfileOptionEmulatorTranslationForm extends BaseopProfileOptionEmulatorTranslationForm
+{
+  public function configure()
+  {
+  }
+}
