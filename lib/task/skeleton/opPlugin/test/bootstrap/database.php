413efde688b129a39766213e7987e060b1e69369
Rimpei Ogawa <ogawa@tejimaya.com>
2009-10-04 02:00:22 +0900

    added opTestFunctional class (fixes #25)
    For mobile tests, added opMobileUserAgent::resetInstance() method
    removes a cache of the Net_UserAgent_Mobile instance.
    
    Fixed functional test skelton files made by the opGenerate:plugin task
    for using the opTestFunctional class.

--- lib/task/skeleton/opPlugin/test/bootstrap/database.php
+++ lib/task/skeleton/opPlugin/test/bootstrap/database.php
@@ -8,21 +8,10 @@
  * file and the NOTICE file that were distributed with this source code.
  */
 
-// guess current application
-if (!isset($app))
-{
-  $traces = debug_backtrace();
-  $caller = $traces[0];
+$configuration = ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true);
 
-  $dirPieces = explode(DIRECTORY_SEPARATOR, dirname($caller['file']));
-  $app = array_pop($dirPieces);
-}
-
-$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', true);
 new sfDatabaseManager($configuration);
 
-$task = new sfDoctrineBuildAllReloadTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array('--no-confirmation', '--dir='.sfConfig::get('sf_root_dir').'/data/fixtures', '--skip-forms'));
-
-$loadData = new sfDoctrineLoadDataTask($configuration->getEventDispatcher(), new sfFormatter());
-$loadData->run(array('--dir='.dirname(__FILE__).'/../fixtures'));
+$task = new sfDoctrineLoadDataTask($configuration->getEventDispatcher(), new sfFormatter());
+$task->run();
+$task->run(array('--dir='.dirname(__FILE__).'/../fixtures'));
--- lib/task/skeleton/opPlugin/test/functional/mobile_frontend/skeletonActionsTest.php
+++ lib/task/skeleton/opPlugin/test/functional/mobile_frontend/skeletonActionsTest.php
@@ -1,34 +1,15 @@
 <?php
 
-define('MOBILE_USER_AGENT', 'KDDI-CA39 UP.Browser/6.2.0.13.1.5 (FUI) MMP/2.0');
+include dirname(__FILE__).'/../../bootstrap/functional.php';
 
-$_SERVER['HTTP_USER_AGENT'] = MOBILE_USER_AGENT;
+$browser = new opTestFunctional(new sfBrowser(), new lime_test(null, new lime_output_color()));
+$browser->setMobile();
 
-function init()
-{
-  include(dirname(__FILE__).'/../../bootstrap/functional.php');
-  include(dirname(__FILE__).'/../../bootstrap/database.php');
-}
+include dirname(__FILE__).'/../../bootstrap/database.php';
 
-function createUser($mailAddress, $user)
-{
-  $params = array('authMailAddress' => array(
-    'mail_address' => $mailAddress,
-    'password'     => 'password',
-  ));
+$browser->login('sns@example.com', 'password');
+$browser->setCulture('en');
 
-  $user->post('/member/login/authMode/MailAddress', $params);
-
-  return $user;
-}
-
-include(dirname(__FILE__).'/../../bootstrap/functional.php');
-$browser = new sfBrowser();
-$browser->setHttpHeader('User-Agent', MOBILE_USER_AGENT);
-$user = new sfTestFunctional($browser, new lime_test(null, new lime_output_color()));
-
-// create a test user: Mr_OpenPNE
-init();
-$Mr_OpenPNE = createUser('sns@example.com', $user);
-$Mr_OpenPNE
-  ->info('This is Mr.OpenPNE');
+$browser->get('/')
+  ->with('user')->isAuthenticated()
+;
