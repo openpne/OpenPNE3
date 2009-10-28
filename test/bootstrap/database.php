f0e7a4bf3de7b82af6f5077b152370b58e399011
Shogo Kawahara <ooharabucyou@gmail.com>
2009-10-28 21:09:30 +0900

    fixed database.php for test, for optimization (fixes #134)

--- test/bootstrap/database.php
+++ test/bootstrap/database.php
@@ -3,5 +3,14 @@
 $configuration = ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true);
 new sfDatabaseManager($configuration);
 
-$task = new sfDoctrineBuildAllReloadTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array('--no-confirmation', '--dir='.dirname(__FILE__).'/../fixtures', '--skip-forms'));
+$task = new sfDoctrineDropDbTask($configuration->getEventDispatcher(), new sfFormatter());
+$task->run(array('--no-confirmation'));
+
+$task = new sfDoctrineBuildDbTask($configuration->getEventDispatcher(), new sfFormatter());
+$task->run();
+
+$task = new sfDoctrineInsertSqlTask($configuration->getEventDispatcher(), new sfFormatter());
+$task->run();
+
+$task = new sfDoctrineLoadDataTask($configuration->getEventDispatcher(), new sfFormatter());
+$task->run(array('--dir='.dirname(__FILE__).'/../fixtures'));
