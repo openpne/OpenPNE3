8f026213a8ed546a458edac778995be967c1906d
Kousuke Ebihara <ebihara@tejimaya.com>
2009-10-28 15:24:28 +0900

    added the opBaseSendMailTask (refs #32)

---
+++ lib/task/opBaseSendMailTask.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+abstract class opBaseSendMailTask extends sfDoctrineBaseTask
+{
+  public function initialize(sfEventDispatcher $dispatcher, sfFormatter $formatter)
+  {
+    parent::initialize($dispatcher, $formatter);
+
+    // This is just a hack to make OpenPNE generate a valid url in some helper function
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $this->openDatabaseConnection();
+
+    // sfOpenPNEMailSend requires Zend
+    sfOpenPNEApplicationConfiguration::registerZend();
+  }
+
+  protected function openDatabaseConnection()
+  {
+    new sfDatabaseManager($this->configuration);
+  }
+
+  protected function getContextByEmailAddress($address)
+  {
+    $application = 'pc_frontend';
+    if (opToolkit::isMobileEmailAddress($address))
+    {
+      $application = 'mobile_frontend';
+    }
+
+    if (!sfContext::hasInstance($application))
+    {
+      $context = sfContext::createInstance($this->createConfiguration($application, 'prod'), $application);
+    }
+    else
+    {
+      $context = sfContext::getInstance($application);
+    }
+
+    return $context;
+  }
+}
