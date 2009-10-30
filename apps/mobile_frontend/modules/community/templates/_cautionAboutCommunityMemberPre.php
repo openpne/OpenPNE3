e64354532cb2091f14b3b66bec2f5c1e06594231
Kousuke Ebihara <ebihara@tejimaya.com>
2009-10-31 03:56:22 +0900

    changed processing joining community request to use confirmation page in the mobile_frontend (refs #88)

--- apps/mobile_frontend/modules/community/templates/_cautionAboutCommunityMemberPre.php
+++ apps/mobile_frontend/modules/community/templates/_cautionAboutCommunityMemberPre.php
@@ -1,13 +1,7 @@
-<?php foreach ($communityMembers as $communityMember) : ?>
+<?php if ($communityMembersCount): ?>
 <font color="red">
-<?php echo __('%1% send request for participation to %2%.', array(
-'%1%' => link_to($communityMember->getMember()->getName(), 'member/profile?id='.$communityMember->getMemberId()),
-'%2%' => link_to($communityMember->getCommunity()->getName(), 'community/home?id='.$communityMember->getCommunityId())
-)) ?>
+<?php echo __('You\'ve gotten %1% one\'s %community% joining requests', array('%1%' => $communityMembersCount)); ?>
 &nbsp;
-<?php $param = 'id='.$communityMember->getCommunityId().'&member_id='.$communityMember->getMemberId(); ?>
-<?php echo link_to(__('Accept'), 'community/joinAccept?'.$param) ?>
-&nbsp;
-<?php echo link_to(__('Reject'), 'community/joinReject?'.$param) ?>
+<?php echo link_to(__('Go to Confirmation Page'), '@confirmation_list?category=community_confirm') ?>
 </font><br>
-<?php endforeach; ?>
+<?php endif; ?>
