ba9cfbbe6869e321d8166c2e783a60c4f05aaaef
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-17 20:21:26 +0900

    fixed for opCommunityTopicPlugin (refs #239)

--- data/migrations/3.1.3/021_add_is_login_rejected_column.php
+++ data/migrations/3.1.3/021_add_is_login_rejected_column.php
@@ -16,6 +16,14 @@ class Revision21_AddIsLoginRejectedColumn extends Doctrine_Migration_Base
       'notnull' => 1,
       'default' => 0,
     ));
+
+    // and try to fix community topic revision if the plugin is exists
+    $conn = Doctrine_Manager::getInstance()->getConnectionForComponent('SnsConfig');
+    $result = $conn->fetchOne('SELECT value FROM sns_config WHERE name = ?', array('opCommunityTopicPlugin_revision'));
+    if (!$result)
+    {
+      Doctrine::getTable('SnsConfig')->set('opCommunityTopicPlugin_revision', '4');
+    }
   }
 
   public function down()
