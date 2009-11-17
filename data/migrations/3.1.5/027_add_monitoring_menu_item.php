61ec8089aad317744fc9e143328082e68fab52c0
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-17 22:09:52 +0900

    fixes for opDiaryPlugin (refs #239)

--- data/migrations/3.1.5/027_add_monitoring_menu_item.php
+++ data/migrations/3.1.5/027_add_monitoring_menu_item.php
@@ -10,4 +10,14 @@
 
 class Revision27_AddMonitoringMenuItem extends Doctrine_Migration_Base
 {
+  public function up()
+  {
+    // and try to fix community topic revision if the plugin is exists
+    $conn = Doctrine_Manager::getInstance()->getConnectionForComponent('SnsConfig');
+    $result = $conn->fetchOne('SELECT value FROM sns_config WHERE name = ?', array('opDiaryPlugin_revision'));
+    if (!$result)
+    {
+      Doctrine::getTable('SnsConfig')->set('opDiaryPlugin_revision', '3');
+    }
+  }
 }
