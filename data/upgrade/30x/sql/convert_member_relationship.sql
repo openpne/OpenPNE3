d6f1ab0ba3ff9ca380afce9d9df8f2c7aa5f2b6d
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-20 04:46:02 +0900

    added upgrading "convert_member_relationship" (refs #237)

---
+++ data/upgrade/30x/sql/convert_member_relationship.sql
@@ -0,0 +1,3 @@
+ALTER TABLE member_relationship MODIFY is_friend tinyint(1) DEFAULT NULL;
+ALTER TABLE member_relationship MODIFY is_friend_pre tinyint(1) DEFAULT NULL;
+ALTER TABLE member_relationship MODIFY is_access_block tinyint(1) DEFAULT NULL;
