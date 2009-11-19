0f5bd22f4b0855882690bbdd43b4e99d85dacfad
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-20 04:30:41 +0900

    added upgrading "build_model" (refs #237)

---
+++ lib/upgrade/strategy/opUpgradeBuildModelStrategy.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * This strategy builds models.
+ *
+ * @package    OpenPNE
+ * @subpackage upgrade
+ * @author     Kousuke Ebihara <ebihara@tejimaya.com>
+ */
+class opUpgradeBuildModelStrategy extends opUpgradeAbstractStrategy
+{
+  public function run()
+  {
+    sfOpenPNEApplicationConfiguration::unregisterZend();
+
+    $task = new sfDoctrineBuildModelTask($this->options['dispatcher'], $this->options['formatter']);
+    $task->run();
+
+    $task = new sfCacheClearTask($this->options['dispatcher'], $this->options['formatter']);
+    $task->run();
+
+    sfOpenPNEApplicationConfiguration::registerZend();
+  }
+}
+
