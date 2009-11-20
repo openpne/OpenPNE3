a203493597e108249f9edfcfb481237265857bc4
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-20 14:58:32 +0900

    added dummy class for BC (refs #237)

---
+++ lib/util/sfPropelBaseTask.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// dummy class for BC
+abstract class sfPropelBaseTask extends sfBaseTask
+{
+}
