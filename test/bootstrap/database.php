8614c71e7b16f30b00617a3795fb333d58a5b13f
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-24 20:40:39 +0900

    fixed coding standard error; a variable name is not camelCased (refs #221)

--- test/bootstrap/database.php
+++ test/bootstrap/database.php
@@ -2,19 +2,19 @@
 
 $_app = 'pc_frontend';
 $_env = 'test';
-$_task_config = array('--application='.$_app, '--env='.$_env);
+$_taskConfig = array('--application='.$_app, '--env='.$_env);
 
 $configuration = ProjectConfiguration::getApplicationConfiguration($_app, $_env, true);
 new sfDatabaseManager($configuration);
 
 $task = new sfDoctrineDropDbTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array_merge($_task_config, array('--no-confirmation')));
+$task->run(array_merge($_taskConfig, array('--no-confirmation')));
 
 $task = new sfDoctrineBuildDbTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run($_task_config);
+$task->run($_taskConfig);
 
 $task = new sfDoctrineInsertSqlTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run($_task_config);
+$task->run($_taskConfig);
 
 $task = new sfDoctrineLoadDataTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array_merge($_task_config, array('--dir='.dirname(__FILE__).'/../fixtures')));
+$task->run(array_merge($_taskConfig, array('--dir='.dirname(__FILE__).'/../fixtures')));
