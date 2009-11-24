20cab1abd3754ae234603562cf143188ef782e64
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-24 19:45:02 +0900

    changed the executing doctrine task to be with "application" option and "env" option (refs #221)

--- test/bootstrap/database.php
+++ test/bootstrap/database.php
@@ -1,16 +1,20 @@
 <?php
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true);
+$_app = 'pc_frontend';
+$_env = 'test';
+$_task_config = array('--application='.$_app, '--env='.$_env);
+
+$configuration = ProjectConfiguration::getApplicationConfiguration($_app, $_env, true);
 new sfDatabaseManager($configuration);
 
 $task = new sfDoctrineDropDbTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array('--no-confirmation'));
+$task->run(array_merge($_task_config, array('--no-confirmation')));
 
 $task = new sfDoctrineBuildDbTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run();
+$task->run($_task_config);
 
 $task = new sfDoctrineInsertSqlTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run();
+$task->run($_task_config);
 
 $task = new sfDoctrineLoadDataTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array('--dir='.dirname(__FILE__).'/../fixtures'));
+$task->run(array_merge($_task_config, array('--dir='.dirname(__FILE__).'/../fixtures')));
