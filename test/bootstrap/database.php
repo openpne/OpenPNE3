80dd78aa731a462840f7f0162688f457336705f0
Kousuke Ebihara <ebihara@tejimaya.com>
2009-11-25 22:02:18 +0900

    fixed loading data in test bootstrap file (refs #140)

--- test/bootstrap/database.php
+++ test/bootstrap/database.php
@@ -2,19 +2,14 @@
 
 $_app = 'pc_frontend';
 $_env = 'test';
-$_taskConfig = array('--application='.$_app, '--env='.$_env);
 
 $configuration = ProjectConfiguration::getApplicationConfiguration($_app, $_env, true);
 new sfDatabaseManager($configuration);
 
-$task = new sfDoctrineDropDbTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array_merge($_taskConfig, array('--no-confirmation')));
-
-$task = new sfDoctrineBuildDbTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run($_taskConfig);
-
-$task = new sfDoctrineInsertSqlTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run($_taskConfig);
-
-$task = new sfDoctrineLoadDataTask($configuration->getEventDispatcher(), new sfFormatter());
-$task->run(array_merge($_taskConfig, array('--dir='.dirname(__FILE__).'/../fixtures')));
+$task = new sfDoctrineBuildTask($configuration->getEventDispatcher(), new sfFormatter());
+$task->setConfiguration($configuration);
+$task->run(array(), array(
+  'no-confirmation' => true,
+  'db'              => true,
+  'and-load'        => dirname(__FILE__).'/../fixtures',
+));
