f72eb6b43d25d93c7571202edcdf606679416cb9
Kousuke Ebihara <ebihara@tejimaya.com>
2009-12-13 09:29:14 +0900

    added support for color configuration of mobile (refs #282)

--- apps/mobile_frontend/config/mobile_frontendConfiguration.class.php
+++ apps/mobile_frontend/config/mobile_frontendConfiguration.class.php
@@ -12,49 +12,6 @@ require_once dirname(__FILE__).'/../../../lib/config/sfOpenPNEApplicationConfigu
 
 class mobile_frontendConfiguration extends sfOpenPNEApplicationConfiguration
 {
-  public function configure()
-  {
-    // TODO: They must be managed by an original config handler
-    $colors = array(
-      'core_color_1'  => '#FFFFFF',
-      'core_color_2'  => '#0D6DDF',
-      'core_color_3'  => '#DDDDDD',
-      'core_color_4'  => '#EEEEFF',
-      'core_color_5'  => '#7DDADF',
-      'core_color_6'  => '#E0EAEF',
-      'core_color_7'  => '#FFFFFF',
-      'core_color_8'  => '#C49FFF',
-      'core_color_9'  => '#DCD1EF',
-      'core_color_10' => '#FFFFFF',
-      'core_color_11' => '#0D6DDF',
-      'core_color_12' => '#B3CEEF',
-      'core_color_13' => '#BFA4EF',
-      'core_color_14' => '#000000',
-      'core_color_15' => '#0000FF',
-      'core_color_23' => '#FFFFFF',
-      'core_color_17' => '#800080',
-      'core_color_18' => '#EEEEEE',
-      'core_color_19' => '#999966',
-      'core_color_20' => '#0C5F0F',
-      'core_color_21' => '#C49FFF',
-      'core_color_22' => '#FF0000',
-      'core_color_24' => '#000000',
-      'core_color_25' => '#000000',
-      'core_color_26' => '#000000',
-      'core_color_27' => '#DDDDDD',
-      'core_color_28' => '#000000',
-    );
-
-    $prefix = 'op_'.sfConfig::get('sf_app').'_color_config_';
-    foreach ($colors as $key => $value)
-    {
-      if (!sfConfig::has($prefix.$key))
-      {
-        sfConfig::set($prefix.$key, $value);
-      }
-    }
-  }
-
   public function initialize()
   {
     parent::initialize();
--- lib/config/opColorConfig.class.php
+++ lib/config/opColorConfig.class.php
@@ -17,6 +17,36 @@
  */
 class opColorConfig extends opConfig
 {
+  protected static $defaultColors = array(
+      'core_color_1'  => '#FFFFFF',
+      'core_color_2'  => '#0D6DDF',
+      'core_color_3'  => '#DDDDDD',
+      'core_color_4'  => '#EEEEFF',
+      'core_color_5'  => '#7DDADF',
+      'core_color_6'  => '#E0EAEF',
+      'core_color_7'  => '#FFFFFF',
+      'core_color_8'  => '#C49FFF',
+      'core_color_9'  => '#DCD1EF',
+      'core_color_10' => '#FFFFFF',
+      'core_color_11' => '#0D6DDF',
+      'core_color_12' => '#B3CEEF',
+      'core_color_13' => '#BFA4EF',
+      'core_color_14' => '#000000',
+      'core_color_15' => '#0000FF',
+      'core_color_23' => '#FFFFFF',
+      'core_color_17' => '#800080',
+      'core_color_18' => '#EEEEEE',
+      'core_color_19' => '#999966',
+      'core_color_20' => '#0C5F0F',
+      'core_color_21' => '#C49FFF',
+      'core_color_22' => '#FF0000',
+      'core_color_24' => '#000000',
+      'core_color_25' => '#000000',
+      'core_color_26' => '#000000',
+      'core_color_27' => '#DDDDDD',
+      'core_color_28' => '#000000',
+    );
+
   public static function get($name, $default = '#000000', $app = null)
   {
     if (is_null($app))
@@ -24,7 +54,7 @@ class opColorConfig extends opConfig
       $app = sfConfig::get('sf_app');
     }
     $configName = 'op_'.$app.'_color_config_'.$name;
-    $result = sfConfig::get($configName, $default);
+    $result = sfConfig::get($configName, opConfig::get($app.'_'.$name, self::$defaultColors[$name]));
 
     sfContext::getInstance()->getConfiguration()->loadHelpers('Escaping');
     return sfOutputEscaper::escape(sfConfig::get('sf_escaping_method'), $result);
