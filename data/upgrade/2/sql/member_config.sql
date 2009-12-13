d8ff5053b69378017c72ed66e884027fc42a3747
Kousuke Ebihara <ebihara@tejimaya.com>
2009-12-13 10:40:31 +0900

    added upgrading for member_config (refs #308)

---
+++ data/upgrade/2/sql/member_config.sql
@@ -0,0 +1,7 @@
+INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "blog_url", rss, NOW(), NOW() FROM c_member);
+INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "age_public_flag", 1, NOW(), NOW() FROM c_member WHERE public_flag_birth_year = "public");
+INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "age_public_flag", 2, NOW(), NOW() FROM c_member WHERE public_flag_birth_year = "friend");
+INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "age_public_flag", 3, NOW(), NOW() FROM c_member WHERE public_flag_birth_year = "private");
+INSERT INTO member_config (id, member_id, name, value_datetime, created_at, updated_at) (SELECT NULL, c_member_id, "lastLogin", access_date, NOW(), NOW() FROM c_member);
+INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "op_ashiato_count", ashiato_count_log, NOW(), NOW() FROM c_member);
+
