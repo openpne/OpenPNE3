457cda06cc5849b25ee1af9c4c51d96b3c4c4daa
Kousuke Ebihara <ebihara@tejimaya.com>
2009-12-13 09:33:28 +0900

    fixed useless escaping (refs #140)

--- apps/pc_frontend/modules/default/templates/_freeAreaBox.php
+++ apps/pc_frontend/modules/default/templates/_freeAreaBox.php
@@ -4,5 +4,5 @@ if ($gadget->getConfig('title'))
 {
   $options['title'] = $gadget->getConfig('title');
 }
-op_include_box('freeArea_'.$gadget->getId(), $sf_data->getRaw('gadget')->getConfig('value'), $options)
+op_include_box('freeArea_'.$gadget->getId(), $gadget->getRawValue()->getConfig('value'), $options)
 ?>
--- apps/pc_frontend/modules/default/templates/_informationBox.php
+++ apps/pc_frontend/modules/default/templates/_informationBox.php
@@ -1,3 +1,3 @@
 <?php
-op_include_parts('informationBox', 'information_'.$gadget->getId(), array('body' => $sf_data->getRaw('gadget')->getConfig('value')))
+op_include_parts('informationBox', 'information_'.$gadget->getId(), array('body' => $gadget->getRawValue()->getConfig('value')))
 ?>
