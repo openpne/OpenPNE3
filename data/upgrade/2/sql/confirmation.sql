796fa482fddbfd2969f0b5439a0da070bc593548
Kousuke Ebihara <ebihara@tejimaya.com>
2009-12-13 11:03:39 +0900

    added upgrading for confirmation (refs #308)

---
+++ data/upgrade/2/sql/confirmation.sql
@@ -0,0 +1,2 @@
+UPDATE c_commu_admin_confirm AS ccac, community_member AS cm SET cm.position = "admin_confirm" WHERE ccac.c_commu_id = cm.community_id AND ccac.c_member_id_to = cm.member_id;
+INSERT INTO community_member (id, community_id, member_id, position, is_receive_mail_pc, is_receive_mail_mobile, created_at, updated_at) (SELECT NULL, c_commu_id, c_member_id, "pre", 0, 0, r_datetime, r_datetime FROM c_commu_member_confirm);
