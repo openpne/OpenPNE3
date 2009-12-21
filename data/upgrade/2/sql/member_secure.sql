7baa8b8187fa5b9173798d97a0dce5c7917d12ab
Kousuke Ebihara <ebihara@tejimaya.com>
2009-12-22 00:27:09 +0900

    fixed member_secure converting that is measure for "?" in the secret questions are ambiguous character (fixes #406)

--- data/upgrade/2/sql/member_secure.sql
+++ data/upgrade/2/sql/member_secure.sql
@@ -3,15 +3,20 @@ INSERT INTO member_config (member_id, name, value) (SELECT c_member_id, "mobile_
 
 <?php
 $questions = array(
-  '1' => '母または父の旧姓は?',
-  '2' => '運転免許証番号の下 5 桁は?',
-  '3' => '初恋の人の名前は?',
-  '4' => '卒業した小学校の名前は？',
-  '5' => '本籍地の県名は？',
+  '母または父の旧姓は?' => '1',
+  '母または父の旧姓は？' => '1',
+  '運転免許証番号の下 5 桁は?' => '2',
+  '運転免許証番号の下 5 桁は？' => '2',
+  '初恋の人の名前は?' => '3',
+  '初恋の人の名前は？' => '3',
+  '卒業した小学校の名前は?' => '4',
+  '卒業した小学校の名前は？' => '4',
+  '本籍地の県名は?' => '5',
+  '本籍地の県名は？' => '5',
 );
 ?>
 
-<?php foreach ($questions as $id => $question): ?>
-INSERT INTO member_config (member_id, name, value) (SELECT c_member_id, "secret_question", <?php echo $id ?>  FROM c_member_secure WHERE c_member_id IN (SELECT c_member_id FROM c_member WHERE c_password_query_id = (SELECT c_password_query_id FROM c_password_query WHERE c_password_query_question = "<?php echo $question ?>")));
+<?php foreach ($questions as $question => $id): ?>
+INSERT INTO member_config (member_id, name, value) (SELECT c_member_id, "secret_question", <?php echo $id ?> FROM c_member_secure WHERE c_member_id IN (SELECT c_member_id FROM c_member WHERE c_password_query_id = (SELECT c_password_query_id FROM c_password_query WHERE c_password_query_question = "<?php echo $question ?>")) AND c_member_id NOT IN (SELECT member_id FROM member_config WHERE name = "secret_question"));
 <?php endforeach; ?>
 INSERT INTO member_config (member_id, name, value) (SELECT c_member_id, "secret_answer", hashed_password_query_answer FROM c_member_secure WHERE c_member_id IN (SELECT member_id FROM member_config WHERE name = "secret_question"));
