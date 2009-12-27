e0760ae7d08897d24039c8957b5cb9d17437fcd1
Shogo Kawahara <ooharabucyou@gmail.com>
2009-12-27 20:50:47 +0900

    added migration script (refs #230)

---
+++ data/migrations/3.3.2/036_create_table_for_activity.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+class Revision36_CreateTableForActivity extends Doctrine_Migration_Base
+{
+  public function migrate($direction)
+  {
+    $this->table($direction, 'activity_data', array(
+      'id' => array(
+        'type' => 'integer',
+        'primary' => 1,
+        'autoincrement' => 1,
+        'comment' => 'Serial number',
+        'length' => 4,
+      ),
+      'member_id' => array(
+        'type' => 'integer',
+        'notnull' => true,
+        'comment' => 'Member id',
+        'length' => 4
+      ),
+      'in_reply_to_activity_id' => array(
+        'type' => 'integer',
+        'comment' => 'Activity data id is in reply to',
+        'length' => 4
+      ),
+      'body' => array(
+        'type' => 'string',
+        'notnull' => true,
+        'comment' => 'Activity body',
+        'length' => 140
+      ),
+      'uri' => array(
+        'type' => 'string',
+        'comment' => 'Activity URI'
+      ),
+      'public_flag' => array(
+        'type' => 'integer',
+        'notnull' => true,
+        'default' => 1,
+        'comment' => 'Public flag of activity',
+        'length' => 1
+      ),
+      'is_pc' => array(
+        'type' => 'boolean',
+        'notnull' => true,
+        'default' => 1,
+        'comment' => 'Display this in PC?'
+      ),
+      'is_mobile' => array(
+        'type' => 'boolean',
+        'notnull' => true,
+        'default' => 1,
+        'comment' => 'Display this in Mobile?'
+      ),
+      'source' => array(
+        'type' => 'string',
+        'comment' => 'The source caption',
+        'length' => 64
+      ),
+      'source_uri' => array(
+        'type' => 'string',
+        'comment' => 'The source URI',
+      ),
+      'foreign_table' => array(
+        'type' => 'string',
+        'comment' => 'Reference table name',
+      ),
+      'foreign_id' => array(
+        'type' => 'integer',
+        'comment' => 'The is of reference table',
+      )
+    ));
+
+    $this->table($direction, 'activity_image', array(
+      'id' => array(
+        'type' => 'integer',
+        'primary' => 1,
+        'autoincrement' => 1,
+        'comment' => 'Serial number',
+        'length' => 4
+      ),
+      'activity_data_id' => array(
+        'type' => 'integer',
+        'notnull' => true,
+        'comment' => 'Activity data id',
+        'length' => 4
+      ),
+      'mine_type' => array(
+        'type' => 'string',
+        'notnull' => true,
+        'comment' => 'MIME type',
+        'length' => 64
+      ),
+      'uri' => array(
+        'type' => 'string',
+        'comment' => 'Image URI'
+      ),
+      'file_id' => array(
+        'type' => 'integer',
+        'comment' => 'File id',
+        'length' => 4
+      )
+    ));
+  }
+}
---
+++ data/migrations/3.3.2/037_index_for_activity.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+class Revision37_IndexForActivity extends Doctrine_Migration_Base
+{
+  public function migrate($direction)
+  {
+    $this->index($direction, 'activity_data', 'member_id_idx', array(
+      'name' => 'member_id_idx',
+      'fields' => array('member_id')
+    ));
+
+    $this->index($direction, 'activity_data', 'in_reply_to_activity_id_idx', array(
+      'name' => 'in_reply_to_activity_id_idx',
+      'fields' => array('in_reply_to_activity_id')
+    ));
+
+    $this->index($direction, 'activity_image', 'activity_data_id_idx', array(
+      'name' => 'activity_data_id_idx',
+      'fields' => array('activity_data_id')
+    ));
+
+    $this->index($direction, 'activity_image', 'file_id_idx', array(
+      'name' => 'file_id_idx',
+      'fields' => array('file_id')
+    ));
+
+    $this->foreignKey($direction, 'activity_data', 'activity_data_member_id_member_id', array(
+      'name' => 'activity_data_member_id_member_id',
+      'local' => 'member_id',
+      'foreign' => 'id',
+      'foreignTable' => 'member',
+      'onDelete' => 'CASCADE'
+    ));
+
+    $this->foreignKey($direction, 'activity_data', 'activity_data_in_reply_to_activity_id_activity_data_id', array(
+      'name' => 'activity_data_in_reply_to_activity_id_activity_data_id',
+      'local' => 'in_reply_to_activity_id',
+      'foreign' => 'id',
+      'foreignTable' => 'activity_data',
+      'onDelete' => 'CASCADE'
+    ));
+
+    $this->foreignKey($direction, 'activity_image', 'activity_image_activity_data_id_activity_data_id', array(
+      'name' => 'activity_image_activity_data_id_activity_data_id',
+      'local' => 'activity_data_id',
+      'foreign' => 'id',
+      'foreignTable' => 'activity_data',
+      'onDelete' => 'CASCADE'
+    ));
+
+    $this->foreignKey($direction, 'activity_image', 'activity_image_file_id_file_id', array(
+      'name' => 'activity_image_file_id_file_id',
+      'local' => 'file_id',
+      'foreign' => 'id',
+      'foreignTable' => 'file',
+      'onDelete' => 'CASCADE'
+    ));
+  }
+}
