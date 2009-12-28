d7944d9a861af22019f9ab649e6aa35fb84144b3
Kousuke Ebihara <ebihara@tejimaya.com>
2009-12-28 19:06:21 +0900

    fixed converting banner_image (fixes #410)

--- data/upgrade/2/sql/banner.sql
+++ data/upgrade/2/sql/banner.sql
@@ -23,7 +23,7 @@ ALTER TABLE c_banner CHANGE image_filename image_filename text CHARACTER SET utf
 INSERT INTO banner_image (id, file_id, url, name, created_at, updated_at) (SELECT c_banner_id, <?php echo $this->getSQLForFileId('image_filename') ?>, a_href, nickname, NOW(), NOW() FROM c_banner);
 <?php foreach ($list as $k => $v): ?>
 UPDATE banner, c_siteadmin SET banner.html = c_siteadmin.body, banner.is_use_html = 1 WHERE c_siteadmin.target = "<?php echo $v['c_siteadmin'] ?>" AND banner.name = "<?php echo $k ?>";
-INSERT INTO banner_use_image (id, banner_id, banner_image_id, created_at, updated_at) (SELECT NULL, (SELECT id FROM banner WHERE name = "<?php echo $k ?>" LIMIT 1), c_banner_id, NOW(), NOW() FROM c_banner WHERE type = "<?php echo $v['c_banner'][0] ?>" AND <?php echo $v['c_banner'][1] ?> = 0 AND (SELECT id FROM banner WHERE name = "<?php echo $k ?>" AND html = "" LIMIT 1) IS NOT NULL);
+INSERT INTO banner_use_image (id, banner_id, banner_image_id, created_at, updated_at) (SELECT NULL, (SELECT id FROM banner WHERE name = "<?php echo $k ?>" LIMIT 1), c_banner_id, NOW(), NOW() FROM c_banner WHERE type = "<?php echo $v['c_banner'][0] ?>" AND <?php echo $v['c_banner'][1] ?> = 0 AND (SELECT id FROM banner WHERE name = "<?php echo $k ?>" AND (html = "" OR html IS NULL) LIMIT 1) IS NOT NULL);
 <?php endforeach; ?>
 UPDATE banner SET is_use_html = 0 WHERE html = "";
 
