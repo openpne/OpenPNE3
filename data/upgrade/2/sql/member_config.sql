6c8284e4902c3adaf356adeed40d8bda715b73a0
Kousuke Ebihara <ebihara@tejimaya.com>
2009-12-28 09:25:59 +0900

    fixed importing "blog_url" of member for not converting if the member doesn't have "rss" configuration (fixes #407)

--- data/upgrade/2/sql/member_config.sql
+++ data/upgrade/2/sql/member_config.sql
@@ -1,4 +1,4 @@
-INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "blog_url", rss, NOW(), NOW() FROM c_member);
+INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "blog_url", rss, NOW(), NOW() FROM c_member WHERE rss <> "");
 INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "age_public_flag", 1, NOW(), NOW() FROM c_member WHERE public_flag_birth_year = "public");
 INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "age_public_flag", 2, NOW(), NOW() FROM c_member WHERE public_flag_birth_year = "friend");
 INSERT INTO member_config (id, member_id, name, value, created_at, updated_at) (SELECT NULL, c_member_id, "age_public_flag", 3, NOW(), NOW() FROM c_member WHERE public_flag_birth_year = "private");
