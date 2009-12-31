e87220ea51c627bc930408c304085299fbbb2fb5
Kousuke Ebihara <ebihara@tejimaya.com>
2010-01-01 02:40:33 +0900

    Revert "add googlemaps modules and fixed googlemaps cmd bug. (refs #313)"
    This reverts commit 73ebab3eccd394868a953b6d5ee7bdd85ce7e689.
    Because these changes are not under a backport-ticket (see http://redmine.openpne.jp/projects/op3/wiki/Ticket_Workflow).

--- web/cmd/maps.google.com.js
+++ web/cmd/maps.google.com.js
@@ -1,16 +1,16 @@
-function url2cmd(url, googlemapsUrl) {
-  var maps_google_com_maps = url.match(/^http:\/\/maps\.google\.com\/maps\?(.+)/);
-  var maps_google_com = url.match(/^http:\/\/maps\.google\.com\/\?(.+)/);
-
-  if (maps_google_com_maps || maps_google_com) {
-     var id = RegExp.$1;
-     main(id, googlemapsUrl);
+function url2cmd(url) {
+	var maps_google_com_maps = url.match(/^http:\/\/maps\.google\.com\/maps\?(.+)/);
+	var maps_google_com = url.match(/^http:\/\/maps\.google\.com\/\?(.+)/);
+			
+	if (maps_google_com_maps || maps_google_com) {
+	   var id = RegExp.$1;
+	   main(id);
     } else {
        pne_url2a(url);
     }
 }
 
-function main(id, googlemapsUrl) {
+function main(id) {
     var cmd = id.split("&amp;");
     var param = new Array();
     param["z"] = "15";
@@ -27,9 +27,9 @@ function main(id, googlemapsUrl) {
     var q = param["q"];
 
     var html = ''
-    + '<iframe marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bordercolor="#000000" src="'+googlemapsUrl+'?x='+ll[0]+'&y='+ll[1]+'&z='+z+'&t='+t+'&q='+q+'" name="sample" height="350">'
-    + 'この部分はインラインフレームを使用しています。'
-    + '</iframe>';
-
+	+ '<iframe MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="#000000" src="?m=pc&a=page_h_googlemap'+'&x='+ll[0]+'&y='+ll[1]+'&z='+z+'&t='+t+'&q='+q+'" name="sample" height="350">'
+	+ 'この部分はインラインフレームを使用しています。'
+	+ '</iframe>';
+	
     document.write(html);
 }
