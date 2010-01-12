d646eeca3fda896c157d329367585ecb32e9430c
Kousuke Ebihara <ebihara@tejimaya.com>
2010-01-12 22:57:11 +0900

    removed openpne:upgrade-from-2 task (fixes #481)

--- data/upgrade/2/sql/gadget.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO gadget_config (id, name, gadget_id, value, created_at, updated_at) VALUES(NULL, "value", (SELECT id FROM gadget WHERE name = "informationBox" AND type = "top"), (SELECT body FROM c_siteadmin WHERE target = "h_home"), NOW(), NOW());
-INSERT INTO gadget_config (id, name, gadget_id, value, created_at, updated_at) VALUES(NULL, "value", (SELECT id FROM gadget WHERE name = "informationBox" AND type = "mobileTop"), (SELECT body FROM c_siteadmin WHERE target = "k_h_home"), NOW(), NOW());
-
