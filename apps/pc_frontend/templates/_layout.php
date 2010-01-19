dd487f85e7a9cbf8a49a25ca0fb7531b419ad884
fukamachi <e.arrows@gmail.com>
2010-01-19 18:07:13 +0900

    fixed not to display cached other member's local navigations (refs #602, BP from #517)

--- apps/pc_frontend/templates/_layout.php
+++ apps/pc_frontend/templates/_layout.php
@@ -32,10 +32,13 @@ $context = sfContext::getInstance();
 $module = $context->getActionStack()->getLastEntry()->getModuleName();
 $localNavOptions = array(
   'is_secure' => opToolkit::isSecurePage(),
-  'id'        => $context->getRequest()->getParameter('id'),
   'type'      => sfConfig::get('sf_nav_type', sfConfig::get('mod_'.$module.'_default_nav', 'default')),
   'culture'   => $context->getUser()->getCulture(),
 );
+if ('default' !== $localNavOptions['type'])
+{
+  $localNavOptions['nav_id'] = sfConfig::get('sf_nav_id', $context->getRequest()->getParameter('id'));
+}
 include_component('default', 'localNav', $localNavOptions);
 ?>
 </div><!-- localNav -->
