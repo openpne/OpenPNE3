b88adffa34da7155563dbb4dbdcadf87946e4efb
Kousuke Ebihara <ebihara@tejimaya.com>
2010-02-18 19:35:14 +0900

    fixed that there is Doctrine style code in the SnsConfigPeer (fixes #680)

--- lib/model/SnsConfigPeer.php
+++ lib/model/SnsConfigPeer.php
@@ -40,7 +40,7 @@ class SnsConfigPeer extends BaseSnsConfigPeer
       $config->setName($name);
     }
     $config->setValue($value);
-    $this->configs[$name] = $config;
+    self::$configs[$name] = $config;
     return $config->save();
   }
 
