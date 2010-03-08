546eb0d184e3ca1a7a41cb38bb23a008a9bc0a82
Kousuke Ebihara <ebihara@tejimaya.com>
2010-03-08 12:56:38 +0900

    added ability to resolve plugin dependency with OpenPNE (refs #557)

---
+++ lib/plugin/opPluginDependency.class.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * The opPluginDependency class
+ *
+ * @package    OpenPNE
+ * @subpackage plugin
+ * @author     Kousuke Ebihara <ebihara@php.net>
+ */
+class opPluginDependency extends PEAR_Dependency2
+{
+  public function validatePackageDependency($dep, $required, $params, $depv1 = false)
+  {
+    if ('symfony' === strtolower($dep['name']))
+    {
+      return true;
+    }
+
+    if ('openpne' === strtolower($dep['name']))
+    {
+      $extra = $this->_getExtraString($dep);
+
+      if (isset($dep['min']) && !version_compare(OPENPNE_VERSION, $dep['min'], '>='))
+      {
+        return $this->handleOpenPNEDependencyError('%s requires OpenPNE'.$extra.', installed version is '.OPENPNE_VERSION);
+      }
+
+      if (isset($dep['max']) && !version_compare(OPENPNE_VERSION, $dep['max'], '<='))
+      {
+        return $this->handleOpenPNEDependencyError('%s requires OpenPNE'.$extra.', installed version is '.OPENPNE_VERSION);
+      }
+
+      if (isset($dep['exclude']))
+      {
+        foreach ((array)$dep['exclude'] as $exclude)
+        {
+          if (version_compare(OPENPNE_VERSION, $exclude, '=='))
+          {
+            return $this->handleOpenPNEDependencyError('%s is not compatible with OpenPNE version '.$exclude);
+          }
+        }
+      }
+
+      return true;
+    }
+
+    return parent::validatePackageDependency($dep, $required, $params, $depv1);
+  }
+
+  protected function handleOpenPNEDependencyError($message)
+  {
+    $_message = ' '.ucfirst(sprintf($message, 'this plugin'));
+
+    $formatter = $this->getFormatter();
+    if ($formatter)
+    {
+      $len = strlen($_message) + 4;
+      $_message = str_repeat(' ', $len).PHP_EOL
+                . $_message.str_repeat(' ', 4).PHP_EOL
+                . str_repeat(' ', $len).PHP_EOL;
+
+      echo $formatter->format($_message, 'ERROR', STDERR);
+    }
+
+    return $this->raiseError($message);
+  }
+
+  protected function getFormatter()
+  {
+    if ('cli' !== PHP_SAPI)
+    {
+      return null;
+    }
+
+    // It is not Windows, and the stdout is an interactive terminal
+    if (DIRECTORY_SEPARATOR !== '\\' && function_exists('posix_isatty') && @posix_isatty(STDOUT))
+    {
+      return new sfAnsiColorFormatter();
+    }
+
+    return new sfFormatter();
+  }
+}
