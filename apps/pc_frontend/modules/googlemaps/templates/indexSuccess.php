5327a96945533eaa9c27b81369b11b9d38d8cce1
Kousuke Ebihara <ebihara@tejimaya.com>
2010-03-30 23:28:16 +0900

    changed way to processing request parameter on google-maps cmd (refs #872)

--- apps/pc_frontend/modules/googlemaps/templates/indexSuccess.php
+++ apps/pc_frontend/modules/googlemaps/templates/indexSuccess.php
@@ -11,16 +11,34 @@
 <?php use_javascript('http://maps.google.co.jp/maps?file=api&v=2.x&key='.$op_config['google_maps_api_key']) ?>
 <?php
 $googlemaps_script = <<<EOM
+// parse request parameters
+var request = {
+  x: "", y: "", z: "", q: ""
+};
+var params = window.location.search.substr(1).split('&');
+for (var i = 0; i < params.length; i++) {
+  var parts = params[i].split('=');
+
+  var n = parts[0];
+  var v = decodeURIComponent(parts[1]);
+  if ("z" == n)
+  {
+    v = parseInt(v);
+  }
+  request[n] = v;
+}
+var MapType = %s;  // It is not user-inputed values
+
 var gls;
 var gMap;
 function OnLocalSearch() {
     if (!gls.results) return;
     var first = gls.results[0];
     var point = new GLatLng(parseFloat(first.lat), parseFloat(first.lng));
-    var zoom = (%s);
+    var zoom = request.z;
     gMap.addControl(new GSmallMapControl());
     gMap.addControl(new GMapTypeControl());
-    gMap.setMapType((%s));
+    gMap.setMapType(MapType);
     gMap.setCenter(point, zoom);
     var marker = new GMarker(point);
     gMap.addOverlay(marker);
@@ -28,7 +46,7 @@ function OnLocalSearch() {
 }
 function load() {
     if (GBrowserIsCompatible()) {
-        if (((%s) == 0) && ((%s) == 0)){
+        if ((request.x == 0) && (request.y == 0)){
             gMap = new GMap2(document.getElementById('map'));
             gMap.addControl(new GSmallMapControl());
             gMap.addControl(new GMapTypeControl());
@@ -36,16 +54,16 @@ function load() {
             gls = new GlocalSearch();
             gls.setCenterPoint(gMap);
             gls.setSearchCompleteCallback(null, OnLocalSearch);
-            var q = '(%s)';
+            var q = request.q;
             gls.execute(q);
         } else {
-            var point = new GLatLng((%s), (%s));
-            var zoom = (%s);
+            var point = new GLatLng(request.x, request.y);
+            var zoom = request.z;
             gMap = new GMap2(document.getElementById('map'));
             gMap.addControl(new GSmallMapControl());
             gMap.addControl(new GMapTypeControl());
             gMap.setCenter(point, zoom);
-            gMap.setMapType((%s));
+            gMap.setMapType(MapType);
             var marker = new GMarker(point);
             gMap.addOverlay(marker);
             geocoder = new GClientGeocoder();
@@ -53,7 +71,7 @@ function load() {
     }
 }
 EOM;
-echo javascript_tag(sprintf($googlemaps_script, $z, $mapType, $x, $y, $q, $x, $y, $z, $mapType)); ?>
+echo javascript_tag(sprintf($googlemaps_script, $mapType)); ?>
 <?php endif; ?>
 <?php include_stylesheets() ?>
 <?php include_javascripts() ?>
