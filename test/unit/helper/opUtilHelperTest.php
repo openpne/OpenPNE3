d6d26714ce5ff6455646078dccacd01edbc2a73b
ShinichiU <urabe@tejimaya.com>
2010-04-17 01:51:05 +0900

    (fixes #840) add op_link_to_member() unit test.

--- test/unit/helper/opUtilHelperTest.php
+++ test/unit/helper/opUtilHelperTest.php
@@ -1,7 +1,12 @@
 <?php
 
 include_once dirname(__FILE__).'/../../bootstrap/unit.php';
-include_once sfConfig::get('sf_lib_dir').'/helper/opUtilHelper.php';
+include_once dirname(__FILE__) . '/../../bootstrap/database.php';
+
+sfContext::createInstance(ProjectConfiguration::getApplicationConfiguration('pc_frontend', 'test', true));
+
+include_once sfConfig::get('sf_lib_dir').'/vendor/symfony/lib/helper/HelperHelper.php';
+use_helper('opUtil', 'Url', 'Tag');
 
 $t = new lime_test(null, new lime_output_color());
 
@@ -34,3 +39,14 @@ $t->is(op_format_last_login_time($now - 75 * 86400, $now), '3 months ago');
 $t->is(op_format_last_login_time($now - 370 * 86400, $now), 'about 1 year ago');
 $t->is(op_format_last_login_time($now - 4 * 370 * 86400, $now), 'over 4 years ago');
 $t->is(op_format_last_login_time($now - 1000 * 86400, $now), 'over 2 years ago');
+
+//------------------------------------------------------------
+$t->diag('op_link_to_member()');
+$t->is(op_link_to_member(1), link_to('A', '@obj_member_profile?id=1'), 'link to member 1');
+$t->is(op_link_to_member(1, '@obj_friend_unlink'), link_to('A', '@obj_friend_unlink?id=1'), 'link to unlink member 1');
+$t->is(op_link_to_member(1, '@obj_member_profile', array('link_target' => 'tetetete')), link_to('tetetete', '@obj_member_profile?id=1'), 'link to member 1 (free text)');
+$t->is(op_link_to_member(9999), '-', 'set undefine member');
+$t->is(op_link_to_member(null), '-', 'set null member');
+
+Doctrine::getTable('SnsConfig')->set('nickname_of_member_who_does_not_have_credentials', 'I am a pen.');
+$t->is(op_link_to_member(null), 'I am a pen.', 'set nickname_of_member_who_does_not_have_credentials original setting');
