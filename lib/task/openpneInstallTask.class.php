06734d60364893be2f35140002d1417517e002f7
Kousuke Ebihara <ebihara@tejimaya.com>
2010-05-04 05:10:59 +0900

    added an option not to recreate DB in openpne:install (fixes #1026)

--- lib/task/openpneInstallTask.class.php
+++ lib/task/openpneInstallTask.class.php
@@ -19,6 +19,7 @@ class openpneInstallTask extends sfDoctrineBaseTask
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', null),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
       new sfCommandOption('redo', null, sfCommandOption::PARAMETER_NONE, 'Executes a reinstall'),
+      new sfCommandOption('non-recreate-db', null, sfCommandOption::PARAMETER_NONE, 'Non recreate DB'),
     ));
 
     $this->briefDescription = 'Install OpenPNE';
@@ -259,18 +260,115 @@ EOF;
     $task->setConfiguration($this->configuration);
     $task->run(array(), array(
       'no-confirmation' => true,
-      'db'              => true,
+      'db'              => !$options['non-recreate-db'],
       'model'           => true,
       'forms'           => true,
       'filters'         => true,
-      'sql'             => true,
-      'and-load'        => $tmpdir,
+      'sql'             => !$options['non-recreate-db'],
+      'and-load'        => $options['non-recreate-db'] ? null : $tmpdir,
       'application'     => $options['application'],
       'env'             => $options['env'],
     ));
 
+    if ($options['non-recreate-db'])
+    {
+      $connection = Doctrine_Manager::connection();
+
+      $config = $this->getCliConfig();
+      Doctrine_Core::loadModels($config['models_path'], Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
+
+      $tables = array();
+      $relatedTables = array();
+      $droppedTables = array();
+
+      $models = Doctrine_Core::getLoadedModels();
+      foreach ($models as $model)
+      {
+        $table = Doctrine::getTable($model)->getTableName();
+        $tables[] = $table;
+
+        $relations = $connection->import->listTableRelations($table);
+        foreach ($relations as $relation)
+        {
+          if (empty($relatedTables[$relation['table']]))
+          {
+            $relatedTables[$relation['table']] = array();
+          }
+
+          $relatedTables[$relation['table']][] = $table;
+        }
+      }
+
+      // first, non-related tables can be removed
+      $nonRelatedTables = array_diff($tables, array_keys($relatedTables));
+      foreach ($nonRelatedTables as $targetTable)
+      {
+        $droppedTables[] = $targetTable;
+        if ($connection->import->tableExists($targetTable))
+        {
+          $connection->export->dropTable($targetTable);
+        }
+      }
+
+      // second, related tables
+      uasort($relatedTables, create_function('$a, $b', '$_a = count($a); $_b = count($b); if ($_a == $_b) return 0; return ($_a < $_b) ? -1 : 1;'));
+      foreach ($relatedTables as $relatedTable => &$relation)
+      {
+        $this->dropRelations($relatedTable, $relatedTables, $droppedTables);
+      }
+
+      $this->initDb($tmpdir);
+    }
+
     $this->getFilesystem()->remove(sfFinder::type('file')->in(array($tmpdir)));
     $this->getFilesystem()->remove($tmpdir);
+  }
+
+  protected function initDb($tmpdir)
+  {
+    $task = new sfDoctrineBuildSqlTask($this->dispatcher, $this->formatter);
+    $task->setCommandApplication($this->commandApplication);
+    $task->setConfiguration($this->configuration);
+    $task->run();
+
+    $task = new sfDoctrineInsertSqlTask($this->dispatcher, $this->formatter);
+    $task->setCommandApplication($this->commandApplication);
+    $task->setConfiguration($this->configuration);
+    $task->run();
+
+    $task = new sfDoctrineDataLoadTask($this->dispatcher, $this->formatter);
+    $task->setCommandApplication($this->commandApplication);
+    $task->setConfiguration($this->configuration);
+    $task->run(array(
+      'dir_or_file' => $tmpdir,
+    ));
+  }
+
+  protected function dropRelations($target, &$relatedTables, &$droppedTables)
+  {
+    $connection = Doctrine_Manager::connection();
+
+    foreach ($relatedTables[$target] as $relation)
+    {
+      if (in_array($relation, $droppedTables))
+      {
+        continue;
+      }
+
+      if ($target === $relation)  // self-relationship
+      {
+        $connection->execute('TRUNCATE TABLE '.$connection->quoteIdentifier($target));
+        continue;
+      }
+
+      $this->dropRelations($relation, $relatedTables, $droppedTables);
+    }
+
+    $droppedTables[] = $target;
+    if ($connection->import->tableExists($target))
+    {
+      $connection->export->dropTable($target);
+    }
   }
 
   protected function installPlugins()
