77b5097f31e2d5785ada26c6a42f507e4f4e5f17
tunn <tunn@f10.pne.jp>
2010-05-10 19:17:36 +0900

    (fixes #979, BP from #976) add some routing rules

--- apps/pc_frontend/templates/_footer.php
+++ apps/pc_frontend/templates/_footer.php
@@ -1,6 +1,6 @@
 <p>
-<?php echo link_to(__('Privacy Policy'), 'default/privacyPolicy', array('target' => '_blank')); ?> 
-<?php echo link_to(__('Terms of Service'), 'default/userAgreement', array('target' => '_blank')); ?> 
+<?php echo link_to(__('Privacy Policy'), '@privacy_policy', array('target' => '_blank')); ?> 
+<?php echo link_to(__('Terms of Service'), '@terms_of_service', array('target' => '_blank')); ?> 
 <?php $snsConfigSettings = sfConfig::get('openpne_sns_config'); ?>
 <?php if (opToolkit::isSecurePage()) : ?>
 <?php echo Doctrine::getTable('SnsConfig')->get('footer_after', $snsConfigSettings['footer_after']['Default']); ?>
