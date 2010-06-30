d8d791afd6c0b40619760a0035d1763fc7849cb2
tunn <tu3009@gmail.com>
2010-06-30 16:11:53 +0900

    (fixes #1180, BP from #1179) reflect configed community name to entry names

--- apps/pc_backend/modules/community/templates/_communityInfo.php
+++ apps/pc_backend/modules/community/templates/_communityInfo.php
@@ -1,8 +1,8 @@
 <table>
 <tr><th>ID</th><td><?php echo $community->getId() ?></td></tr>
-<tr><th><?php echo __('Community Name') ?></th><td><?php echo $community->getName() ?></td></tr>
-<tr><th><?php echo __('Community Category') ?></th><td><?php echo $community->getCommunityCategory() ?></td></tr>
-<tr><th><?php echo __('Administrator') ?></th><td><?php echo $community->getAdminMember()->getName() ?></td></tr>
+<tr><th><?php echo __('%community% Name', array('%community%' => $op_term['community'])) ?></th><td><?php echo $community->getName() ?></td></tr>
+<tr><th><?php echo __('%community% Category', array('%community%' => $op_term['community'])) ?></th><td><?php echo $community->getCommunityCategory() ?></td></tr>
+<tr><th><?php echo __('%community% Administrator', array('%community%' => $op_term['community'])) ?></th><td><?php echo $community->getAdminMember()->getName() ?></td></tr>
 <?php
 $subAdminCaption = '';
 foreach ($community->getSubAdminMembers() as $m)
@@ -12,7 +12,7 @@ foreach ($community->getSubAdminMembers() as $m)
 <?php if ($subAdminCaption): ?>
 <tr><th><?php echo __('Sub Administrator') ?></th><td><ul><?php echo $subAdminCaption ?></ul></td></tr>
 <?php endif; ?>
-<tr><th><?php echo __('Community Members') ?></th><td><?php echo $community->countCommunityMembers() ?></td></tr>
+<tr><th><?php echo __('%community% Members', array('%community%' => $op_term['community'])) ?></th><td><?php echo $community->countCommunityMembers() ?></td></tr>
 <tr><th><?php echo __('Date Created') ?></th><td><?php echo $community->getCreatedAt() ?></td></tr>
 <?php foreach ($community->getConfigs() as $name => $config): ?>
 <tr><th><?php echo __(op_replace_sns_term($name), array(), 'form_community') ?></th><td><?php echo nl2br($config) ?></td></tr>
