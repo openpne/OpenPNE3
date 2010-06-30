d21c1cbb33fb073e11ff3d5ab493ec35147190b2
tunn <tu3009@gmail.com>
2010-06-30 15:43:57 +0900

    (fixes #1179) reflect configed community name to entry names

--- apps/pc_backend/modules/community/templates/listSuccess.php
+++ apps/pc_backend/modules/community/templates/listSuccess.php
@@ -2,7 +2,7 @@
 <?php include_partial('submenu') ?>
 <?php end_slot() ?>
 
-<?php slot('title', __('コミュニティリスト')); ?>
+<?php slot('title', __('%community%リスト', array('%community%' => $op_term['community']))); ?>
 
 <?php $form->getWidgetSchema()->setLabel("name", __('%community% Name', array('%community%' => $op_term['community']))); ?>
 <?php $form->getWidgetSchema()->setLabel("community_category_id", __('%community% Category', array('%community%' => $op_term['community']))); ?>
@@ -15,7 +15,7 @@
 </form>
 
 <?php if (!$pager->getNbResults()): ?>
-<?php echo __('該当するコミュニティは存在しません。') ?></p>
+<?php echo __('該当する%community%は存在しません。', array('%community%' => $op_term['community'])) ?></p>
 <?php else: ?>
 <?php slot('pager') ?>
 <p><?php op_include_pager_navigation($pager, 'community/list?page=%d', array('use_current_query_string' => true)) ?></p>
@@ -24,12 +24,12 @@
 <?php foreach ($pager->getResults() as $community): ?>
 <?php $moreInfo = array(
   link_to(__('Delete'), 'community/delete?id='.$community->getId()),
-  link_to(__('Make all members join in this community'), 'community/addAllMember?id='.$community->getId())
+  link_to(__('Make all members join in this %community%', array('%community%' => $op_term['community'])), 'community/addAllMember?id='.$community->getId())
 ) ?>
 <?php if (!(bool)$community->getConfig('is_default')): ?>
-<?php $moreInfo[] = link_to(__('Add this community as a default'), 'community/addDefaultCommunity?id='.$community->getId()) ?>
+<?php $moreInfo[] = link_to(__('Add this %community% as a default', array('%community%' => $op_term['community'])), 'community/addDefaultCommunity?id='.$community->getId()) ?>
 <?php else: ?>
-<?php $moreInfo[] = link_to(__('Remove this community from the default'), 'community/removeDefaultCommunity?id='.$community->getId()) ?>
+<?php $moreInfo[] = link_to(__('Remove this %community% from the default', array('%community%' => $op_term['community'])), 'community/removeDefaultCommunity?id='.$community->getId()) ?>
 <?php endif; ?>
 <?php include_partial('community/communityInfo', array(
   'community' => $community, 
