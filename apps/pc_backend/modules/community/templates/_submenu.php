c81a0f2dc433945672379ed2f97ec510079d7ed0
tunn <tu3009@gmail.com>
2010-07-09 22:17:44 +0900

    (fixes #1181, BP from #1179) reflected configed community name to submenu

--- apps/pc_backend/modules/community/templates/_submenu.php
+++ apps/pc_backend/modules/community/templates/_submenu.php
@@ -1,3 +1,3 @@
-<li><?php echo link_to(__('コミュニティリスト'), 'community/list') ?></li>
-<li><?php echo link_to(__('コミュニティカテゴリ設定'), 'community/categoryList') ?></li>
-<li><?php echo link_to(__('初期コミュニティ設定'), 'community/defaultCommunityList') ?></li>
+<li><?php echo link_to(__('%community%リスト', array('%community%' => $op_term['community'])), 'community/list') ?></li>
+<li><?php echo link_to(__('%community%カテゴリ設定', array('%community%' => $op_term['community'])), 'community/categoryList') ?></li>
+<li><?php echo link_to(__('初期%community%設定', array('%community%' => $op_term['community'])), 'community/defaultCommunityList') ?></li>
