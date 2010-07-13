11444bde55eb42a25edf76bba5797e2686b41cce
Shogo Kawahara <kawahara@bucyou.net>
2010-07-13 18:16:44 +0900

    removed some unnecessarily mail templates (fixes #1309)

--- apps/pc_backend/templates/mail/_reissuedPassword.php
+++
@@ -1,6 +0,0 @@
-<?php echo __('The password was reissued.') ?>
-
-
-<?php echo __('MailAddress') ?>: <?php echo $mailAddress ?>
-
-<?php echo __('New Password') ?>: <?php echo $newPassword ?>
--- apps/pc_backend/templates/mail/_requestRegisterURL.php
+++
@@ -1,7 +0,0 @@
-以下のURLから<?php echo $op_config['sns_name'] ?>に登録してください。
-
-<?php if ($isMobile) : ?>
-<?php echo app_url_for('mobile_frontend', sprintf('opAuthMailAddress/register?token=%s&authMode=%s', $token, $authMode), true) ?>
-<?php else: ?>
-<?php echo app_url_for('pc_frontend', sprintf('opAuthMailAddress/register?token=%s&authMode=%s', $token, $authMode), true) ?>
-<?php endif; ?>
