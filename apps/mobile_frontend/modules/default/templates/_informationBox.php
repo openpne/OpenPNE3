9daef1319867e9969f917e7eab5edd27a78c8f4c
Shogo Kawahara <kawahara@bucyou.net>
2010-07-14 16:17:24 +0900

    fixed a template of default/informationBox component, because it had escaped in the HTML entities about body (fixes #1255, BP from #1020)

--- apps/mobile_frontend/modules/default/templates/_informationBox.php
+++ apps/mobile_frontend/modules/default/templates/_informationBox.php
@@ -1,3 +1,3 @@
 <?php
-op_include_parts('informationBox', 'information', array('body' => $sf_data->getRaw('gadget')->getConfig('value')))
+op_include_parts('informationBox', 'information', array('body' => $gadget->getRawValue()->getConfig('value')))
 ?>
