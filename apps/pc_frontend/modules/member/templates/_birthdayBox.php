c1abad7d79359cbfc89c7490b3b0b97ac2c8b0c5
Masato Nagasawa <nagasawa@tejimaya.com>
2010-07-16 16:18:23 +0900

    fixed a bithday image using op_image_tag(). (fixes #623)

--- apps/pc_frontend/modules/member/templates/_birthdayBox.php
+++ apps/pc_frontend/modules/member/templates/_birthdayBox.php
@@ -2,16 +2,16 @@
 <?php if ($targetDay === 0): ?>
 <div class="parts birthday">
 <?php if ($sf_request->hasParameter('id')): ?>
-<?php $img = public_path('images/birthday_f.gif') ?>
+<?php $img = 'birthday_f.gif' ?>
 <?php else: ?>
-<?php $img = public_path('images/birthday_h.gif') ?>
+<?php $img = 'birthday_h.gif' ?>
 <?php endif ?>
-<img src="<?php echo $img ?>" alt="Happy Birthday!" />
+<?php echo op_image_tag($img, array('alt' => 'Happy Birthday!')) ?>
 </div>
 <?php else: ?>
 <?php if ($sf_request->hasParameter('id') && 0 < $targetDay && $targetDay <= 3): ?>
 <div class="parts birthday">
-<img src="<?php echo public_path('images/birthday_f_2.gif') ?>" alt="" />
+<?php echo op_image_tag('birthday_f_2.gif') ?>
 </div>
 <?php endif ?>
 <?php endif ?>
