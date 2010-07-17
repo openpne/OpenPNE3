34087dd63d89d21c3b7c3e403b3fec9d6ac7ed52
Kousuke Ebihara <ebihara@tejimaya.com>
2010-07-17 18:55:02 +0900

    fixed the translations for community/_submenu in pc_backend is short (fixes #1179)

--- apps/pc_backend/modules/community/templates/_submenu.php
+++ apps/pc_backend/modules/community/templates/_submenu.php
@@ -1,3 +1,3 @@
-<li><?php echo link_to(__('%community%リスト', array('%community%' => $op_term['community'])), 'community/list') ?></li>
-<li><?php echo link_to(__('%community%カテゴリ設定', array('%community%' => $op_term['community'])), 'community/categoryList') ?></li>
-<li><?php echo link_to(__('初期%community%設定', array('%community%' => $op_term['community'])), 'community/defaultCommunityList') ?></li>
+<li><?php echo link_to(__('%community% List', array('%community%' => $op_term['community']->titleize())), 'community/list') ?></li>
+<li><?php echo link_to(__('%community% Category Configuration', array('%community%' => $op_term['community']->titleize())), 'community/categoryList') ?></li>
+<li><?php echo link_to(__('Default %community% Configuration', array('%community%' => $op_term['community']->titleize())), 'community/defaultCommunityList') ?></li>
