2c27d0fc3dc6facc5c5381e0da27479ba62b25b3
tunn <tu3009@gmail.com>
2010-07-21 11:10:14 +0900

    removed meaningless spaces (fixes #1207, BP from #1204)

--- lib/form/opPresetProfileForm.class.php
+++ lib/form/opPresetProfileForm.class.php
@@ -67,7 +67,7 @@ class opPresetProfileForm extends ProfileForm
 
     foreach ($list as $k => $v)
     {
-      if ($this->getObject()->getName() === 'op_preset_'.$v['Name'] || !Doctrine::getTable('Profile')->retrieveByName('op_preset_'.$v['Name']))     
+      if ($this->getObject()->getName() === 'op_preset_'.$v['Name'] || !Doctrine::getTable('Profile')->retrieveByName('op_preset_'.$v['Name']))
       {
         $result[$k] = $i18n->__($v['Caption']);
       }
