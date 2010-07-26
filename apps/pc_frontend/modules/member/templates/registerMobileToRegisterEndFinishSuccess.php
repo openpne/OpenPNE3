0c68707fae06a773788a7cbedb6b24ef631968c3
Tajima Itsuro <tajima@tejimaya.com>
2010-07-26 15:35:07 +0900

    (fixes #1220 BP from #1059) fixed registration flow in case of require UID

---
+++ apps/pc_frontend/modules/member/templates/registerMobileToRegisterEndFinishSuccess.php
@@ -0,0 +1,4 @@
+<?php op_include_box('finish', __('When mobile information registration is done, the member registration is completed.').'<br />'.__('Sent a mail for your mobile e-mail address. Begin your registration from a URL in the mail.'), array('title' => __('Mobile E-mail Address Registration Completed'))); ?>
+
+
+
