b1b65df4e4e45ef6507bae2013bba3d04158b81d
ShinichiU <urabe@tejimaya.com>
2010-07-28 18:53:50 +0900

    (fixes #1429, BP from #1432) fixed backend monitoring pager bug for fileList.

--- apps/pc_backend/modules/monitoring/templates/fileListSuccess.php
+++ apps/pc_backend/modules/monitoring/templates/fileListSuccess.php
@@ -15,13 +15,14 @@ $params['title'] = __('表示件数');
 $params['params'] = array(20, 50, 100, 500);
 $params['unit'] = '件';
 $params['name'] = 'size';
+$params['default'] = $size;
 include_partial('global/changePageSize', array('params' => $params));
 ?>
 
-<p><?php op_include_pager_navigation($pager, 'monitoring/fileList?page=%d') ?></p>
+<p><?php op_include_pager_navigation($pager, 'monitoring/fileList?page=%d&size='.$size) ?></p>
 <div class="fileListTable">
 <?php include_partial('fileInfo', array('files' => $pager->getResults(), 'deleteBtn' => true)) ?>
 </div>
 <br class="clear"/>
-<p><?php op_include_pager_navigation($pager, 'monitoring/fileList?page=%d') ?></p>
+<p><?php op_include_pager_navigation($pager, 'monitoring/fileList?page=%d&size='.$size) ?></p>
 <?php endif; ?>
