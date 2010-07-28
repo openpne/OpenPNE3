f4cf1220473dc2d33ab4d96866d24222322aaf20
Shogo Kawahara <kawahara@bucyou.net>
2010-07-28 19:49:29 +0900

    removed unnecesarry lines in member/profile on the pc_frontend (fixes #1417,BP from #1416)

--- apps/pc_frontend/modules/member/templates/profileSuccess.php
+++ apps/pc_frontend/modules/member/templates/profileSuccess.php
@@ -8,7 +8,6 @@
 <?php endif; ?>
 <?php endforeach; ?>
 <?php end_slot() ?>
-<?php echo sortable_element('Top', array('tag' => 'div', 'handle' => 'partsHeading', 'onChange' => 'function(obj){storeSort(obj)}')) ?>
 <?php endif; ?>
 
 <?php if (isset($sideMenuGadgets)): ?>
@@ -19,7 +18,6 @@
 <?php endif; ?>
 <?php endforeach; ?>
 <?php end_slot() ?>
-<?php echo sortable_element('Left', array('tag' => 'div', 'handle' => 'partsHeading', 'onChange' => 'function(obj){storeSort(obj)}')) ?>
 <?php endif; ?>
 
 <?php if (isset($contentsGadgets)): ?>
@@ -28,7 +26,6 @@
 <?php include_component($gadget->getComponentModule(), $gadget->getComponentAction(), array('gadget' => $gadget)); ?>
 <?php endif; ?>
 <?php endforeach; ?>
-<?php echo sortable_element('Center', array('tag' => 'div', 'handle' => 'partsHeading', 'onChange' => 'function(obj){storeSort(obj)}')) ?>
 <?php endif; ?>
 
 <?php if (isset($bottomGadgets)): ?>
@@ -39,7 +36,6 @@
 <?php endif; ?>
 <?php endforeach; ?>
 <?php end_slot() ?>
-<?php echo sortable_element('Bottom', array('tag' => 'div', 'handle' => 'partsHeading')) ?>
 <?php endif; ?>
 
 <?php slot('op_top'); ?>
@@ -69,87 +65,3 @@
 <?php endforeach; ?>
 <?php endif; ?>
 <?php end_slot(); ?>
-
-<?php echo javascript_tag('
-function storeSort(obj)
-{
-  var result = "";
-  Element.childElements(obj.parentNode).each(function(child, index){
-    if (child.id && child.id.match(/_[0-9]+$/)) {
-      if (result) {
-        result = result + ",";
-      }
-      result = result + child.id;
-    }
-  });
-  var path = "'.($sf_request->getRelativeUrlRoot() ? $sf_request->getRelativeUrlRoot() : '/').'";
-  var expires = new Date();
-  expires.setTime((new Date()).getTime() + (10 * 12 * 30 * 24 * 60 * 60 * 1000));
-  var pos = obj.parentNode.id;
-
-  opCookie.set("HomeGadget_" + pos + "_sort", result, expires, path);
-}
-
-function foldObj(obj, display)
-{
-  Element.childElements(obj.parentNode).each(function(child, index){
-    if (!child.hasClassName("partsHeading")) {
-      if (display == null) {
-        child.toggle();
-      } else {
-        if (display == "true") {
-          child.show();
-        } else {
-          child.hide();
-        }
-      }
-
-      var size = Element.childElements(obj.parentNode).length;
-      if (size == index + 1) {  // It is a last loop maybe
-        var path = "'.($sf_request->getRelativeUrlRoot() ? $sf_request->getRelativeUrlRoot() : '/').'";
-        var id = child.parentNode.parentNode.id;
-        var expires = new Date();
-        expires.setTime((new Date()).getTime() + (10 * 12 * 30 * 24 * 60 * 60 * 1000));
-        opCookie.set("HomeGadget_" + id + "_toggle", child.visible(), expires, path);
-      }
-    }
-  });
-}
-
-$$(".partsHeading").each(function(obj){
-  // folding
-  Event.observe(obj, "dblclick", function(e){
-    foldObj(obj);
-  });
-  var id = obj.parentNode.parentNode.id;
-  var display = opCookie.get("HomeGadget_"+id+"_toggle");
-  if (display != null) {
-    foldObj(obj, display);
-  }
-
-});
-
-["Top", "Left", "Center"].each(function(type){
-  var sortInfo = opCookie.get("HomeGadget_" + type + "_sort");
-  if (sortInfo)
-  {
-    var obj = document.getElementById(type);
-    var preGadget = null;
-    sortInfo.split(",").each(function(value){
-      var gadget = document.getElementById(value);
-      if (preGadget)
-      {
-        Element.remove(gadget);
-        Insertion.After(preGadget, gadget);
-      }
-      else
-      {
-        Element.remove(gadget);
-        Insertion.Top(obj, gadget);
-      }
-      preGadget = gadget;
-    });
-  }
-});
-
-') ?>
