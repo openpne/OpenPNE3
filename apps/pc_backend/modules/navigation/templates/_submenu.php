c897c74ebb3518cf626aaa6087afe35a2d184d4f
Masato Nagasawa <nagasawa@tejimaya.com>
2010-07-30 17:29:26 +0900

    added navigation translation catalog (fixes #1270 BP from #1088)

--- apps/pc_backend/modules/navigation/templates/_submenu.php
+++ apps/pc_backend/modules/navigation/templates/_submenu.php
@@ -1,3 +1,3 @@
-<li><?php echo link_to(__('PC frontend'), 'navigation/list') ?></li>
-<li><?php echo link_to(__('Mobile frontend'), 'navigation/list?app=mobile') ?></li>
-<li><?php echo link_to(__('Administration'), 'navigation/list?app=backend') ?></li>
+<li><?php echo link_to(__('PC frontend', array(), 'navigation'), 'navigation/list') ?></li>
+<li><?php echo link_to(__('Mobile frontend', array(), 'navigation'), 'navigation/list?app=mobile') ?></li>
+<li><?php echo link_to(__('Administration', array(), 'navigation'), 'navigation/list?app=backend') ?></li>
