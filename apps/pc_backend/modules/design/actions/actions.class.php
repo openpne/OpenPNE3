b9b7df808b55903614037ea5189e2aafa9c0db69
Masato Nagasawa <nagasawa@tejimaya.com>
2010-08-10 23:14:30 +0900

    fixed CSRF at delete of banner action in the backend.

--- apps/pc_backend/modules/design/actions/actions.class.php
+++ apps/pc_backend/modules/design/actions/actions.class.php
@@ -406,6 +406,7 @@ class designActions extends sfActions
 
     if ($request->isMethod(sfWebRequest::POST))
     {
+      $request->checkCSRFProtection();
       $banner->delete();
       $this->redirect('design/banner');
     }
--- apps/pc_backend/modules/design/templates/bannerdeleteSuccess.php
+++ apps/pc_backend/modules/design/templates/bannerdeleteSuccess.php
@@ -2,8 +2,12 @@
 <?php include_partial('submenu'); ?>
 <?php end_slot() ?>
 
+<?php $form = new BaseForm() ?>
 <h2><?php echo __('Delete a banner image') ?></h2>
 <p><?php echo __('Delete truly this banner image?') ?></p>
 <form action="" method="post">
-<td colspan="2"><input type="submit" value="<?php echo __('Delete') ?>" /></td>
+<td colspan="2">
+<input type="hidden" name="<?php echo $form->getCSRFFieldName() ?>" value="<?php echo $form->getCSRFToken() ?>" />
+<input type="submit" value="<?php echo __('Delete') ?>" />
+</td>
 </form>
