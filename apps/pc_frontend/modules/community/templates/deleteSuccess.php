d3c43ca4e70055832e02a7efd425e2226828f38b
Masato Nagasawa <nagasawa@tejimaya.com>
2010-08-10 19:02:23 +0900

    fixed CSRF at the delete action of community.

--- apps/pc_frontend/modules/community/templates/deleteSuccess.php
+++ apps/pc_frontend/modules/community/templates/deleteSuccess.php
@@ -1,7 +1,9 @@
 <?php
+$form = new BaseForm();
 op_include_parts('yesNo', 'deleteConfirmForm', array(
   'title' => __('Do you delete this %community%?'),
-  'yes_form' => '<input type="hidden" name="is_delete">',
+  'yes_form' => '<input type="hidden" name="is_delete"/>'
+              . '<input type="hidden" name="'.$form->getCSRFFieldName().'" value="'.$form->getCSRFToken().'"/>',
   'button' => __('Delete'),
 ))
 ?>
