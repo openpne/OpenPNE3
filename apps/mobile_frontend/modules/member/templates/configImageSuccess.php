e7a375c13c318d2b51771e3b238118ae3743f9ba
Masato Nagasawa <nagasawa@tejimaya.com>
2010-08-10 19:27:31 +0900

    fixed CSRF of delete image action in the home.

--- apps/mobile_frontend/modules/member/templates/configImageSuccess.php
+++ apps/mobile_frontend/modules/member/templates/configImageSuccess.php
@@ -3,6 +3,8 @@
 <?php $_member = $sf_user->getMember() ?>
 <?php $images = $_member->getMemberImage() ?>
 <?php if ($images->count()): ?>
+<?php $form = new sfForm() ?>
+<?php $csrfToken = '&'.$form->getCSRFFieldName().'='.$form->getCSRFToken() ?>
 <?php foreach ($images as $image) : ?>
 <?php echo op_image_tag_sf_image($image->getFile(), array('size' => '120x120', 'format' => 'jpg')) ?><br>
 <?php echo sprintf('[%s]',link_to(__('Expansion'), sf_image_path($image->getFile(), array('size' => '320x320', 'format' => 'jpg')))) ?><br>
@@ -13,7 +15,7 @@ if ($image->getIsPrimary())
 }
 else
 {
-  $main = link_to(__('Main Photo'), 'member/changeMainImage?member_image_id='.$image->getId());
+  $main = link_to(__('Main Photo'), 'member/changeMainImage?member_image_id='.$image->getId().$csrfToken);
 }
 ?>
 <?php echo sprintf('[%s|%s]', link_to(__('Delete'), 'member/deleteImage?member_image_id='.$image->getId()), $main) ?>
