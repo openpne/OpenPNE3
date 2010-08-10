c1c866c45a32fb773af2e6379289e704b55a1450
Masato Nagasawa <nagasawa@tejimaya.com>
2010-08-10 18:48:24 +0900

    fixed syntax error at delete of community form.

--- apps/mobile_frontend/modules/community/templates/deleteSuccess.php
+++ apps/mobile_frontend/modules/community/templates/deleteSuccess.php
@@ -3,7 +3,7 @@
 $form = new sfForm();
 op_include_parts('yesNo', 'deleteConfirmForm', array(
   'body' => __('Do you delete this community?'),
-  'yes_form' => '<input type="hidden" name="is_delete">',
+  'yes_form' => '<input type="hidden" name="is_delete">'
               . '<input type="hidden" name="'.$form->getCSRFFieldName().'" value="'.$form->getCSRFToken().'">',
   'button' => __('Delete'),
 ))
