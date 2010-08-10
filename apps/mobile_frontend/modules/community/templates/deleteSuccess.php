7884dc4aeb877c0437fc857aa4c3f5cb09ca048f
Masato Nagasawa <nagasawa@tejimaya.com>
2010-08-10 16:52:35 +0900

    fixed CSRF at delete of community action in the mobile.

--- apps/mobile_frontend/modules/community/templates/deleteSuccess.php
+++ apps/mobile_frontend/modules/community/templates/deleteSuccess.php
@@ -1,8 +1,10 @@
 <?php op_mobile_page_title(__('Delete this %community%'), $community->getName()) ?>
 <?php
+$form = new BaseForm();
 op_include_parts('yesNo', 'deleteConfirmForm', array(
   'body' => __('Do you delete this %community%?'),
-  'yes_form' => '<input type="hidden" name="is_delete">',
+  'yes_form' => '<input type="hidden" name="is_delete">'
+              . '<input type="hidden" name="'.$form->getCSRFFieldName().'" value="'.$form->getCSRFToken().'">',
   'button' => __('Delete'),
 ))
 ?>
