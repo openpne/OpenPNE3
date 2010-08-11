8b9c7fedc99657c3141214e8ff8e13cf024b4058
Kousuke Ebihara <ebihara@tejimaya.com>
2010-08-11 11:26:04 +0900

    fixed sorting of navigations that doesn't require a token for doing action (refs #1497)

--- apps/pc_backend/modules/navigation/templates/listSuccess.php
+++ apps/pc_backend/modules/navigation/templates/listSuccess.php
@@ -49,7 +49,8 @@
 <?php echo sortable_element('type_'.str_replace(' ', '_', $type), array(
   'tag'  => 'tbody',
   'only' => 'sortable',
-  'url'  => 'navigation/sort'
+  'url'  => 'navigation/sort',
+  'with' => 'Sortable.serialize("type_'.str_replace(' ', '_', $type).'")+"&'.urlencode($sortForm->getCSRFFieldName()).'='.urlencode($sortForm->getCSRFToken()).'"',
 )) ?>
 
 <?php endforeach; ?>
