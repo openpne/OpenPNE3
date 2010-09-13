03b26f1ee703de55cce21aa66e3c6119b2178142
Kousuke Ebihara <ebihara@tejimaya.com>
2010-09-13 20:00:05 +0900

    fixed registration phase that doesn't register i-mode ID (fixes #905)

--- apps/mobile_frontend/modules/member/templates/registerMobileToRegisterEndSuccess.php
+++ apps/mobile_frontend/modules/member/templates/registerMobileToRegisterEndSuccess.php
@@ -3,7 +3,7 @@
 <?php echo __('You register mobile UID.') ?><br>
 <?php echo __('Prease input your password, press "%1%" button.', array('%1%' => __('Register'))) ?><br>
 
-<form action="<?php echo url_for('member/registerMobileToRegisterEnd?token='.$sf_params->get('token').'&id='.$sf_params->get('id')) ?>" method="post" utn>
+<form action="<?php echo url_for('member/registerMobileToRegisterEnd?token='.$sf_params->get('token').'&id='.$sf_params->get('id')).'?guid=on' ?>" method="post" utn>
 <?php echo $form ?>
 <input type="submit" value="<?php echo __('Register') ?>">
 </form>
