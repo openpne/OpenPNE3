5568000946ef4cbab16fe0b9179c3606dbfdd569
Tajima Itsuro <tajima@tejimaya.com>
2010-09-29 20:29:17 +0900

    (fixes #1407, BP from #1372) fixed to output error message on mail not sent
    Conflicts:
    apps/mobile_frontend/i18n/messages.ja.xml
    apps/pc_backend/i18n/messages.ja.xml

---
+++ apps/pc_backend/modules/default/actions/mailErrorAction.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * mail error actions.
+ *
+ * @package    OpenPNE
+ * @subpackage default
+ * @author     Itsuro Tajima <tajima@tejimaya.com>
+ */
+class mailErrorAction extends sfAction
+{
+ /**
+  * Executes error action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function execute($request)
+  {
+  }
+}
