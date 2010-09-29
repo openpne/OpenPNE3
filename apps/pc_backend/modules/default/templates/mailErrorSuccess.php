dfa0a8524edd1d05c94d600499260acdfee31283
Tajima Itsuro <tajima@tejimaya.com>
2010-09-29 20:29:17 +0900

    fixed to output error message on mail not sent
    Conflicts:
    apps/mobile_frontend/i18n/messages.ja.xml
    apps/pc_backend/i18n/messages.ja.xml
    
    Conflicts:
    apps/mobile_frontend/i18n/messages.ja.xml
    
    (cherry picked from commit 55680009)

---
+++ apps/pc_backend/modules/default/templates/mailErrorSuccess.php
@@ -0,0 +1,4 @@
+<p><?php echo __('Couldn\'t send E-mail. Please retry or contact to administrator.') ?></p>
+
+<?php use_helper('Javascript') ?>
+<p><?php echo link_to_function(__('Back to previous page'), 'history.back()') ?></p>
