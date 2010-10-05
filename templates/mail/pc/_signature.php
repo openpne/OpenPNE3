643f6a0ae344f8cd1b67b0c7cbc0f29654f251ea
Masato Nagasawa <nagasawa@tejimaya.com>
2010-10-05 15:44:55 +0900

    Revert "fixed translated to English in the mail template of signature. (fixes #1619)"
    This reverts commit ef23471d94e1f0904f77fde05a9461606bb071ac.

--- templates/mail/pc/_signature.php
+++ templates/mail/pc/_signature.php
@@ -1,11 +1,11 @@
-<?php echo __('*+--------------------------------------------------------')."\n" ?>
+■□━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-<?php echo __('Feedback and Suggestions Feel free to us!')."\n" ?>
+ご意見、ご感想などはお気軽にどうぞ！
 
 <?php echo $op_config['sns_name']."\n" ?>
-<?php echo __('URL') ?>: <?php echo sfConfig::get('op_base_url')."\n" ?>
-<?php echo __('Contact us') ?>: <?php echo $op_config['admin_mail_address']."\n" ?>
+URL　　　　　： <?php echo sfConfig::get('op_base_url')."\n" ?>
+お問い合わせ ： <?php echo $op_config['admin_mail_address']."\n" ?>
 
-<?php echo __('Unapproved reproduction of the contents written to this mail, Unauthorized copying is prohibited.') ?>
+このメールに書かれた内容の無断転載、無断複製を禁じます。
 
-<?php echo __('--------------------------------------------------------+*') ?>
+━━━━━━━━━━━━━━━━━━━━━━━━━━━□■
