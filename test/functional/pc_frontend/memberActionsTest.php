8e5da56988dfa09feef6ab4d3d0716a8798ef60a
Kousuke Ebihara <ebihara@tejimaya.com>
2010-10-20 15:35:12 +0900

    fixed test for uploading member profile image doesn't work (refs #1741)

--- test/functional/pc_frontend/memberActionsTest.php
+++ test/functional/pc_frontend/memberActionsTest.php
@@ -2,45 +2,53 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$filePath = sfConfig::get('sf_web_dir').'/images/skin_header_logo.jpg';
+$filePath = sfConfig::get('sf_web_dir').'/images/dummy.gif';
 
-$browser = new sfTestFunctional(new opBrowser(), new lime_test(null, new lime_output_color()));
-$params = array('authMailAddress' => array(
-  'mail_address' => 'sns@example.com',
-  'password'     => 'password',
-));
+$browser = new opTestFunctional(new opBrowser(), new lime_test(null, new lime_output_color()));
 $browser
+  ->login('sns@example.com', 'password')
+
+  ->info('member/configImage')
   ->info('1. When an user tries to post more than 4 photos, he gets an error message. (ref. #3544)')
 
   ->info('1st Post')
-  ->post('/member/login/authMode/MailAddress', $params)
   ->get('member/configImage')
   ->click('アップロードする', array('member_image' => array(
-    'file' => array('name' => $filePath, 'type' => 'image/jpeg'),
+    'file' => array('name' => $filePath, 'type' => 'image/gif'),
   )))
-  ->isStatusCode(302)
+  ->with('response')->begin()
+    ->isStatusCode(302)
+  ->end()
   ->followRedirect()
 
   ->info('2nd Post')
   ->click('アップロードする', array('member_image' => array(
-    'file' => array('name' => $filePath, 'type' => 'image/jpeg'),
+    'file' => array('name' => $filePath, 'type' => 'image/gif'),
   )))
-  ->isStatusCode(302)
+  ->with('response')->begin()
+    ->isStatusCode(302)
+  ->end()
   ->followRedirect()
 
   ->info('3rd Post')
   ->click('アップロードする', array('member_image' => array(
-    'file' => array('name' => $filePath, 'type' => 'image/jpeg'),
+    'file' => array('name' => $filePath, 'type' => 'image/gif'),
   )))
-  ->isStatusCode(302)
+  ->with('response')->begin()
+    ->isStatusCode(302)
+  ->end()
   ->followRedirect()
 
   ->info('4th Post')
   ->click('アップロードする', array('member_image' => array(
-    'file' => array('name' => $filePath, 'type' => 'image/jpeg'),
+    'file' => array('name' => $filePath, 'type' => 'image/gif'),
   )))
-  ->isStatusCode(200)
   ->with('response')->begin()
-  ->checkElement('#flashError td:contains("これ以上画像を追加できません。")', true)
+    ->isStatusCode(302)
+  ->end()
+  ->followRedirect()
+  ->with('response')->begin()
+    ->isStatusCode(200)
+    ->checkElement('#flashError td:contains("これ以上画像を追加できません。")', true)
   ->end()
 ;
