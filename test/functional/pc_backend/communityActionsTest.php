1f9b9ed1e30be79a51e8bad5ea8e920701b348bc
Masato Nagasawa <nagasawa@tejimaya.com>
2010-11-03 02:07:26 +0900

    added community of the pc_backend  to the functional test (refs #1741) (cherry picked from commit 65e82c9d909643dfee8dad9eb718cfb0cbac8662)

--- test/functional/pc_backend/communityActionsTest.php
+++ test/functional/pc_backend/communityActionsTest.php
@@ -1,19 +1,84 @@
-<?php
-
+<?php 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new opTestFunctional(new opBrowser(), new lime_test(null, new lime_output_color()));
+$browser
+  ->info('Login')
+  ->get('/')
+  ->click('ログイン', array('admin_user' => array(
+    'username' => 'admin',
+    'password' => 'password',
+  )))
 
-$browser->
-  get('/community/index')->
+// CSRF
+  ->info('/community/categoryList - CSRF')
+  ->post('/community/categoryList')
+  ->checkCSRF()
 
-  with('request')->begin()->
-    isParameter('module', 'community')->
-    isParameter('action', 'index')->
-  end()->
+  ->info('/community/categoryEdit/id/1 - CSRF')
+  ->post('/community/categoryEdit/id/1')
+  ->checkCSRF()
 
-  with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
-  end()
+  ->info('/community/categoryDelete/id/1 - CSRF')
+  ->post('/community/categoryDelete/id/1')
+  ->checkCSRF()
+
+  ->info('/community/addDefaultCommunity/id/1 - CSRF')
+  ->post('/community/addDefaultCommunity/id/1')
+  ->checkCSRF()
+
+  ->info('/community/removeDefaultCommunity/id/1 - CSRF')
+  ->post('/community/removeDefaultCommunity/id/1')
+  ->checkCSRF()
+
+  ->info('/community/addAllMember/id/1 - CSRF')
+  ->post('/community/addAllMember/id/1')
+  ->checkCSRF()
+
+  ->info('/community/delete/id/1 - CSRF')
+  ->post('/community/delete/id/1')
+  ->checkCSRF()
+
+// XSS
+  ->info('/community/list - XSS')
+  ->get('/community/list')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name')
+  ->end()
+
+  ->info('/community/defaultCommunityList - XSS')
+  ->get('/community/defaultCommunityList')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name2')
+  ->end()
+
+  ->info('/community/addDefaultCommunity/id/1055 - XSS')
+  ->get('/community/addDefaultCommunity/id/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name')
+  ->end()
+
+  ->info('/community/removeDefaultCommunity/id/1056 - XSS')
+  ->get('/community/removeDefaultCommunity/id/1056')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name2')
+  ->end()
+
+  ->info('/community/addAllMember/id/1055 - XSS')
+  ->get('/community/addAllMember/id/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name')
+  ->end()
+
+  ->info('/community/delete/id/1055 - XSS')
+  ->get('/community/delete/id/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name')
+  ->end()
 ;
