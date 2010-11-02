a433f356410c81eea31c7e2d7385058b1200f4a8
Masato Nagasawa <nagasawa@tejimaya.com>
2010-11-03 02:57:27 +0900

    added member of the backend actions to the functional test (refs #1741)

--- test/functional/pc_backend/memberActionsTest.php
+++ test/functional/pc_backend/memberActionsTest.php
@@ -5,7 +5,7 @@ $filePath = sfConfig::get('sf_web_dir').'/images/test.png';
 $fileParams = array('member_image' => array('file' => array('name' => $filePath, 'type' => 'image/png')));
 
 $browser = new opTestFunctional(new opBrowser(), new lime_test(null, new lime_output_color()));
-echo $browser
+$browser
   ->info('Login')
   ->get('/')
   ->click('ログイン', array('admin_user' => array(
@@ -13,23 +13,54 @@ echo $browser
     'password' => 'password',
   )))
 
+// CSRF
   ->info('/member/delete/id/2 - CSRF')
-  ->post('/member/delete/id/2', array())
+  ->post('/member/delete/id/2')
   ->checkCSRF()
 
   ->info('/member/reject/id/2 - CSRF')
-  ->post('/member/reject/id/2', array())
+  ->post('/member/reject/id/2')
   ->checkCSRF()
 
   ->info('/member/reissuePassword/id/2 - CSRF')
-  ->post('/member/reissuePassword/id/2', array())
+  ->post('/member/reissuePassword/id/2')
   ->checkCSRF()
 
   ->info('/member/blacklist/uid - CSRF')
-  ->post('/member/blacklist/uid', array())
+  ->post('/member/blacklist/uid')
+  ->checkCSRF()
+
+  ->info('/member/blacklistDelete/id/1 - CSRF')
+  ->post('/member/blacklistDelete/id/1')
   ->checkCSRF()
 
   ->info('/member/invite - CSRF')
-  ->post('/member/invite', array())
+  ->post('/member/invite')
   ->checkCSRF()
+
+// XSS
+  ->info('/member/list - XSS')
+  ->get('/member/list')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('MemberProfile', 'value')
+  ->end()
+
+  ->info('/member/delete/id/1055 - XSS')
+  ->get('/member/delete/id/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/member/reissuePassword/id/1055 - XSS')
+  ->get('/member/reissuePassword/id/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/member/reject/id/1055 - XSS')
+  ->get('/member/reject/id/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
 ;
