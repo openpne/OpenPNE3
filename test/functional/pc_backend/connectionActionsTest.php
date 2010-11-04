b464b7f4cdbcbf88b85cbb9e490e55f461971904
Masato Nagasawa <nagasawa@tejimaya.com>
2010-11-04 15:52:30 +0900

    added the connection actions of pc_backend for the functional test (refs #1741)

--- test/functional/pc_backend/connectionActionsTest.php
+++ test/functional/pc_backend/connectionActionsTest.php
@@ -2,18 +2,30 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new opTestFunctional(new sfBrowser(), new lime_test(null, new lime_output_color()));
+$browser
+  ->info('Login')
+  ->get('/default/login')
+  ->click('ログイン', array('admin_user' => array(
+    'username' => 'admin',
+    'password' => 'password',
+  )))
+  ->isStatusCode(302)
 
-$browser->
-  get('/connection/index')->
+// CSRF
+  ->info('/connection - CSRF')
+  ->post('/connection')
+  ->checkCSRF()
 
-  with('request')->begin()->
-    isParameter('module', 'connection')->
-    isParameter('action', 'index')->
-  end()->
+  ->info('/connection/1 - CSRF')
+  ->post('/connection/1')
+  ->checkCSRF()
 
-  with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
-  end()
+  ->info('/connection/1/delete - CSRF')
+  ->post('/connection/1/delete')
+  ->checkCSRF()
+
+  ->info('//connection/removeToken/id/1 - CSRF')
+  ->post('/connection/removeToken/id/1')
+  ->checkCSRF()
 ;
