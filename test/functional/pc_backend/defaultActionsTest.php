3ba33f607cd38a382f580ceca4fa64154c669c0f
Masato Nagasawa <nagasawa@tejimaya.com>
2010-11-05 11:16:41 +0900

    added the default actions of pc_backend for the functional test (refs #1817 BP from #1741)

--- test/functional/pc_backend/defaultActionsTest.php
+++ test/functional/pc_backend/defaultActionsTest.php
@@ -1,19 +1,17 @@
 <?php
-
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+$browser = new opTestFunctional(new opBrowser(), new lime_test(null, new lime_output_color()));
+$browser
+  ->info('Login')
+  ->get('/')
+  ->click('ログイン', array('admin_user' => array(
+    'username' => 'admin',
+    'password' => 'password',
+  )))
 
-$browser->
-  get('/default/index')->
-
-  with('request')->begin()->
-    isParameter('module', 'default')->
-    isParameter('action', 'index')->
-  end()->
-
-  with('response')->begin()->
-    isStatusCode(200)->
-    checkElement('body', '!/This is a temporary page/')->
-  end()
+// CSRF
+  ->info('/default/login - CSRF')
+  ->post('/default/login')
+  ->checkCSRF()
 ;
