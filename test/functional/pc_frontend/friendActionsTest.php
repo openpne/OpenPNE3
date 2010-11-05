32d8fa89929b9c6780c3cd27735d0759adf9f94a
Masato Nagasawa <nagasawa@tejimaya.com>
2010-11-05 15:10:23 +0900

    added the friend actions of pc_frontend for the functional test (refs #1741)

--- test/functional/pc_frontend/friendActionsTest.php
+++ test/functional/pc_frontend/friendActionsTest.php
@@ -8,13 +8,59 @@ $browser
   ->login('sns@example.com', 'password')
   ->isStatusCode(302)
 
+// CSRF
+  ->info('/friend/link/id/6 - CSRF')
   ->post('/friend/link/id/6')
   ->checkCSRF()
 
-  ->post('/friend/unlink/2')
+  ->info('/friend/unlink/4 - CSRF')
+  ->post('/friend/unlink/4')
   ->checkCSRF()
 
-  ->get('/friend/unlink/2')
-  ->click('はい', array())
-  ->checkCSRF()
+// XSS
+  ->info('/friend/list - XSS')
+  ->get('/friend/list')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/friend/link/id/1056 - XSS')
+  ->get('/friend/link/id/1056')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/friend/unlink/1055 - XSS')
+  ->get('/friend/unlink/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/friend/manage - XSS')
+  ->get('/friend/manage')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/friend/showImage/1055 - XSS')
+  ->get('/friend/showImage/1055')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->login('html1@example.com', 'password')
+  ->isStatusCode(302)
+
+  ->info('/friend/showActivity - XSS')
+  ->get('/friend/showActivity')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/ components - XSS')
+  ->get('/')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('ActivityData', 'body')
+  ->end()
 ;
