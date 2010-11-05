fbf7f0d7df273d147c3176fdfc5ff67002a3a8df
Masato Nagasawa <nagasawa@tejimaya.com>
2010-11-05 11:39:48 +0900

    fixed removed 'echo' (refs #1741)

--- test/functional/pc_backend/designActionsTest.php
+++ test/functional/pc_backend/designActionsTest.php
@@ -5,7 +5,7 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
 $browser = new opTestFunctional(new opBrowser(), new lime_test(null, new lime_output_color()));
 $browser->setTester('doctrine', 'sfTesterDoctrine');
-echo $browser
+$browser
   ->info('0. Login')
   ->get('/default/login')
   ->click('ログイン', array('admin_user' => array(
