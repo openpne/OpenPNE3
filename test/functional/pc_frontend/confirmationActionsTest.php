140172db71d044f6f84d4d9baf326bbdaaa30b57
Masato Nagasawa <nagasawa@tejimaya.com>
2010-11-08 19:39:48 +0900

    added confirmation/list action of pc_frontend for the functional test (refs #1741)

--- test/functional/pc_frontend/confirmationActionsTest.php
+++ test/functional/pc_frontend/confirmationActionsTest.php
@@ -8,22 +8,53 @@ $browser
   ->login('sns@example.com', 'password')
 
   ->info('/confirmation/friend_confirm/2 - CSRF')
-  ->post('/confirmation/friend_confirm/2', array())
+  ->post('/confirmation/friend_confirm/2')
   ->checkCSRF()
 
   ->info('/confirmation/community_confirm/11 - CSRF')
-  ->post('/confirmation/community_confirm/11', array())
+  ->post('/confirmation/community_confirm/11')
   ->checkCSRF()
 
   ->login('sns2@example.com', 'password')
 
   ->info('/confirmation/community_admin_request/5 - CSRF')
-  ->post('/confirmation/community_admin_request/5', array())
+  ->post('/confirmation/community_admin_request/5')
   ->checkCSRF()
 
   ->login('sns3@example.com', 'password')
 
   ->info('/confirmation/community_sub_admin_request/8 - CSRF')
-  ->post('/confirmation/community_sub_admin_request/8', array())
+  ->post('/confirmation/community_sub_admin_request/8')
   ->checkCSRF()
+
+  ->login('html1@example.com', 'password')
+
+  ->info('/confirmation?category=friend_confirm - XSS')
+  ->get('/confirmation?category=friend_confirm')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+  ->end()
+
+  ->info('/confirmation?category=community_confirm - XSS')
+  ->get('/confirmation?category=community_confirm')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name')
+  ->end()
+
+  ->login('html2@example.com', 'password')
+
+  ->info('/confirmation?category=community_admin_request - XSS')
+  ->get('/confirmation?category=community_admin_request')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name')
+  ->end()
+
+  ->info('/confirmation?category=community_sub_admin_request - XSS')
+  ->get('/confirmation?category=community_sub_admin_request')
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('Member', 'name')
+    ->isAllEscapedData('Community', 'name')
+  ->end()
 ;
