3b8823462b2a4dd29fb2b54109a69569dba81fe3
Masato Nagasawa <nagasawa@tejimaya.com>
2010-12-21 22:24:02 +0900

    fixed supported sortable of the community category (fixes #1591)

--- test/unit/model/doctrine/CommunityCategoryTableTest.php
+++ test/unit/model/doctrine/CommunityCategoryTableTest.php
@@ -3,7 +3,7 @@
 include_once dirname(__FILE__) . '/../../../bootstrap/unit.php';
 include_once dirname(__FILE__) . '/../../../bootstrap/database.php';
 
-$t = new lime_test(8, new lime_output_color());
+$t = new lime_test(13, new lime_output_color());
 
 $table = Doctrine::getTable('CommunityCategory');
 
@@ -15,6 +15,16 @@ $t->isa_ok($table->retrieveAll(), 'Doctrine_Collection');
 //------------------------------------------------------------
 $t->diag('CommunityCategoryTable::retrieveAllRoots()');
 $t->isa_ok($table->retrieveAllRoots(), 'Doctrine_Collection');
+$categories = $table->retrieveAllRoots(false);
+$t->is($categories[0]->getSortOrder(), 2);
+$categories = $table->retrieveAllRoots(true);
+$t->is($categories[0]->getSortOrder(), 1);
+
+//------------------------------------------------------------
+$t->diag('CommunityCategoryTable::getAllRootsQuery()');
+$t->isa_ok($table->getAllRootsQuery(), 'opDoctrineQuery');
+$t->is($table->getAllRootsQuery(false)->getDql(), ' FROM CommunityCategory WHERE lft = 1');
+$t->is($table->getAllRootsQuery(true)->getDql(), ' FROM CommunityCategory WHERE lft = 1 ORDER BY sort_order');
 
 //------------------------------------------------------------
 $t->diag('CommunityCategoryTable::retrieveAllChildren()');
--- test/unit/model/doctrine/CommunityCategoryTest.php
+++ test/unit/model/doctrine/CommunityCategoryTest.php
@@ -3,7 +3,7 @@
 include_once dirname(__FILE__) . '/../../../bootstrap/unit.php';
 include_once dirname(__FILE__) . '/../../../bootstrap/database.php';
 
-$t = new lime_test(4, new lime_output_color());
+$t = new lime_test(9, new lime_output_color());
 
 $communityCategory1 = Doctrine::getTable('CommunityCategory')->findOneByName('CategoryA');
 
@@ -28,3 +28,14 @@ $t->is($newCommunityCategory2->getLevel(), 1);
 //------------------------------------------------------------
 $t->diag('CommunityCategory::getForm()');
 $t->isa_ok($communityCategory1->getForm(), 'CommunityCategoryForm');
+
+//------------------------------------------------------------
+$t->diag('CommunityCategory::getChildren()');
+$t->isa_ok($communityCategory1->getChildren(), 'Doctrine_Collection');
+$t->is(count($communityCategory1->getChildren()), 0);
+
+$communityCategory = Doctrine::getTable('CommunityCategory')->find(1);
+$category = $communityCategory->getChildren();
+$t->is(count($category), 2);
+$t->is($category[0]->getName(), 'CategoryB');
+$t->is($category[1]->getName(), 'CategoryA');
