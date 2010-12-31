ea46788956ff8ddeb134fdec8caa6a24c82fe6df
Shogo Kawahara <kawahara@bucyou.net>
2010-12-31 21:46:44 +0900

    removed block comments of license from a plugin skeleton (fixes #1826)

--- lib/task/skeleton/opPlugin/test/bootstrap/functional.php
+++ lib/task/skeleton/opPlugin/test/bootstrap/functional.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * This file is part of the OpenPNE package.
- * (c) OpenPNE Project (http://www.openpne.jp/)
- *
- * For the full copyright and license information, please view the LICENSE
- * file and the NOTICE file that were distributed with this source code.
- */
-
 // guess current application
 if (!isset($app))
 {
