f21c9aa25ecd1899c0be80d0efe841bec3f16a10
Masato Nagasawa <nagasawa@tejimaya.com>
2011-05-06 22:57:41 +0900

    fixed moved get parameter to input tag (fixes #2063 BP from #2058)
    Conflicts:
    
    	apps/mobile_frontend/modules/community/templates/removeSubAdminInput.php
    	apps/pc_frontend/modules/community/templates/removeSubAdminInput.php

--- apps/pc_frontend/modules/community/templates/removeSubAdminInput.php
+++ apps/pc_frontend/modules/community/templates/removeSubAdminInput.php
@@ -1,6 +1,7 @@
 <?php op_include_parts('yesNo', 'removeSubAdminConfirmForm', array(
   'body'      => __("Do you demotion %0% from this %community%'s sub-administrator?", array('%0%' => $member->getName())),
   'yes_form'  => new BaseForm(),
-  'no_url'    => url_for('community/memberManage?id='.$community->getId()),
+  'no_url'    => url_for('community/memberManage'),
   'no_method' => 'get',
+  'no_form'   => '<input type="hidden" name="id" value="'.$community->getId().'"/>',
 )) ?>
