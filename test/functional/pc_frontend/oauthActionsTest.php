3241f269daec8c8a82dc8f87a5f50c5c84733845
Masato Nagasawa <nagasawa@tejimaya.com>
2011-05-09 16:00:51 +0900

    added the XSS/CSRF functional tests for oauth module in the pc_frontend application (refs #1817 BP from #1741)
    Conflicts:
    
    	test/fixtures/xss_test_data.yml

--- test/functional/pc_frontend/oauthActionsTest.php
+++ test/functional/pc_frontend/oauthActionsTest.php
@@ -2,14 +2,33 @@
 
 include(dirname(__FILE__).'/../../bootstrap/functional.php');
 
-$browser = new sfTestFunctional(new sfBrowser());
+function _oauth_get_request_token_params(OAuthConsumer $consumer, $callbackUrl)
+{
+  $request = OAuthRequest::from_consumer_and_token($consumer, null, 'GET', 'http://localhost/index.php/oauth/request_token', array('oauth_callback' => $callbackUrl));
+  $request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, null);
 
-$browser->
-  get('/oauth/request_token')->
+  return $request->to_postdata();
+}
 
-  with('request')->begin()-> isParameter('module', 'oauth')->
-    isParameter('action', 'requestToken')->
-  end()->
+$info = Doctrine::getTable('OAuthConsumerInformation')->find(1055);
 
-  resetCurrentException()
+$consumer = new OAuthConsumer($info->getKeyString(), $info->getSecret());
+$params = _oauth_get_request_token_params($consumer, 'oob');
+
+$browser = new opTestFunctional(new opBrowser());
+$browser
+  ->login('html1@example.com', 'password')
+  ->get('/oauth/request_token?'.$params)
+;
+
+parse_str(sfContext::getInstance()->getResponse()->getContent(), $params);
+
+// XSS
+$browser
+  ->info('/oauth/authorize - XSS')
+  ->get('/oauth/authorize?oauth_token='.$params['oauth_token'])
+
+  ->with('html_escape')->begin()
+    ->isAllEscapedData('OAuthConsumerInformation', 'name')
+  ->end()
 ;
