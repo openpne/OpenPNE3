2c4078735674cd8458027c17043a4969d8f86bf4
Masato Nagasawa <nagasawa@tejimaya.com>
2011-05-27 20:44:11 +0900

    fixed banner is not displayed when logged in (refs #2138, BP from #1678)
    Conflicts:
    
    	apps/smartphone_frontend/modules/default/templates/_sideBanner.php
    	apps/smartphone_frontend/templates/_header.php

--- apps/mobile_frontend/modules/default/templates/_sideBanner.php
+++ apps/mobile_frontend/modules/default/templates/_sideBanner.php
@@ -1,4 +1,5 @@
-<?php if ($sf_user->getMember()): ?>
+<?php $member = $sf_user->getMember()->getRawValue() ?>
+<?php if ($member && 'opAnonymousMember' !== get_class($member)): ?>
 <?php echo op_banner('side_after') ?>
 <?php else: ?>
 <?php echo op_banner('side_before') ?>
