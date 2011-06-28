294515dbdc43ce2f94e1f926575d08c99019f74f
mtakai <m.takai@tejimaya.com>
2011-06-28 16:14:23 +0900

    (refs #2058) Reverted commits for an appropriate fix
    * revert commit:032f641badd65e02400a432dd28a5baef1819bc0
    * revert commit:043cb4ac14f4bc76616ff02c7db1d2c8d1392975

--- apps/mobile_frontend/modules/community/templates/removeSubAdminInput.php
+++ apps/mobile_frontend/modules/community/templates/removeSubAdminInput.php
@@ -1,11 +1,10 @@
-<?php op_mobile_page_title(__('Demotion of the sub-administrator of this %community%'), $community->getName()) ?>
+<?php op_mobile_page_title(__('Demotion sub-administrator of this %community%'), $community->getName()) ?>
 <?php
 op_include_parts('yesNo', 'removeSubAdminConfirmForm', array(
-  'body' => __("Do you demote %0% from this %community%'s sub-administrator?", array('%0%' => $member->getName())),
+  'body' => __("Do you demotion %0% from this %community%'s sub-administrator?", array('%0%' => $member->getName())),
   'yes_form'  => new BaseForm(),
-  'no_url'    => url_for('@community_memberManage'),
+  'no_url'    => url_for('@community_memberManage?id='.$community->getId()),
   'no_method' => 'get',
-  'no_form'   => '<input type="hidden" name="id" value="'.$community->getId().'">',
   'align' => 'center',
 ))
 ?>
