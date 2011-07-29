fbe049baaee1e8e080885bf9bc82efc16dffc4ed
ShinichiU <urabe@nuts-choco.com>
2011-07-29 20:45:30 +0900

    added ability to categorize test data (refs #2297, BP from #1527)
    Conflicts:
    
    	test/fixtures/common/xss_test_data.yml

--- test/bootstrap/database.php
+++ test/bootstrap/database.php
@@ -3,6 +3,11 @@
 $_app = 'pc_frontend';
 $_env = 'test';
 
+if (!isset($fixture))
+{
+  $fixture = 'common';
+}
+
 $configuration = ProjectConfiguration::getApplicationConfiguration($_app, $_env, true);
 new sfDatabaseManager($configuration);
 
@@ -11,7 +16,7 @@ $task->setConfiguration($configuration);
 $task->run(array(), array(
   'no-confirmation' => true,
   'db'              => true,
-  'and-load'        => dirname(__FILE__).'/../fixtures',
+  'and-load'        => dirname(__FILE__).'/../fixtures/'.$fixture,
   'application'     => $_app,
   'env'             => $_env,
 ));
