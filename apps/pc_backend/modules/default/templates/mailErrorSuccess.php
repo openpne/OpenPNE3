9387cb0ec8989c5e7dc5797fbbefa29ee5f8473a
ShinichiU <urabe@nuts-choco.com>
2011-08-09 12:57:52 +0900

    Revert "fixed to output error message on mail not sent"
    This reverts commit 461564a2fcb13ad7fd6b426caadc8453f890fdd2.
    
    Conflicts:
    
    	apps/mobile_frontend/i18n/messages.ja.xml
    	apps/pc_backend/i18n/messages.ja.xml
    
    Conflicts:
    
            apps/pc_backend/i18n/messages.ja.xml
    
    Conflicts:
    
            apps/mobile_frontend/i18n/messages.ja.xml
    
    (cherry picked from commit a2c560fb)

--- apps/pc_backend/modules/default/templates/mailErrorSuccess.php
+++
@@ -1,4 +0,0 @@
-<p><?php echo __('Couldn\'t send E-mail. Please retry or contact to administrator.') ?></p>
-
-<?php use_helper('Javascript') ?>
-<p><?php echo link_to_function(__('Back to previous page'), 'history.back()') ?></p>
