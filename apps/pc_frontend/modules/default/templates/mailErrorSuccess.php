de9e84e37650e2ceba1c60576ab6d4400df6619d
touri <tourimgr@gmail.com>
2011-08-17 19:04:42 +0900

    (fixes #1407, BP from #1372) fixed to mail error template.

--- apps/pc_frontend/modules/default/templates/mailErrorSuccess.php
+++ apps/pc_frontend/modules/default/templates/mailErrorSuccess.php
@@ -1,3 +1,10 @@
+<?php
+$options = array(
+  'title' => __('Errors'),
+);
+op_include_box('mailError', __('Couldn\'t send E-mail. Please retry or contact to administrator.'), $options);
+?>
+
 <?php use_helper('Javascript') ?>
-<?php echo __('Couldn\'t send E-mail. Please retry or contact to administrator.') ?>
 <?php op_include_line('backLink', link_to_function(__('Back to previous page'), 'history.back()')) ?>
+
