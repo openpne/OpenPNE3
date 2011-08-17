fdda9334e8c42d570b435cd7398b3f0cf5d2b844
Kousuke Ebihara <ebihara@php.net>
2011-08-17 16:47:44 +0900

    added importing configuration for keeping backward compatible with a legacy (insecure) implementation of mobile uid authentication on upgrade from OpenPNE 2 (cherry picked from commit 4cc88caacc9bd5884fb231de090efd2332f173b2)

---
+++ data/upgrade/2/sql/mobile_uid_bc_config.sql
@@ -0,0 +1,2 @@
+INSERT INTO sns_config (name, value) VALUES ("op_auth_MobileUID_plugin_uid_type", "0");
+INSERT INTO sns_config (name, value) VALUES ("op_auth_MobileUID_plugin_allow_fallback_uid", "1");
