7e43eef753e011edae6fbd2f16570eef0293a232
watanabe <watanabe@tejimaya.com>
2011-09-13 21:05:16 +0900

    (fixes #2401, BP from #2400) fixed to replace PlaceHolder used in gadget

--- apps/pc_backend/modules/design/templates/_plotGadget.php
+++ apps/pc_backend/modules/design/templates/_plotGadget.php
@@ -3,7 +3,7 @@
 <?php foreach ($gadgets as $gadget) : ?>
 <div class="sortable" id="plot<?php echo ucfirst($type) ?>_gadget_<?php echo $gadget->getId() ?>">
 <?php
-echo link_to_function($gadgetConfig[$gadget->getName()]['caption']['ja_JP'], 'showModalOnParent(\''.url_for('design/editGadget?id='.$gadget->getId()).'\')');
+echo link_to_function(__($gadgetConfig[$gadget->getName()]['caption']['ja_JP']), 'showModalOnParent(\''.url_for('design/editGadget?id='.$gadget->getId()).'\')');
 ?>
 </div>
 <?php endforeach; ?>
