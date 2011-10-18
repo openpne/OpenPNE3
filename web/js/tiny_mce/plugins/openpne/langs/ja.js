e1ec01161ca3dfd067f2c87d8bcd8b2b2a9c71e1
77web <info@77-web.com>
2011-10-19 08:10:20 +0900

    deleted unnecessary files according to master at openpne/OpenPNE3

--- web/js/tiny_mce/plugins/openpne/langs/ja.js
+++
@@ -1,2 +0,0 @@
-
-tinyMCE.addI18n('ja.openpne',{op_b:'太字',op_u:'下線',op_s:'取り消し線',op_i:'斜体',op_large:'大きめの文字',op_small:'小さめの文字',op_color:'文字色変更',op_emoji_docomo:'絵文字挿入（DoCoMo）',op_emoji_au:'絵文字挿入（au）',op_emoji_softbank:'絵文字挿入（SoftBank）',op_cmd:'小窓紹介'});
