ca1ca47111f7a5b3be9c1e8145d8609ecd8e7d16
watanabe <watanabe@tejimaya.com>
2011-10-20 15:56:01 +0900

    (fixes #3101, BP from #2532) replace comment symbol

--- apps/pc_backend/modules/profile/templates/deleteSuccess.php
+++ apps/pc_backend/modules/profile/templates/deleteSuccess.php
@@ -1,6 +1,6 @@
 <h2><?php echo __('Delete profile entry') ?></h2>
 <p><?php echo __('Do you want to delete this anyway?') ?></p>
-<p><?php echo __('※All the member\'s data in this entry will be lost.') ?></p>
+<p><?php echo __('NOTE: All the member\'s data in this entry will be lost.') ?></p>
 <form action="<?php echo url_for('profile/delete?id='.$profile->getId()) ?>" method="post">
 <?php $formCSRF = new sfForm(); ?><input type="hidden" name="<?php echo $formCSRF->getCSRFFieldName() ?>" value="<?php echo $formCSRF->getCSRFToken() ?>" />
 <input type="submit" value="<?php echo __('Delete') ?>" />
