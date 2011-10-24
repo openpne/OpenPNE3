48045abd0323efa1b40c6645a6b92cbb46335fba
Maki Takahashi <takahashi@tejimaya.com>
2011-10-24 20:08:54 +0900

    (fixes #3389, BP from #2491) added static keyword to opToolkit::getRandom()

--- lib/util/opToolkit.class.php
+++ lib/util/opToolkit.class.php
@@ -490,7 +490,7 @@ class opToolkit
    * @param  int    $length  Length of a hash
    * @return string
    */
-  public function getRandom($length = 64)
+  public static function getRandom($length = 64)
   {
     static $srand = false;
 
