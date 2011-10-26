fbdf24084968ddfe93a23f44d90be83ce345ba11
watanabe <watanabe@openpne.jp>
2011-10-26 16:01:27 +0900

    fixed registration phase that doesn't register i-mode ID (fixes #2524, BP from #905)
    BP from $905
    commit 03b26f1ee703de55cce21aa66e3c6119b2178142
    
    Conflicts:
    
    	apps/mobile_frontend/modules/member/templates/registerInputSuccess.php

--- apps/mobile_frontend/modules/member/templates/registerInputSuccess.php
+++ apps/mobile_frontend/modules/member/templates/registerInputSuccess.php
@@ -1,5 +1,5 @@
 <?php op_include_form('registerForm', $form, array(
-  'url'    => url_for('member/registerInput'),
+  'url'    => url_for('member/registerInput').'?guid=on',
   'button' => __('Register'),
   'align'  => 'center' 
 )) ?>
