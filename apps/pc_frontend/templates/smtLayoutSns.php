1a282268c6a730dacb39da93a8a89ba6d8c5aa4c
kashiwasan <pc.kashiwasan@gmail.com>
2011-12-11 16:02:32 +0900

    disable main.css (from 'opSkinBasicPlugin') on smartphone template (refs #2227)

--- apps/pc_frontend/templates/smtLayoutSns.php
+++ apps/pc_frontend/templates/smtLayoutSns.php
@@ -1,4 +1,6 @@
 <?php use_helper('Javascript') ?>
+<?php $sf_response->removeStylesheet('/opSkinBasicPlugin/css/main.css') ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
 <head>
