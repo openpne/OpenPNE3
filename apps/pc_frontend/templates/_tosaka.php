018a9eb6e8e3de0b3e7389f6ad0794a174a911b5
Kimura Youichi <kim.upsilon@bucyou.net>
2011-12-11 14:42:27 +0900

    add layouts for smartphone (refs #2227)

---
+++ apps/pc_frontend/templates/_tosaka.php
@@ -0,0 +1,11 @@
+<div id="slot_tosaka">
+  <div class="row">
+    <div class="span12">
+      <div class="row">
+        <div class="span4"><?php echo link_to(op_image_tag('LOGO.png', array('height' => '32', 'class' => 'menubutton')), '@homepage') ?></div>
+        <div class="span4 center"><?php echo op_image_tag('NOTIFY_CENTER.png', array('height' => '32', 'class' => 'ncbutton')) ?></div>
+        <div class="span3 offset1 center"><?php echo op_image_tag('POST.png', array('height' => '32', 'class' =>'postbutton')) ?></div>
+      </div>
+    </div>
+  </div>
+</div>
