c400cefb14d56e07e3e9aa73d25343a601749daa
watanabe <watanabe@openpne.jp>
2012-01-17 01:27:26 +0900

    (fixes #2323) fixed to put links automatically when the text has whitespaces

--- lib/helper/opUtilHelper.php
+++ lib/helper/opUtilHelper.php
@@ -313,7 +313,8 @@ if (!defined('SF_AUTO_LINK_RE'))
     (                       # leading text
       <\w+.*?>|             #   leading HTML tag, or
       [^=!:\'"/]|           #   leading punctuation, or
-      ^                     #   beginning of line
+      ^|                    #   beginning of line, or
+      \s?                   #   leading whitespaces
     )
     (
       (?:https?://)|        # protocol spec, or
