75ac98e5faa86e4926cf11364d830f8008c666ab
Maki Takahashi <takahashi@tejimaya.com>
2012-01-17 20:21:10 +0900

    (refs #2550, BP from #2490) fixed to get correct order i18n dirs

--- lib/config/opApplicationConfiguration.class.php
+++ lib/config/opApplicationConfiguration.class.php
@@ -321,10 +321,10 @@ abstract class opApplicationConfiguration extends sfApplicationConfiguration
   {
     $dirs = array();
 
-    $dirs = array_merge($dirs, array(sfConfig::get('sf_root_dir').'/i18n'));
     $dirs = array_merge($dirs, $this->globEnablePlugin('/apps/'.sfConfig::get('sf_app').'/i18n'));
     $dirs = array_merge($dirs, $this->globEnablePlugin('/apps/'.sfConfig::get('sf_app').'/modules/'.$moduleName.'/i18n'));
     $dirs = array_merge($dirs, parent::getI18NDirs($moduleName));
+    $dirs = array_merge($dirs, array(sfConfig::get('sf_root_dir').'/i18n'));
 
     return $dirs;
   }
