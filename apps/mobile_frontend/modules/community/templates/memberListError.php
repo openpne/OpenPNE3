ec4e964f53a3d9237290239d7e0d8253f94ce434
watanabe <watanabe@openpne.jp>
2012-02-06 23:59:41 +0900

    (fixes #3242, BP from #2604) fixed to translate 'Community Top' in edit, memberList, and search page

--- apps/mobile_frontend/modules/community/templates/memberListError.php
+++ apps/mobile_frontend/modules/community/templates/memberListError.php
@@ -3,4 +3,4 @@
 
 <hr color="<?php echo $op_color['core_color_11'] ?>">
 
-<?php echo link_to(__('Community Top'), '@community_home?id='.$community->getId()) ?>
+<?php echo link_to(__('%Community% Top'), '@community_home?id='.$community->getId()) ?>
