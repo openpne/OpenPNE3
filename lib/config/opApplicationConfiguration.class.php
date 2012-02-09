fa3559b8faaa95e81b905fc7c7c908d71db0a465
watanabe <watanabe@openpne.jp>
2012-02-09 13:26:15 +0900

    (fixes #2725, BP from #2008) fixed to flip default flag to evaluate correct boolean
    BP from #2008
    commit:2ed9c936f62482e4c3d5ea91954b01943873a47c

--- lib/config/opApplicationConfiguration.class.php
+++ lib/config/opApplicationConfiguration.class.php
@@ -583,7 +583,7 @@ abstract class opApplicationConfiguration extends sfApplicationConfiguration
       $isDefault = 'http://example.com' === $url;
     }
 
-    if ($isDefault)
+    if (!$isDefault)
     {
       $parts = parse_url($url);
 
