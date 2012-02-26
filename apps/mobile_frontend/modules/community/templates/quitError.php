0969f25c9e0da3fa20357cc5e8b3cf1048fafbdc
watanabe <watanabe@openpne.jp>
2012-02-27 01:36:11 +0900

    (fixes #2629) fixed for sns term

--- apps/mobile_frontend/modules/community/templates/quitError.php
+++ apps/mobile_frontend/modules/community/templates/quitError.php
@@ -1,6 +1,6 @@
 <?php op_mobile_page_title(__('Error')) ?>
 <?php if ($isAdmin): ?>
-<font color="<?php echo $op_color['core_color_22'] ?>"><?php echo __('The administrator doesn\'t leave the community.') ?></font>
+<font color="<?php echo $op_color['core_color_22'] ?>"><?php echo __('The administrator doesn\'t leave the %community%.') ?></font>
 <?php else: ?>
-<font color="<?php echo $op_color['core_color_22'] ?>"><?php echo __('You don\'t join this community yet.') ?></font>
+<font color="<?php echo $op_color['core_color_22'] ?>"><?php echo __('You don\'t join this %community% yet.') ?></font>
 <?php endif; ?>
