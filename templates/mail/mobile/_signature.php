7cfa154754e5f4020775419497539b1fc54eb5b1
Kousuke Ebihara <ebihara@php.net>
2012-03-02 17:10:10 +0900

    added missing template file of signature for mobile mail (refs #2783)

---
+++ templates/mail/mobile/_signature.php
@@ -0,0 +1,11 @@
+<?php echo __('*+--------------------------------------------------------')."\n" ?>
+
+<?php echo __('Feedback and Suggestions Feel free to us!')."\n" ?>
+
+<?php echo $op_config['sns_name']."\n" ?>
+<?php echo __('URL') ?>: <?php echo sfConfig::get('op_base_url')."\n" ?>
+<?php echo __('Contact us') ?>: <?php echo $op_config['admin_mail_address']."\n" ?>
+
+<?php echo __('Unapproved reproduction of the contents written to this mail, Unauthorized copying is prohibited.') ?>
+
+<?php echo __('--------------------------------------------------------+*') ?>
