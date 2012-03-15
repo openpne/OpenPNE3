9f50bdf66e3cdaaa4372669902995a1558d8cbe8
kashiwasan <pc.kashiwasan@gmail.com>
2012-03-15 18:00:54 +0900

    add notification center for PC (refs #2767)

--- apps/pc_frontend/templates/_header.php
+++ apps/pc_frontend/templates/_header.php
@@ -1,5 +1,59 @@
 <h1><?php echo link_to($op_config['sns_name'], '@homepage') ?></h1>
 
+<?php if(opToolkit::isSecurePage()): ?>
+<div id="notificationCenter">
+  <?php echo op_image_tag('NOTIFY_CENTER.png', array('height' => '32', 'class' => 'ncbutton')) ?>
+  <div id="notificationCenterDetail">
+    <div id="notificationCenterDetailHeader">
+      <?php echo __('Notification Center') ?>
+    </div>
+    <div id="notificationCenterLoading">
+      <?php echo op_image_tag('ajax-loader.gif') ?>
+    </div>
+    <div id="notificationCenterError">
+      <?php echo __('There is no new notification.') ?>
+    </div>
+  </div>
+</div>
+
+<script id="notificationCenterListTemplate" type="text/x-jquery-tmpl">
+    <div class="{{if unread==false}}isread {{/if}}{{if category=="message" || category=="other"}}nclink {{/if}}push" data-notify-id="${id}" data-location-url="${url}" data-member-id="${member_id_from}">
+      <div class="push_icon">
+        <img src="${icon_url}" width="48">
+      </div>
+      <div class="push_content">
+      {{if category=="link"}}
+        {{if unread==false}}
+        <?php echo __('Friend request') ?>
+        {{else}}
+        <?php echo __('Do you accept friend request?') ?>
+        <div class="push_yesno">
+          <button class="friend-accept">YES</button>
+          <button class="friend-reject">NO</button>
+          <div class="ncfriendloading"><?php echo op_image_tag('ajax-loader.gif') ?></div>
+          <div class="ncfriendresultmessage"></div>
+        </div>
+        {{/if}}
+      {{else}}
+        ${body}
+      {{/if}}
+      </div>
+    </div>
+</script>
+
+<script id="notificationCenterCountTemplate" type="text/x-jquery-tmpl">
+  {{if message!==0}}
+  <span id="nc_icon1">${message}</span>
+  {{/if}}
+  {{if link!==0}}
+  <span id="nc_icon2">${link}</span>
+  {{/if}}
+  {{if other!==0}}
+  <span id="nc_icon3">${other}</span>
+  {{/if}}
+</script>
+
+<?php endif ?>
 
 <div id="globalNav">
 <?php
