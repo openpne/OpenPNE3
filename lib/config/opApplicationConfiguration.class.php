d94ebdbbd5334535abda9a0d6f401e45ec79f67c
Kimura Youichi <kim.upsilon@bucyou.net>
2012-03-19 16:19:49 +0900

    Revert (#1966) "add support for 'sf_route' to opApplicationConfiguration::generateAppUrl()"
    This reverts commit c1678c50ec2756b8d987f14d42fc445ec51847df.

--- lib/config/opApplicationConfiguration.class.php
+++ lib/config/opApplicationConfiguration.class.php
@@ -552,16 +552,8 @@ abstract class opApplicationConfiguration extends sfApplicationConfiguration
 
   public function generateAppUrl($application, $parameters = array(), $absolute = false)
   {
-    if (is_array($parameters) && isset($parameters['sf_route']))
-    {
-      $route = $parameters['sf_route'];
-      unset($parameters['sf_route']);
-    }
-    else
-    {
-      list($route, $parameters) = sfContext::getInstance()->getController()
-        ->convertUrlStringToParameters($parameters);
-    }
+    list($route, $parameters) = sfContext::getInstance()->getController()
+      ->convertUrlStringToParameters($parameters);
 
     return $this->getAppRouting($application)->generate($route, $parameters, $absolute);
   }
