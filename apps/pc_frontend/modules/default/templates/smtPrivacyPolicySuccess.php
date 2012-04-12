212b5907223d7f43345524d2420a80cca3c5f79a
Kimura Youichi <kim.upsilon@bucyou.net>
2012-04-12 15:19:18 +0900

    add link for back to previous page for smartphone (fixes #2965, #2966, BP from #2931, #2932)

--- apps/pc_frontend/modules/default/templates/smtPrivacyPolicySuccess.php
+++ apps/pc_frontend/modules/default/templates/smtPrivacyPolicySuccess.php
@@ -5,3 +5,8 @@
 <div class="row">
 <?php echo nl2br($op_config['privacy_policy']); ?>
 </div>
+
+<?php use_helper('Javascript') ?>
+<div class="row">
+<p><?php echo link_to_function(__('Back to previous page'), 'history.back()', array('class' => 'btn')) ?></p>
+</div>
