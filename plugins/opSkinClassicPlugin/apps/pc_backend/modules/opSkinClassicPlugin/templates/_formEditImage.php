d68d68eed88f0d97acc93a5e2804c20d1eea4179
kashiwasan <pc.kashiwasan@gmail.com>
2012-04-25 18:12:16 +0900

    removed opSkinClassicPlugin (fixes #3037)

--- plugins/opSkinClassicPlugin/apps/pc_backend/modules/opSkinClassicPlugin/templates/_formEditImage.php
+++
@@ -1,8 +0,0 @@
-<?php if (0 === strpos($target, 'skin_')): ?>
-<?php echo image_tag(opSkinClassicConfig::get($target.'_image'), array('width' => '180')) ?><br />
-<?php else: ?>
-<?php echo image_tag(opSkinClassicConfig::get($target.'_image')) ?><br />
-<?php endif; ?>
-<br />
-%input%<br />
-%delete% %delete_label%
