9c524c3855389400e2b397766a954e60ab16afa6
watanabe <watanabe@openpne.jp>
2012-04-27 18:01:45 +0900

    (fixes #2984) removed unnessessary properties

--- lib/response/opWebResponse.class.php
+++ lib/response/opWebResponse.class.php
@@ -17,8 +17,6 @@
 class opWebResponse extends sfWebResponse
 {
   protected
-    $displayMember = null,
-    $displayCommunity = null,
     $smtStylesheets = array(),
     $smtJavascripts = array();
 
