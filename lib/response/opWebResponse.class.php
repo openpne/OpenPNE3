91b09bbae018ed73bdc621504d87c6acf1d30300
watanabe <watanabe@openpne.jp>
2012-04-27 18:00:25 +0900

    (fixes #2984) add smartphone properties operation in opWebResponse

--- lib/response/opWebResponse.class.php
+++ lib/response/opWebResponse.class.php
@@ -22,6 +22,86 @@ class opWebResponse extends sfWebResponse
     $smtStylesheets = array(),
     $smtJavascripts = array();
 
+  /**
+   * Initializes this opWebResponse.
+   *
+   *  * charset:           The charset to use (utf-8 by default)
+   *  * content_type:      The content type (text/html by default)
+   *  * send_http_headers: Whether to send HTTP headers or not (true by default)
+   *  * http_protocol:     The HTTP protocol to use for the response (HTTP/1.0 by default)
+   *
+   * @param  sfEventDispatcher $dispatcher  An sfEventDispatcher instance
+   * @param  array             $options     An array of options
+   *
+   * @return bool true, if initialization completes successfully, otherwise false
+   *
+   * @throws <b>sfInitializationException</b> If an error occurs while initializing this sfResponse
+   *
+   * @see sfWebResponse->initialize()
+   */
+  public function initialize(sfEventDispatcher $dispatcher, $options = array())
+  {
+    parent::initialize($dispatcher, $options);
+
+    $this->smtJavascripts = array_combine($this->positions, array_fill(0, count($this->positions), array()));
+    $this->smtStylesheets = array_combine($this->positions, array_fill(0, count($this->positions), array()));
+  }
+
+  /**
+   * Copies all properties from a given opWebResponse object to the current one.
+   *
+   * @param sfWebResponse $response  An sfWebResponse instance
+   *
+   * @see sfWebResponse->copyProperties()
+   */
+  public function copyProperties(sfWebResponse $response)
+  {
+    parent::copyProperties($response);
+
+    if ($response instanceof opWebResponse)
+    {
+      $this->smtStylesheets = $response->getSmtStylesheets(self::RAW);
+      $this->smtJavascripts = $response->getSmtJavascripts(self::RAW);
+    }
+  }
+
+  /**
+   * Merges all properties from a given opWebResponse object to the current one.
+   *
+   * @param sfWebResponse $response  An sfWebResponse instance
+   *
+   * @see sfWebResponse->merge()
+   */
+  public function merge(sfWebResponse $response)
+  {
+    parent::merge($response);
+
+    if ($response instanceof opWebResponse)
+    {
+      foreach ($this->getPositions() as $position)
+      {
+        $this->smtJavascripts[$position] = array_merge($this->getSmtJavascripts($position), $response->getSmtJavascripts($position));
+        $this->smtStylesheets[$position] = array_merge($this->getSmtStylesheets($position), $response->getSmtStylesheets($position));
+      }
+    }
+  }
+
+  /**
+   * @see sfWebResponse
+   */
+  public function serialize()
+  {
+    return serialize(array($this->content, $this->statusCode, $this->statusText, $this->options, $this->headerOnly, $this->headers, $this->metas, $this->httpMetas, $this->stylesheets, $this->javascripts, $this->slots, $this->smtStylesheets, $this->smtJavascript));
+  }
+
+  /**
+   * @see sfWebResponse
+   */
+  public function unserialize($serialized)
+  {
+    list($this->content, $this->statusCode, $this->statusText, $this->options, $this->headerOnly, $this->headers, $this->metas, $this->httpMetas, $this->stylesheets, $this->javascripts, $this->slots, $this->smtStylesheets, $this->smtJavascript) = unserialize($serialized);
+  }
+
   public function getTitle()
   {
     $result = parent::getTitle();
