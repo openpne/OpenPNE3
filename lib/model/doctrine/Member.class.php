4924d48b10b7fe639e1b09263d401524c4eb011e
77web <info@77-web.com>
2012-05-18 16:49:46 +0900

    fixed Member::delete() to delete not only MemberImage but File and FileBin (fixes #3375)

--- lib/model/doctrine/Member.class.php
+++ lib/model/doctrine/Member.class.php
@@ -313,6 +313,12 @@ class Member extends BaseMember implements opAccessControlRecordInterface
       $adminCommunityMember = $communityMemberTable->retrieveByMemberIdAndCommunityId($this->getId(), $communityId);
       $adminCommunityMember->delete();
     }
+
+    foreach ($this->getMemberImage() as $memberImage)
+    {
+      $memberImage->delete();
+    }
+
     return parent::delete($conn);
   }
 
