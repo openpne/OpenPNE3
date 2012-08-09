5689f175449e70a71e14d41f1a942889055c3d3b
Rimpei Ogawa <ogawa@tejimaya.com>
2012-08-09 23:20:41 +0900

    [opAuthMailAddressPlugin] registration with mobile address in the pc_frontend application for smartphone users (refs #3159, BP from #3155)

--- plugins/opAuthMailAddressPlugin/apps/pc_frontend/modules/opAuthMailAddress/templates/registerError.php
+++ plugins/opAuthMailAddressPlugin/apps/pc_frontend/modules/opAuthMailAddress/templates/registerError.php
@@ -2,5 +2,5 @@
 $options = array(
   'title' => __('Errors'),
 );
-op_include_box('registerError', __('Can access this registration URL with mobile only.'), $options);
+op_include_box('registerError', __('Your e-mail address is not registered.'), $options);
 ?>
