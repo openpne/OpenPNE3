2349a3ce78eeb0e9319a83a18cfab5f141f13843
Rimpei Ogawa <ogawa@tejimaya.com>
2012-08-09 23:31:44 +0900

    [opAuthMailAddressPlugin] change the default value of `is_check_multiple_address' from 0 to 1 for smartphone users (refs #3155)

--- plugins/opAuthMailAddressPlugin/lib/form/opAuthConfigFormMailAddress.class.php
+++ plugins/opAuthMailAddressPlugin/lib/form/opAuthConfigFormMailAddress.class.php
@@ -17,4 +17,13 @@
  */
 class opAuthConfigFormMailAddress extends opAuthConfigForm
 {
+  public function setup()
+  {
+    parent::setup();
+
+    $this->widgetSchema->setHelp(
+      'is_check_multiple_address',
+      'どちらか一方を受け付ける設定とした場合、携帯メールアドレスを利用しているスマートフォンユーザーがログインできなくなります。'
+    );
+  }
 }
