9c0841b5ca210dbbecb20a32e3fbb86142cc0f95
watanabe <watanabe@openpne.jp>
2012-09-12 15:33:38 +0900

    (fixes #2649) fixed i18n

--- apps/pc_backend/modules/admin/lib/AdminUserEditPasswordForm.class.php
+++ apps/pc_backend/modules/admin/lib/AdminUserEditPasswordForm.class.php
@@ -29,6 +29,11 @@ class AdminUserEditPasswordForm extends AdminUserForm
       'callback'  => array('AdminUserEditPasswordForm', 'validate'),
       'arguments' => array('object' => $this->getObject()),
     )));
+
+    $this->widgetSchema->setLabels(array(
+      'old_password' => 'Old password',
+      'new_password' => 'New password',
+    ));
   }
 
   public static function validate($validator, $values, $arguments = array())
