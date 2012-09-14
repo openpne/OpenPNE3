8064b916465136cfdd8056cb23a012e60021145d
Kimura Youichi <kim.upsilon@bucyou.net>
2012-09-14 18:10:09 +0900

    accept multiple images in activity/post.json (fixes #3203)

--- apps/api/modules/activity/actions/actions.class.php
+++ apps/api/modules/activity/actions/actions.class.php
@@ -188,24 +188,31 @@ class activityActions extends opJsonApiActions
 
     $options['source'] = 'API';
 
-    $imageFile = $request->getFiles('images');
-    if (!empty($imageFile))
-    {   
-      $validator = new opValidatorImageFile(array('required' => false));
-      try 
-      {   
-        $obj = $validator->clean($imageFile);
-      }   
-      catch (sfValidatorError $e) 
-      {   
-        $this->forward400('This image file is invalid.');
-      }   
-      $file = new File();
-      $file->setFromValidatedFile($obj);
-      $file->setName('ac_'.$this->getUser()->getMemberId().'_'.$file->getName());
-      $file->save();
-      $options['images'][] = array('file_id' => $file->getId());
-    }   
+    $imageFiles = $request->getFiles('images');
+    if (!empty($imageFiles))
+    {
+      foreach ((array)$imageFiles as $imageFile)
+      {
+        $validator = new opValidatorImageFile(array('required' => false));
+        try
+        {
+          $obj = $validator->clean($imageFile);
+        }
+        catch (sfValidatorError $e)
+        {
+          $this->forward400('This image file is invalid.');
+        }
+        if (is_null($obj))
+        {
+          continue;
+        }
+        $file = new File();
+        $file->setFromValidatedFile($obj);
+        $file->setName('ac_'.$this->getUser()->getMemberId().'_'.$file->getName());
+        $file->save();
+        $options['images'][] = array('file_id' => $file->getId());
+      }
+    }
 
     $this->activity = Doctrine::getTable('ActivityData')->updateActivity($memberId, $body, $options);
 
