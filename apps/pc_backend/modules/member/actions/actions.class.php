cd39f7420245c85c39fed7380db1ae96bb4896bb
nakajima <nakajima@www1124uc.sakura.ne.jp>
2013-03-28 16:27:02 +0900

    (fixes #3323) Search for members in the member ID

--- apps/pc_backend/modules/member/actions/actions.class.php
+++ apps/pc_backend/modules/member/actions/actions.class.php
@@ -36,7 +36,7 @@ class memberActions extends sfActions
   {
     $params = $request->getParameter('member', array());
 
-    $this->form = new opMemberProfileSearchForm(array(), array('use_id' => true, 'is_check_public_flag' => false));
+    $this->form = new opMemberProfileSearchForm(array(), array('is_use_id' => true, 'is_check_public_flag' => false));
     $this->form->getWidgetSchema()->setLabel('name', 'Nickname');
     $this->form->bind($params);
 
