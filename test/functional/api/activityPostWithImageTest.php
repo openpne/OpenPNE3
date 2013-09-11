11d21fc61da6dc458bb23812fa917b4c5bcbc4bd
Kimura Youichi <kim.upsilon@bucyou.net>
2013-09-11 17:43:03 +0900

    add testcases for activity/post.json with multiple images (refs #3400, BP from #3203)
    (cherry picked from commit 502a85ad9abae0416c8e574e6e1c1bc7cb10c6c9)

--- test/functional/api/activityPostWithImageTest.php
+++ test/functional/api/activityPostWithImageTest.php
@@ -4,7 +4,7 @@ $executeLoader = false;
 include dirname(__FILE__).'/../../bootstrap/functional.php';
 include dirname(__FILE__).'/../../bootstrap/database.php';
 
-$numOfTests = 6;
+$numOfTests = 9;
 $tester = new opTestFunctional(
   new opBrowser(),
   new lime_test($numOfTests, new lime_output_color())
@@ -23,6 +23,7 @@ if (in_array('opTimelinePlugin', ProjectConfiguration::getActive()->getPlugins()
 }
 
 $tester
+  ->info('/activity/post.json - single image upload')
   ->postWithFiles(
     '/activity/post.json',
     array('apiKey' => $apiKeyMember1, 'body' => 'hogehoge'),
@@ -39,9 +40,26 @@ $t->is($response['data']['image_url'], 'http://localhost/cache/img/gif/w48_h48/'
 $t->is($response['data']['image_large_url'], 'http://localhost/cache/img/gif/w_h/'.$activityImage[0]->File->name.'.gif');
 
 $tester
+  ->info('/activity/post.json - invalid image')
   ->postWithFiles(
     '/activity/post.json',
     array('apiKey' => $apiKeyMember1, 'body' => 'hogehoge'),
     array('images[0]' => dirname(__FILE__).'/uploads/plaintext.txt')
   )
   ->with('response')->isStatusCode(400);
+
+$tester
+  ->info('/activity/post.json - multiple image upload')
+  ->postWithFiles(
+    '/activity/post.json',
+    array('apiKey' => $apiKeyMember1, 'body' => 'hogehoge'),
+    array('images[0]' => dirname(__FILE__).'/uploads/dot.gif', 'images[1]' => dirname(__FILE__).'/uploads/white.jpg')
+  )
+  ->with('response')->isStatusCode(200);
+
+$response = json_decode($tester->getResponse()->getContent(), true);
+$t->is($response['status'], 'success');
+
+$activityImage = Doctrine_Core::getTable('ActivityImage')->findByActivityDataId($response['data']['id']);
+$t->is(count($activityImage), 2);
+// retrieving multiple image_url via JSON API is not available yet
