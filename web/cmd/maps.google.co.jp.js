273d4013113611a20fb5b41086c7bfaaa00c2da0
Kimura Youichi <kim.upsilon@bucyou.net>
2013-12-12 14:46:43 +0900

    fix pattern for Google Maps url in cmd scripts (fixes #3523 BP from #3521) (cherry picked from commit c588f6f2a338cd4bbe4a058f7c4d392cc0fdf521)
    patch from https://redmine.openpne.jp/issues/1755#note-4

--- web/cmd/maps.google.co.jp.js
+++ web/cmd/maps.google.co.jp.js
@@ -1,6 +1,6 @@
 function url2cmd(url, googlemapsUrl) {
-  var maps_google_co_jp_maps = url.match(/^http:\/\/maps\.google\.co\.jp\/maps\?(.+)/);
-  var maps_google_co_jp = url.match(/^http:\/\/maps\.google\.co\.jp\/\?(.+)/);
+  var maps_google_co_jp_maps = url.match(/^https?:\/\/maps\.google\.co\.jp\/maps\?(.+)/);
+  var maps_google_co_jp = url.match(/^https?:\/\/maps\.google\.co\.jp\/\?(.+)/);
 
   if (maps_google_co_jp_maps || maps_google_co_jp) {
      var id = RegExp.$1;
