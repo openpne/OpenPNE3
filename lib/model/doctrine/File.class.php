dc57650db098596393c1791f4815f4b0017dd3a8
Akihiro KOBAYASHI <a.kobayashi@tejimaya.com>
2014-03-04 13:22:16 +0900

    start

--- lib/model/doctrine/File.class.php
+++ lib/model/doctrine/File.class.php
@@ -59,111 +59,17 @@ class File extends BaseFile
 
   public function save(Doctrine_Connection $conn = null)
   {
+    $this->setFilesize(strlen($this->FileBin->bin));
+
     if ($this->isImage())
     {
-      $uploadimage = sfConfig::get('op_resize_limit_size', 1024);
       $class = sfImageHandler::getStorageClassName();
       $this->setName(call_user_func(array($class, 'getFilenameToSave'), $this->getName()), $class);
+
       $storage = call_user_func(array($class, 'create'), $this, $class);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 29cecef20c23361d85d3f8165ca42fc038f85344
-      $filesize = strlen($this->FileBin->bin);
-      if ($filesize > $uploadimage)
-      {
-        $ratio = sqrt($uploadimage/$filesize)/1.5;
-        $type = $this->getType();
-        $image = imagecreatefromstring($this->FileBin->bin);
-        $x = imagesx($image);
-        $y = imagesy($image);
-        $resizex = $x*$ratio;
-        $resizey = $y*$ratio;
-        $resize = imagecreatetruecolor($resizex, $resizey);
-        switch($type)
-        {
-          case 'image/jpeg':
-            imagecopyresampled($resize, $image, 0, 0, 0, 0, $resizex, $resizey, $x, $y);
-            ob_start();
-            imagejpeg($resize);
-            break;
-          case 'image/gif':
-            $alpha = imagecolortransparent($image);
-            $trnprt_color = imagecolorsforindex($image, $alpha);
-            $alpha = imagecolorallocate($resize, $trnprt_color['red'], $trnprt_color['green'], $trnprt_color['blue']);
-            imagefill($resize, 0, 0, $alpha);
-            imagecolortransparent($resize, $alpha);
-            imagecopyresampled($resize, $image, 0, 0, 0, 0, $resizex, $resizey, $x, $y);
-            ob_start();
-            imagegif($resize);
-            break;
-          case 'image/png':
-            imagealphablending($resize, false);
-            imagesavealpha($resize, true);
-            imagecopyresampled($resize, $image, 0, 0, 0, 0, $resizex, $resizey, $x, $y);
-            ob_start();
-            imagepng($resize);
-<<<<<<< HEAD
-=======
-      $type = $this->getType();
-      if ($type === 'image/jpeg')
-      {
-        $cls = exif_read_data("data://image/jpeg;base64,".base64_encode($this->FileBin->bin));
-        $exif = $cls['Orientation'];
-        $image = imagecreatefromstring($this->FileBin->bin);
-        switch ($exif)
-        {
-          case 3:
-            $image = imagerotate($image, 180, 0);
-            break;
-          case 6:
-            $imagewidth = imagesy($image);
-            $imageheight = imagesx($image);
-            $image = imagerotate($image, -90, 0);
-            imagecopyresampled ($image, $image, 0, 0, (imagesx($image) - $imagewidth)/2, (imagesy($image) - $imageheight)/2, $imagewidth, $imageheight, $imagewidth, $imageheight);
-            break;
-          case 8:
-            $imagewidth = imagesy($image);
-            $imageheight = imagesx($image);
-            $image = imagerotate($image, 90, 0);
-            imagecopyresampled ($image, $image, 0, 0, (imagesx($image) - $imagewidth)/2, (imagesy($image) - $imageheight)/2, $imagewidth, $imageheight, $imagewidth, $imageheight);
->>>>>>> f20e9a37a32f26ceb8d17e7a05443ac584562b91
-=======
->>>>>>> 29cecef20c23361d85d3f8165ca42fc038f85344
-            break;
-          default:
-            break;
-        }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 29cecef20c23361d85d3f8165ca42fc038f85344
-        $ei = ob_get_contents();
-        ob_end_clean();
-        $this->getFileBin()->setBin($ei);
-        imagedestroy($image);
-        imagedestroy($resize);
-<<<<<<< HEAD
-=======
-        ob_start();
-        imagejpeg($image);
-        $ei = ob_get_contents();
-        ob_end_clean();
-        $this->getFileBin()->setBin($ei);
->>>>>>> f20e9a37a32f26ceb8d17e7a05443ac584562b91
-=======
->>>>>>> 29cecef20c23361d85d3f8165ca42fc038f85344
-      }
       $storage->saveBinary($this->getFileBin());
     }
-    $this->setFilesize(strlen($this->FileBin->bin));
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 29cecef20c23361d85d3f8165ca42fc038f85344
 
-=======
->>>>>>> f20e9a37a32f26ceb8d17e7a05443ac584562b91
     return parent::save($conn);
   }
 
@@ -175,6 +81,7 @@ class File extends BaseFile
       $storage = call_user_func(array($class, 'create'), $this, $class);
       $storage->deleteBinary();
     }
+
     return parent::delete($conn);
   }
 }
