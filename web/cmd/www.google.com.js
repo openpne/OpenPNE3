9f3f00e4afb5028b8caaf3e8f50956ad8fc09484
isaosano <sano@tejimaya.com>
2016-03-09 15:19:25 +0900

    Modified to be able to display the URL of the new format of the Googlemap (refs #3891, BP from #3775)

--- web/cmd/www.google.com.js
+++ web/cmd/www.google.com.js
@@ -10,15 +10,27 @@ function url2cmd(url, googlemapsUrl) {
 }
 
 function main(id, googlemapsUrl) {
+  if (id.match(/@/)){
+    var cmd = id.match(/@(.+)z/);
+    var param = cmd[1].split(",");
+    var lon = param[0];
+    var lat = param[1];
+    var z = param[2];
+
+    var html = ''
+    + '<iframe marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bordercolor="#000000" src="'+googlemapsUrl+'?x='+lon+'&y='+lat+'&z='+z+'" name="sample" height="350">'
+    + 'この部分はインラインフレームを使用しています。'
+    + '</iframe>';
+  } else {
     var cmd = id.split("&amp;");
     var param = new Array();
     param["z"] = "15";
     param["ll"] = "0,0";
     for(i=0; i<cmd.length; i++) {
-       var work = cmd[i].split("=");
-       if( work.length == 2 ) {
-         param[work[0]] = work[1];
-       }
+      var work = cmd[i].split("=");
+      if( work.length == 2 ) {
+        param[work[0]] = work[1];
+      }
     }
     var ll = param["ll"].split(",");
     var z = param["z"];
@@ -29,6 +41,6 @@ function main(id, googlemapsUrl) {
     + '<iframe marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bordercolor="#000000" src="'+googlemapsUrl+'?x='+ll[0]+'&y='+ll[1]+'&z='+z+'&t='+t+'&q='+q+'" name="sample" height="350">'
     + 'この部分はインラインフレームを使用しています。'
     + '</iframe>';
-
+  }
     document.write(html);
 }
