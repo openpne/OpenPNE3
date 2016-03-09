28fd265ccccec565bfa91a6f8082d0607856194f
isaosano <sano@tejimaya.com>
2016-03-10 02:43:00 +0900

    Modify to unified indentation (refs #3892, BP from #3775)

--- web/cmd/www.google.com.js
+++ web/cmd/www.google.com.js
@@ -2,15 +2,15 @@ function url2cmd(url, googlemapsUrl) {
   var www_google_com_maps = url.match(/^http:\/\/www\.google\.com\/maps\?(.+)/);
 
   if (www_google_com_maps) {
-     var id = RegExp.$1;
-     main(id, googlemapsUrl);
-    } else {
-       pne_url2a(url);
-    }
+    var id = RegExp.$1;
+    main(id, googlemapsUrl);
+  } else {
+    pne_url2a(url);
+  }
 }
 
 function main(id, googlemapsUrl) {
-  if (id.match(/@/)){
+  if (id.match(/@/)) {
     var cmd = id.match(/@(.+)z/);
     var param = cmd[1].split(",");
     var lon = param[0];
@@ -18,17 +18,17 @@ function main(id, googlemapsUrl) {
     var z = param[2];
 
     var html = ''
-    + '<iframe marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bordercolor="#000000" src="'+googlemapsUrl+'?x='+lon+'&y='+lat+'&z='+z+'" name="sample" height="350">'
-    + 'この部分はインラインフレームを使用しています。'
-    + '</iframe>';
+      + '<iframe marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bordercolor="#000000" src="'+googlemapsUrl+'?x='+lon+'&y='+lat+'&z='+z+'" name="sample" height="350">'
+      + 'この部分はインラインフレームを使用しています。'
+      + '</iframe>';
   } else {
     var cmd = id.split("&amp;");
     var param = new Array();
     param["z"] = "15";
     param["ll"] = "0,0";
-    for(i=0; i<cmd.length; i++) {
+    for (i=0; i<cmd.length; i++) {
       var work = cmd[i].split("=");
-      if( work.length == 2 ) {
+      if ( work.length == 2 ) {
         param[work[0]] = work[1];
       }
     }
@@ -38,9 +38,10 @@ function main(id, googlemapsUrl) {
     var q = param["q"];
 
     var html = ''
-    + '<iframe marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bordercolor="#000000" src="'+googlemapsUrl+'?x='+ll[0]+'&y='+ll[1]+'&z='+z+'&t='+t+'&q='+q+'" name="sample" height="350">'
-    + 'この部分はインラインフレームを使用しています。'
-    + '</iframe>';
+      + '<iframe marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" bordercolor="#000000" src="'+googlemapsUrl+'?x='+ll[0]+'&y='+ll[1]+'&z='+z+'&t='+t+'&q='+q+'" name="sample" height="350">'
+      + 'この部分はインラインフレームを使用しています。'
+      + '</iframe>';
   }
-    document.write(html);
+
+  document.write(html);
 }
