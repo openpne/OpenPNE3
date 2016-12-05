fd0af2c6d14f49726d288990cfbc9cbee4c0824d
nishizoe <nishizoe@tejimaya.com>
2016-12-05 16:51:34 +0900

    (fixed #4055) 変換文字列をダブルクォートではなくシングルクオートで囲むように修正

--- web/js/jquery.notify.js
+++ web/js/jquery.notify.js
@@ -6,7 +6,7 @@
 
     return this.each(function(){
       var dataLocationUrl = $(this).attr('data-location-url');
-      var linkUrl = openpne.baseUrl + dataLocationUrl.replace(/^\/*/g, "");
+      var linkUrl = openpne.baseUrl + dataLocationUrl.replace(/^\/*/g, '');
       var notifyId = $(this).attr('data-notify-id');
       $(this).click(function(){
         if ( false == settings.isDisableRead )
