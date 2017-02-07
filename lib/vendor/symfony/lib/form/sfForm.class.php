e949fbb4052b9f49bb56c6ae70a58f5ee47e8e04
nishizoe <nishizoe@tejimaya.com>
2017-02-07 19:49:43 +0900

    (fixed #4141, BP from #3887) updated to symfony 1.4.20

--- lib/vendor/symfony/lib/form/sfForm.class.php
+++ lib/vendor/symfony/lib/form/sfForm.class.php
@@ -23,7 +23,7 @@
  * @package    symfony
  * @subpackage form
  * @author     Fabien Potencier <fabien.potencier@symfony-project.com>
- * @version    SVN: $Id: sfForm.class.php 29678 2010-05-30 14:38:42Z Kris.Wallsmith $
+ * @version    SVN: $Id: sfForm.class.php 33598 2012-11-25 09:57:29Z fabien $
  */
 class sfForm implements ArrayAccess, Iterator, Countable
 {
