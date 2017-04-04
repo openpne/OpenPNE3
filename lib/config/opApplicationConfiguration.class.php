11ee00f8f770bdf290ad78b1db2931956a5d59ae
Shinichi Urabe <urabe@tejimaya.com>
2017-04-04 16:05:17 +0900

    (refs #4094, BP from #4093) Fix cording standard. - Control statements must have a single space before the opening parenthesis of the conditional.

--- lib/config/opApplicationConfiguration.class.php
+++ lib/config/opApplicationConfiguration.class.php
@@ -215,7 +215,7 @@ abstract class opApplicationConfiguration extends sfApplicationConfiguration
 
     $table = Doctrine::getTable('SnsTerm');
     $application = sfConfig::get('sf_app');
-    if($application == 'pc_backend')
+    if ($application == 'pc_backend')
     {
         $application = 'pc_frontend';
     }
