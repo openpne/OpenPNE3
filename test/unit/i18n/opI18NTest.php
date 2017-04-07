694e8a5fd0ad6b2d7a5e137d0806087cae816208
Shinichi Urabe <urabe@tejimaya.com>
2017-04-07 16:47:25 +0900

    (refs #4093) Add ja_JP test case.

--- test/unit/i18n/opI18NTest.php
+++ test/unit/i18n/opI18NTest.php
@@ -4,16 +4,16 @@ include_once dirname(__FILE__).'/../../bootstrap/unit.php';
 include_once dirname(__FILE__) . '/../../bootstrap/database.php';
 
 $app = 'api';
-sfContext::createInstance(ProjectConfiguration::getApplicationConfiguration($app, 'test', true));
-
-$i18N = sfContext::getInstance()->getI18N();
+$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', true);
+sfContext::createInstance($configuration);
 
 $t = new lime_test(null, new lime_output_color());
 
+$i18N = new opI18N($configuration);
 sfContext::getInstance()->getUser()->setCulture('en');
 
 //------------------------------------------------------------
-$t->diag('opI18N test');
+$t->diag('opI18N test (en)');
 $t->is($i18N->__('%friend%'), 'friend', 'friend normal.');
 $t->is($i18N->__('%Friend%'), 'Friend', 'friend fronting.');
 $t->is($i18N->__('%my_friend%'), 'my friend', 'my friend normal.');
@@ -22,3 +22,17 @@ $t->is($i18N->__('%community%'), 'community', 'community normal.');
 $t->is($i18N->__('%Community%'), 'Community', 'community fronting.');
 $t->is($i18N->__('%nickname%'), 'nickname', 'nickname normal.');
 $t->is($i18N->__('%Nickname%'), 'Nickname', 'nickname fronting.');
+
+$i18N = new opI18N($configuration);
+sfContext::getInstance()->getUser()->setCulture('ja_JP');
+
+//------------------------------------------------------------
+$t->diag('opI18N test (ja_JP)');
+$t->is($i18N->__('%friend%'), 'フレンド', 'friend normal.');
+$t->is($i18N->__('%Friend%'), 'フレンド', 'friend fronting. (The results of normal and fronting are the same)');
+$t->is($i18N->__('%my_friend%'), 'マイフレンド', 'my friend normal.');
+$t->is($i18N->__('%My_friend%'), 'マイフレンド', 'my friend fronting. (The results of normal and fronting are the same)');
+$t->is($i18N->__('%community%'), 'コミュニティ', 'community normal.');
+$t->is($i18N->__('%Community%'), 'コミュニティ', 'community fronting. (The results of normal and fronting are the same)');
+$t->is($i18N->__('%nickname%'), 'ニックネーム', 'nickname normal.');
+$t->is($i18N->__('%Nickname%'), 'ニックネーム', 'nickname fronting. (The results of normal and fronting are the same)');
