90278981b9c7914d5b822a61bf8f134b5f27dc0c
Shinichi Urabe <urabe@tejimaya.com>
2017-06-26 10:52:10 +0900

    (refs #4132) I modified it to call "smtSwitch.initialize();" anytime

--- apps/pc_frontend/templates/_layout.php
+++ apps/pc_frontend/templates/_layout.php
@@ -114,6 +114,7 @@ include_component('default', 'localNav', $localNavOptions);
 <div id="SmtSwitch">
 <a href="javascript:void(0)" id="SmtSwitchLink"><?php echo __('View this page on smartphone style') ?></a>
 <?php echo javascript_tag('
+smtSwitch.initialize();
 document.getElementById("SmtSwitchLink").addEventListener("click", function() {
   opCookie.set("disable_smt", "0", undefined, openpne.baseUrl);
   location.reload();
