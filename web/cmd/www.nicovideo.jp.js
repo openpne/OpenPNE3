b4b1b22a24b80a3b3dccd04d4809758e55bab339
nishizoe <nishizoe@tejimaya.com>
2019-11-29 16:30:39 +0900

    (fixed #4340, BP from #4324) iframe src を更新

--- web/cmd/www.nicovideo.jp.js
+++ web/cmd/www.nicovideo.jp.js
@@ -5,7 +5,7 @@ function url2cmd(url) {
     }
     var vid = RegExp.$1;
     var html = '<div style="width:318px; border:solid 1px #CCC;">'
-        + '<iframe src="http://www.nicovideo.jp/thumb/' + vid + '" width="100%"'
+        + '<iframe src="https://embed.nicovideo.jp/watch/' + vid + '" width="100%"'
         + ' height="198" scrolling="no" border="0" frameborder="0">'
         + '<p style="font-size:12px; padding:4px;">iframe対応ブラウザでご覧下さい。</p>'
         + '</iframe></div>';
